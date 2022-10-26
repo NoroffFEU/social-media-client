4312b64cdb38712f1c748201ef9cc7e4d518752a
johannranudd <33978097+johannranudd@users.noreply.github.com>
2022-10-26 03:23:38 +0200

    øozdfnjgiudrfg

---
+++ sum.js
@@ -0,0 +1,6 @@
+// function sum(a, b) {
+//   const result = a + b;
+//   return result;
+// }
+
+// module.expoorts = sum;
---
+++ sum.test.js
@@ -0,0 +1,6 @@
+// const sum = require('./sum');
+// // import sum from './test';
+
+// test('adds two numbers together', () => {
+//   expect(sum(1, 2)).toBe(3);
+// });
