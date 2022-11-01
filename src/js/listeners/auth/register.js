1fb343f44e5fb51105f5fc292b7ae76ca3ecec7c
RobertDacian <tennerdigitalart@gmail.com>
2022-11-01 16:40:20 -0300

    Added lint staged

--- src/js/listeners/auth/register.js
+++ src/js/listeners/auth/register.js
@@ -1,14 +1,14 @@
 import * as auth from "../../api/auth/index.js";
 
 export async function registerListener(event) {
-  event.preventDefault()
-  const form = event.target
-  const data = new FormData(form)
-  const email = data.get("email")
-  const name = data.get("name")
-  const password = data.get("password")
-  const avatar = data.get("avatar")
-  await auth.register(name, email, password, avatar)
-  await auth.login(email, password)
-  location.reload()
+  event.preventDefault();
+  const form = event.target;
+  const data = new FormData(form);
+  const email = data.get("email");
+  const name = data.get("name");
+  const password = data.get("password");
+  const avatar = data.get("avatar");
+  await auth.register(name, email, password, avatar);
+  await auth.login(email, password);
+  location.reload();
 }
