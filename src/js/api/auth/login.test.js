4cf49c1a2e578cc2a2ab4a9589900a9b128816fe
johannranudd <33978097+johannranudd@users.noreply.github.com>
2022-11-01 19:09:57 +0100

    adds login and logout.test.js files

---
+++ src/js/api/auth/login.test.js
@@ -0,0 +1,9 @@
+// import { login } from "./login";
+
+// test("returns a valid token when provided with valid credentials", async () => {
+//     const result = await login("email", "password")
+//     console.log(result);
+//     expect(result).toEqual("string")
+// })
+
+test.todo("makeshift test for login()");
