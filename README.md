31fdca63afeec0dc83cbea0bafce1ffe4a77a4f9
Gonzalo Longe <gonlonge@icloud.com>
2022-11-01 16:19:11 +0100

    added prettier

---
+++ README.md
@@ -0,0 +1,9 @@
+The following workflows/hooks are required:
+
+Project is configured to run Prettier on commit
+
+Project is configured to run ESLint on commit
+
+Project is configured to run Jest on commit
+
+Project is configured to deploy to pages on merge to default
--- src/js/listeners/post/comment.js
+++ src/js/listeners/post/comment.js
@@ -4,11 +4,11 @@ import { getSearchParams } from "../../router/searchParams.js";
 export async function commentListener(event) {
   event.preventDefault();
   const form = event.target;
-  const data = new FormData(form)
-  const body = data.get('body');
+  const data = new FormData(form);
+  const body = data.get("body");
   const postId = form.dataset.postId;
   const replyToId = getSearchParams().replyToId;
   await comment(postId, body, replyToId);
   form.remove();
-  location.reload()
+  location.reload();
 }
