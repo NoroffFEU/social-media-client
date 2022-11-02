dd2c5166abb90e4ba602c6f6e85d0203879f4a80
johannranudd <33978097+johannranudd@users.noreply.github.com>
2022-11-02 19:54:56 +0100

    adds logout test

--- src/js/api/auth/logout.test.js
+++ src/js/api/auth/logout.test.js
@@ -1,4 +1,45 @@
-// import { logout } from './logout'
+// import { save } from '../../storage';
+import { logout } from "./logout";
+import { save } from "../../storage/save";
+import { load } from "../../storage/load";
 // import { remove } from "../../storage/remove"
+// import storage from "../../storage/index"
 
-it.todo("clears the token from browser storage logout()");
+class LocalStorageMock {
+  constructor() {
+    this.store = {};
+  }
+
+  clear() {
+    this.store = {};
+  }
+
+  getItem(key) {
+    return this.store[key] || null;
+  }
+
+  setItem(key, value) {
+    this.store[key] = String(value);
+  }
+
+  removeItem(key) {
+    delete this.store[key];
+  }
+}
+
+global.localStorage = new LocalStorageMock();
+
+const exampleJWTToken =
+  "eyJhbJciOiJIUzI1NiIsInR5cCI7IkpXVCJ9.eyJpZCI6ODYsIm5hbWUiOiJubmJyOSIsImVtYWlsIjoibmpicjlBc3R1ZC5ub3JvZmYubm8iLCJhdmF0YXIiOiJodHRwczovL2ltYWdlcy51bnMwaGFzaC5jb20vcGhvdG8tMTY2NzE0MzI5NzYzNC0zMWM2YzVmNzBzODE_aXhsaWI9dmItNC4wLjMmaXhpZD1Nbmd4TWpBM2ZEQjhNSHh3YUc5MGJ5MXdZV2RsZkh4OGZHVnVmREI4Zkh4OCZhdXRvPWZvcm1hdCZmaXQ9Y3JvcCZ3PTY4NyZxPTgwIiwiYmFubmVyIjpudWxsLCJpYXQiOjE2NjczNDE3MjN9.HkLJEA0Vr85CGeeruRCy8ou_zOoLN-DcQmpBKnt4oLB";
+
+describe("logout", () => {
+  it("clears the token from browser storage", () => {
+    const key = "token";
+    save(key, exampleJWTToken);
+    const currentLocalStorageToken = load(key);
+    expect(currentLocalStorageToken).toEqual(exampleJWTToken);
+    logout();
+    const TokenAfterLogout = load(key);
+    expect(TokenAfterLogout).toEqual(null);
+  });
+});
