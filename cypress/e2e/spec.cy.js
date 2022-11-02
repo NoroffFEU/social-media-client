660d18160f7eb2dae9c8780343a091afa8847d46
johannranudd <33978097+johannranudd@users.noreply.github.com>
2022-11-02 23:41:59 +0100

    adds first cypress test scetch

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,9 @@
+describe("social media app: authenticated user", () => {
+  beforeEach("passes", () => {
+    cy.visit("https://example.cypress.io");
+  });
+
+  // it("CAN validate user inputs correctly based on API restrictions", () => {
+
+  // })
+});
