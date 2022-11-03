140f854ba821f97fd81b063624d3bc07c46481be
S10ANDK <91675991+S10ANDK@users.noreply.github.com>
2022-11-03 14:30:56 +0100

    cypress is configured

---
+++ cypress.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  e2e: {
+    setupNodeEvents() {
+      // implement node event listeners here
+    },
+  },
+});
+
+// import { defineConfig } from "cypress";
+
+// export default defineConfig({
+//   e2e: {
+//     setupNodeEvents(on, config) {
+//       // implement node event listeners here
+//     },
+//   },
+// });
