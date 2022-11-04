507584df340051b9bfc955734590332402e7e5e6
Espen Holm Pedersen <91594315+PederZzen@users.noreply.github.com>
2022-11-04 14:11:46 +0100

    installed jest

---
+++ src/_test_/jest.test.js
@@ -0,0 +1,7 @@
+const add = (x, y) => x + y;
+
+describe('addition', () => {
+  test('expect 2 + 3 = 5', () => {
+    expect(add(2, 3)).toEqual(5);
+  });
+});
--- src/js/test.js
+++
@@ -1,3 +0,0 @@
-console.log('Helllos');
-
-console.log('sup');
