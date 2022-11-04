37842047c5dab8de4a99e19ae16a590ed1b32be7
Espen Holm Pedersen <91594315+PederZzen@users.noreply.github.com>
2022-11-04 12:22:55 +0100

    testing ESLint

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,7 @@
-import router from "./router/index.js";
-import ui from "./ui/index.js";
+import router from './router/index.js';
+import ui from './ui/index.js';
 
-ui()
-router()
+ui();
+router();
+
+console.log('hello');
