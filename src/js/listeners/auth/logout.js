ca1a50a93f2cb473a49a94d658f969375e8d7407
Littlaa <83633992+Littlaa@users.noreply.github.com>
2022-11-04 13:40:18 +0100

    Added prettier and format run

--- src/js/listeners/auth/logout.js
+++ src/js/listeners/auth/logout.js
@@ -2,7 +2,7 @@ import * as auth from "../../api/auth/index.js";
 import { updateLoginVisibility } from "../../ui/auth.js";
 
 export function logoutListener() {
-  auth.logout()
-  updateLoginVisibility()
-  window.location.href = "/"
+  auth.logout();
+  updateLoginVisibility();
+  window.location.href = "/";
 }
