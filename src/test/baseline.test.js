dffca218c3be15e5194c30a621c2090e7a5d577b
ShaindalDev <89133440+ShaindalDev@users.noreply.github.com>
2022-11-06 00:57:21 +0100

    fixed eslint error and added baseline test

--- src/test/baseline.test.js
+++ src/test/baseline.test.js
@@ -1,3 +1,4 @@
-test('It adds 1 and 2 and gets 3 as a result', () => {
-    expect(1 + 2).toEqual(3);
-  });
+//This is just a baseline test to check that jest runs as it should
+test("It adds 1 and 2 and gets 3 as a result", () => {
+  expect(1 + 2).toEqual(3);
+});
