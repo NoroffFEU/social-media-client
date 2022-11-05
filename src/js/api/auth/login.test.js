f10fce54b0ded217aafd2bbd3ece8a45bb58a096
Melisa-Zorraindo <melisa.zorraindo@gmail.com>
2022-11-05 22:11:11 +0100

    fetchFailure invalid credentials

--- src/js/api/auth/login.test.js
+++ src/js/api/auth/login.test.js
@@ -44,6 +44,17 @@ function fetchSuccess() {
   });
 }
 
+const TEST_EMAIL_FAIL = "melisa@gmail.com";
+const TEST_PASSWORD_FAIL = "1234";
+
+function fetchFailure(statusCode = 404, status = "Not Found") {
+  return Promise.resolve({
+    ok: false,
+    statusCode,
+    status,
+  });
+}
+
 describe("login", () => {
   it("Returns a valid token when provided with valid credentials", async () => {
     global.fetch = jest.fn(() => fetchSuccess());
@@ -52,5 +63,14 @@ describe("login", () => {
     expect(TEST_EMAIL).toMatch(/^[\w\-.]+@(stud.)?noroff.no$/);
     expect(TEST_PASSWORD).toHaveLength(8);
     expect(profile).toMatch(TEST_PROFILE);
+  });
+
+  it("Returns an HTTP 401 error when provided with invalid credentials", async () => {
+    global.fetch = jest.fn(() => fetchFailure(401, "Unauthorized"));
+    const data = await login(TEST_EMAIL_FAIL, TEST_PASSWORD_FAIL);
+    const response = JSON.stringify(data);
+    expect(TEST_EMAIL_FAIL).not.toMatch(/^[\w\-.]+@(stud.)?noroff.no$/);
+    expect(TEST_PASSWORD_FAIL).not.toHaveLength(8);
+    expect(response).toEqual(undefined);
   });
 });
