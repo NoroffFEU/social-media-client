ba9f069086cb159f123ca0fe3f2b11aaafd98729
Melisa-Zorraindo <melisa.zorraindo@gmail.com>
2022-11-06 09:45:31 +0100

    test storage save

--- src/js/storage/save.test.js
+++ src/js/storage/save.test.js
@@ -4,13 +4,14 @@ import * as storage from "./index";
 global.localStorage = new LocalStorageMock();
 
 describe("storage", () => {
-  it("Loads an object from storage", () => {
+  it("Saves an object to storage", () => {
     const key = "profile";
     const value = {
       name: "melisa_zorraindo",
       email: "MelZor72659@stud.noroff.no",
     };
+    const serializedValue = JSON.stringify(value);
     storage.save(key, value);
-    expect(storage.load(key)).toEqual(value);
+    expect(localStorage.getItem(key)).toEqual(serializedValue);
   });
 });
