8c37cfa0311089da29fc10d46e88109f58bfde2b
Melisa-Zorraindo <melisa.zorraindo@gmail.com>
2022-11-06 09:08:44 +0100

    create file for localStorage mock

--- src/js/api/auth/login.test.js
+++ src/js/api/auth/login.test.js
@@ -1,29 +1,5 @@
 import { login } from "./login";
-
-//the following class comes from this link:
-//https://stackoverflow.com/questions/32911630/how-do-i-deal-with-localstorage-in-jest-tests
-
-class LocalStorageMock {
-  constructor() {
-    this.store = {};
-  }
-
-  clear() {
-    this.store = {};
-  }
-
-  getItem(key) {
-    return this.store[key] || null;
-  }
-
-  setItem(key, value) {
-    this.store[key] = String(value);
-  }
-
-  removeItem(key) {
-    delete this.store[key];
-  }
-}
+import { LocalStorageMock } from "../../storage/storageMock";
 
 global.localStorage = new LocalStorageMock();
 
