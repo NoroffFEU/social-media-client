eebc0e125af64ac37218cdf5b8e2ff30486aa471
Anclagen <91462616+Anclagen@users.noreply.github.com>
2022-11-07 01:21:01 +0100

    stop cypress video recordings

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  video: false,
   e2e: {
     setupNodeEvents(on, config) {
       config.env = {
