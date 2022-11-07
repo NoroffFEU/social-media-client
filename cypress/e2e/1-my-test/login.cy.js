cef4f29d48914c40a17bed74afa6478ae9c29399
albertholskog <albert_holskog@live.no>
2022-11-07 10:52:05 +0100

    update eslintc.json fil

--- cypress/e2e/1-my-test/login.cy.js
+++ cypress/e2e/1-my-test/login.cy.js
@@ -1,19 +1,1 @@
 /*eslint-disable */
-const speed = 400;
-
-describe("value token", () => {
-  it("login as valid user", () => {
-    cy.visit("http://127.0.0.1:5500/");
-    cy.get("button.btn.btn-outline-success").last().click({ delay: speed });
-    cy.get("input#loginEmail").type(
-      "albertt@noroff.no",
-      { force: true },
-      { delay: speed }
-    );
-    cy.get("input#loginPassword").type(
-      "albertalbert{enter}",
-      { force: true },
-      { delay: speed }
-    );
-  });
-});
