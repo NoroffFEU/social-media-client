0d7f697b955aeb29dbf36d58725c35e642c464ab
S10ANDK <91675991+S10ANDK@users.noreply.github.com>
2022-11-07 01:00:41 +0100

    written functional cypress tests

---
+++ cypress/e2e/client.cy.js
@@ -0,0 +1,66 @@
+describe('Authenticate user on Social Media App', () => {
+  beforeEach(() => {
+    cy.visit('http://127.0.0.1:5500/');
+    cy.clearLocalStorage();
+    cy.wait(2000);
+    cy.get("#registerForm .modal-header button[type='button']")
+      .should('have.class', 'btn-close')
+      .click();
+    cy.get('header button[data-auth=login]').click();
+    cy
+      .get("#loginModal input[type='email']")
+      .should('be.visible')
+      .type(`me@noroff.no`),
+      { delay: 3000 };
+    cy
+      .get("#loginModal input[type='email']")
+      .should('have.value', 'me@noroff.no'),
+      { delay: 300 };
+
+    cy.wait(2000);
+    cy
+      .get("#loginModal input[type='password']")
+      .should('be.visible')
+      .type(`1234512345{enter}`),
+      { delay: 200 };
+    cy.wait(1000);
+  });
+
+  it('can login with valid credentials', () => {
+    cy.url().should('include', 'profile');
+    expect(localStorage.getItem('token')).to.not.be.null;
+  });
+
+  it('can make a new post', () => {
+    cy.visit('http://127.0.0.1:5500/?view=post');
+    cy.get('#postForm')
+      .should('exist')
+      .within(() => {
+        cy.get("input[name='title']")
+          .should('exist')
+          .type(`title example with cypress test`);
+        cy.get("input[name='tags']")
+          .should('exist')
+          .type(`tag example with cypress test`);
+        cy.get("input[name='media']")
+          .should('exist')
+          .type(`https://www.vg.no/`);
+        cy
+          .get('#postBody')
+          .should('exist')
+          .type(`body example with cypress test`),
+          { delay: 200 };
+        cy.get("button[data-action='submit']").should('exist').click();
+      });
+    cy.wait(2000);
+  });
+
+  it('can log out by removing token from localStorage', () => {
+    cy.get('button[data-auth=logout]')
+      .contains(/log[\s]?out/i)
+      .click({ force: true })
+      .should(() => {
+        expect(localStorage.getItem('token')).to.be.null;
+      });
+  });
+});
