5c1d82ee82d9e9995e5206585ed312099849bb9a
Marianne Pedersen <mariannebpedersen@gmail.com>
2022-11-08 22:39:43 +0100

    Added e2e testing on login, create, logout

---
+++ cypress/e2e/tests/login.cy.js
@@ -0,0 +1,40 @@
+describe("User can NOT log in with invalid inputs based on API restrictions", () => {
+  it("User can't log in", () => {
+    cy.visit("http://127.0.0.1:5500/");
+    cy.clearLocalStorage();
+    cy.wait(500);
+    cy.get("#registerForm > div.modal-footer > button")
+      .contains("Login")
+      .click();
+    cy.wait(500);
+    cy.get(
+      "#loginForm > div.modal-body > div.form-floating > input[type=email]"
+    ).type("caitlin@noroff.no");
+    cy.get(
+      "#loginForm > div.modal-body > div.form-floating > input[type=password]"
+    ).type("passord");
+    cy.get("#loginForm > div.modal-footer > button").contains("Login").click();
+    cy.then(() => expect(window.localStorage.getItem("token")).to.be.null);
+  });
+});
+
+describe("User can log in with valid inputs based on API restrictions", () => {
+  it("User can log in", () => {
+    cy.visit("http://127.0.0.1:5500/");
+    cy.clearLocalStorage();
+    cy.wait(500);
+    cy.get("#registerForm > div.modal-footer > button")
+      .contains("Login")
+      .click();
+    cy.wait(500);
+    cy.get(
+      "#loginForm > div.modal-body > div.form-floating > input[type=email]"
+    ).type("caitlin@noroff.no");
+    cy.get(
+      "#loginForm > div.modal-body > div.form-floating > input[type=password]"
+    ).type("passord1");
+    cy.get("#loginForm > div.modal-footer > button").contains("Login").click();
+    cy.wait(500);
+    cy.then(() => expect(localStorage.getItem("token")).to.not.be.null);
+  });
+});
---
+++ cypress/e2e/tests/logout.cy.js
@@ -0,0 +1,22 @@
+describe("User can log out", () => {
+  it("Logs inn the user", () => {
+    cy.visit("http://127.0.0.1:5500/");
+    cy.clearLocalStorage();
+    cy.wait(500);
+    cy.get("#registerForm > div.modal-footer > button")
+      .contains("Login")
+      .click();
+    cy.wait(500);
+    cy.get(
+      "#loginForm > div.modal-body > div.form-floating > input[type=email]"
+    ).type("caitlin@noroff.no");
+    cy.get(
+      "#loginForm > div.modal-body > div.form-floating > input[type=password]"
+    ).type("passord1");
+    cy.get("#loginForm > div.modal-footer > button").contains("Login").click();
+  });
+  it("Logs the user out when logout button is clicked", () => {
+    cy.wait(1000);
+    cy.get("div.text-end > button").contains("Logout").click();
+  });
+});
