ef507e508079a07d22c164a077b1dae761aa6253
Marianne Pedersen <mariannebpedersen@gmail.com>
2022-11-08 18:04:49 +0100

    minor cleanup of code

--- src/js/api/auth/logout.test.js
+++ src/js/api/auth/logout.test.js
@@ -31,27 +31,3 @@ describe("logout", () => {
     expect(localStorage.getItem("profile")).toEqual(null);
   });
 });
-
-// describe("storage functionality", () => {
-//   it("saves data to localStorage", () => {
-//     const key = "token";
-//     const value = ["email", "password"];
-//     const item = JSON.stringify(value);
-//     storage.save(key, value);
-//     expect(localStorage.getItem(key)).toEqual(item);
-//   });
-//   it("loads data from localStorage", () => {
-//     const key = "token";
-//     const value = ["email", "password"];
-//     storage.save(key, value);
-//     expect(storage.load(key)).toEqual(value);
-//   });
-//   it("removes data from localStorage", () => {
-//     const key = "token";
-//     const value = ["email", "password"];
-//     storage.save(key, value);
-//     expect(storage.load(key)).toEqual(value);
-//     storage.remove(key);
-//     expect(storage.load(key)).toEqual(undefined || null);
-//   });
-// });
