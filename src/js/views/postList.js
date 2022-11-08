b871dd362b75270ac53e664f1ccf146c60207fd9
Martin P <70173574+MartinP3@users.noreply.github.com>
2022-11-08 14:06:03 +0100

    formatted with prettier

--- src/js/views/postList.js
+++ src/js/views/postList.js
@@ -2,7 +2,7 @@ import { postThumbnailTemplate } from "../templates/index.js";
 
 export const postList = (posts, preview = false) => {
   const element = document.createElement("div");
-    element.classList.add("post", "list")
-    element.append(...posts.map(post => postThumbnailTemplate(post, preview)))
-    return element
-}
+  element.classList.add("post", "list");
+  element.append(...posts.map((post) => postThumbnailTemplate(post, preview)));
+  return element;
+};
