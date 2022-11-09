962c958ab1658cde1f9f36cd3346701bcfca6c3e
RobertDacian <tennerdigitalart@gmail.com>
2022-11-09 12:48:24 -0300

    Update gitignore file

--- cypress.config.js
+++ cypress.config.js
@@ -1,23 +1,23 @@
 const { defineConfig } = require("cypress");
 
-module.exports = defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
-});
-
 // module.exports = defineConfig({
 //   e2e: {
-//     URL: "https://fermain.github.io/social-media-client/",
 //     setupNodeEvents(on, config) {
-//       config.env = {
-//         ...process.env,
-//         ...config.env,
-//       };
-//       return config;
+//       // implement node event listeners here
 //     },
 //   },
-//   video: false,
 // });
+
+module.exports = defineConfig({
+  e2e: {
+    URL: "https://fermain.github.io/social-media-client/",
+    setupNodeEvents(on, config) {
+      config.env = {
+        ...process.env,
+        ...config.env,
+      };
+      return config;
+    },
+  },
+  video: false,
+});
