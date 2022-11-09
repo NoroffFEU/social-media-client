bfb3cb15882e0e6953079795f1e2417ffe899b9d
AugustWahlberg <aug.wahlberg@gmail.com>
2022-11-09 16:05:19 +0100

    added userData and locator object to e2e testing

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,7 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+import { locatorObj } from "../pageLocators/locatorObj";
+
+// describe("validates user inputs correctly based on API restrictions", () => {
+//   it("User can login with valid inputs baded on API restrictions", () => {
+//     cy.visit("http://127.0.0.1:5500/")
+//   })
+// } )
