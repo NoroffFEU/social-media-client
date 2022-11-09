78be18164786f9aff3ac1f4d7316142e554b3658
Tactikerl <61546873+Tactikerl@users.noreply.github.com>
2022-11-09 22:47:06 +0100

    added additional tests for cypress

--- cypress/e2e/3-testing/e2e.cy.js
+++ cypress/e2e/3-testing/e2e.cy.js
@@ -11,10 +11,45 @@ describe('validate form input for login', () => {
     cy.url().should('not.include', 'profile');
   });
   it('CAN view the register form', () => {
-    cy.get(`header [data-auth='register']`).click();
+    cy.get(`header [data-auth='register']`).click({ force: true });
+    cy.get('form button').contains('Create Profile').should('be.visible');
   });
-  //   it('CANNOT visit profile page', () => {
-  //     cy.visit('http://127.0.0.1:5500/?view=profile');
-  //     cy.url().should('not.include', 'profile');
-  //   });
+  it('CAN view the login form', () => {
+    cy.get('#registerModal button')
+      .contains('Login')
+      .should('be.visible')
+      .click();
+  });
+});
+
+describe('Social Media App: Authenticated user', () => {
+  beforeEach(() => {
+    cy.visit('http://127.0.0.1:5500/');
+    cy.wait(500);
+    cy.clearLocalStorage();
+    cy.wait(500);
+    cy.get('#registerForm button[type=reset]')
+      .contains('Close')
+      .should('be.visible')
+      .click();
+    cy.wait(500);
+
+    cy.get(`header [data-auth='login']`)
+      .contains('Login')
+      .should('be.visible')
+      .click();
+    cy.wait(500);
+    cy.get('#loginModal input[type="email"]')
+      .should('be.visible')
+      .type('KMTest01@noroff.no', { delay: 100 });
+    cy.get('#loginModal input[type="password"]')
+      .should('be.visible')
+      .type('KMTest01{enter}', { delay: 100 });
+    cy.wait(1000);
+  });
+  it('CAN login', () => {
+    cy.url().should('include', 'profile');
+    cy.url().should('not.include', 'login');
+    expect(localStorage.getItem('token')).to.not.be.null;
+  });
 });
