12d1627984bdc3f3b55a5cc6ebcb0f6f5c160b8c
Tactikerl <61546873+Tactikerl@users.noreply.github.com>
2022-11-09 10:58:38 +0100

    added more cypress tests

--- cypress/e2e/3-testing/e2e.cy.js
+++ cypress/e2e/3-testing/e2e.cy.js
@@ -2,4 +2,19 @@ describe('validate form input for login', () => {
   beforeEach(() => {
     cy.visit('http://127.0.0.1:5500/');
   });
+
+  it('CAN visit the login page', () => {
+    cy.visit('http://127.0.0.1:5500/');
+  });
+  it('CANNOT visit profile page', () => {
+    cy.visit('http://127.0.0.1:5500/?view=profile');
+    cy.url().should('not.include', 'profile');
+  });
+  it('CAN view the register form', () => {
+    cy.get(`header [data-auth='register']`).click();
+  });
+  //   it('CANNOT visit profile page', () => {
+  //     cy.visit('http://127.0.0.1:5500/?view=profile');
+  //     cy.url().should('not.include', 'profile');
+  //   });
 });
