46633c366df660b3c5d8c66bc03acd822ceb74c8
RobertDacian <tennerdigitalart@gmail.com>
2022-11-09 12:00:25 -0300

    Update

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,23 @@
 const { defineConfig } = require("cypress");
 
+// module.exports = defineConfig({
+//   e2e: {
+//     setupNodeEvents(on, config) {
+//       // implement node event listeners here
+//     },
+//   },
+// });
+
 module.exports = defineConfig({
   e2e: {
+    URL: "https://fermain.github.io/social-media-client/",
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      config.env = {
+        ...process.env,
+        ...config.env,
+      };
+      return config;
     },
   },
+  video: false,
 });
