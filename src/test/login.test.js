44ca7174a6a9f3d22448e0fd15065de364b8f056
Tanix <91118560+Tanix98@users.noreply.github.com>
2022-11-09 18:57:54 +0100

    add jest tests

--- src/test/login.test.js
+++ src/test/login.test.js
@@ -1,1 +1,7 @@
-test('login', () => {});
+import { login } from '../js/api/auth/login.js';
+import { profile } from '../js/api/auth/login.js';
+
+test('The login function returns a valid token when provided with valid credentials', () => {
+  login('oysRos99836@stud.noroff.no', 'Password98');
+  expect(profile.accessToken).toBeTruthy();
+});
