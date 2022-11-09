fbaf71e6701245e91d81bc94e1fc5a27b738a2ac
May-Tove <89157761+May-Tove@users.noreply.github.com>
2022-11-09 12:14:43 +0100

    Update login.test.js

--- src/js/api/auth/login.test.js
+++ src/js/api/auth/login.test.js
@@ -1,10 +1,19 @@
 import { login } from "./login";
 
+const name = "mayt";
+const email = "maytest25@noroff.no";
+const invalidEmail = "test@email.no";
+const password = "password";
+const avatar =
+  "https://images.unsplash.com/photo-1588943211346-0908a1fb0b01?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1035&q=80";
+const token =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MjcwLCJuYW1lIjoiTWF5dCIsImVtYWlsIjoibWF5dGVzdDI1QG5vcm9mZi5ubyIsImF2YXRhciI6Imh0dHBzOi8vaW1hZ2VzLnVuc3BsYXNoLmNvbS9waG90by0xNTg4OTQzMjExMzQ2LTA5MDhhMWZiMGIwMT9peGxpYj1yYi00LjAuMyZpeGlkPU1ud3hNakEzZkRCOE1IeHdhRzkwYnkxd1lXZGxmSHg4ZkdWdWZEQjhmSHg4JmF1dG89Zm9ybWF0JmZpdD1jcm9wJnc9MTAzNSZxPTgwIiwiYmFubmVyIjpudWxsLCJpYXQiOjE2Njc5OTE2MjV9.QTqA-Jp8aYphbH53L4qFDnxrWgb87jB3NaowXtRCHh0";
+
 const profile = {
-  name: "my_username",
-  email: "first.last@noroff.no",
-  avatar: "https://img.service.com/avatar.jpg",
-  accessToken: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9....",
+  name: name,
+  email: email,
+  avatar: avatar,
+  accessToken: token,
 };
 
 class LocalStorageMock {
@@ -44,7 +53,7 @@ function fetchSuccess() {
     ok: true,
     status: 200,
     statusText: "OK",
-    json: () => Promise.resolve(profile.accessToken),
+    json: () => Promise.resolve(profile),
   });
 }
 
@@ -58,21 +67,25 @@ function fetchSuccess() {
  * global.fetch = jest.fn(() => fetchFailure(500, "Internal Server Error"));
  * ```
  */
-/*function fetchFailure(status = 401, statusText = "Unsuccessful") {
+function fetchFailure(status = 401, statusText = "Unsuccessful") {
   return Promise.resolve({
     ok: false,
     status,
     statusText,
   });
-}*/
+}
 
 describe("login", () => {
   it("Returns a valid token when provided with valid credentials", async () => {
     global.fetch = jest.fn(() => fetchSuccess());
-    const localStorageToken = JSON.parse(localStorage.getItem("token"));
     const response = await login(profile);
-    expect(localStorageToken).toEqual(profile.accessToken);
+    expect(response.accessToken).toEqual(profile.accessToken);
     expect(response.email).toEqual(profile.email);
     expect(response.password).toEqual(profile.password);
+  });
+
+  it("Will not login in when provided invalid credentials", async () => {
+    global.fetch = jest.fn(() => fetchFailure());
+    await expect(login(invalidEmail, password)).rejects.toThrow("Unsuccessful");
   });
 });
