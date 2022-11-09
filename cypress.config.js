aad15a0e2e43d5186c58d159a8c16b81b77bef4d
RobertDacian <tennerdigitalart@gmail.com>
2022-11-09 12:51:55 -0300

    Updated the cypress.config file

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,5 @@
 const { defineConfig } = require("cypress");
 
-// module.exports = defineConfig({
-//   e2e: {
-//     setupNodeEvents(on, config) {
-//       // implement node event listeners here
-//     },
-//   },
-// });
-
 module.exports = defineConfig({
   e2e: {
     URL: "https://fermain.github.io/social-media-client/",
