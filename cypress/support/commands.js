26bb5c78bf12fe668cfa875ae4c0f1801ccfd31b
Martin Mroz <mamr@hotmail.no>
2022-11-09 20:55:02 +0100

    Working on login test

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,8 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+//email:
+//MarMro46583@stud.noroff.no
+//password:
+//Martinnoroff
