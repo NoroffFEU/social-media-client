524ed93688586d5cc3e1e9d450c6db87f75794db
Nattlegend <44141432+Nattlegend@users.noreply.github.com>
2022-11-10 21:03:31 +0100

    Adding longer waiting time between actions

--- cypress/e2e/social-media-test/login.cy.js
+++ cypress/e2e/social-media-test/login.cy.js
@@ -2,9 +2,9 @@ describe('Can login to the page', () => {
   it('passes', () => {
     cy.visit('/');
     cy.clearLocalStorage();
-    cy.wait(800);
+    cy.wait(1000);
     cy.get("button[data-auth='login']:visible").click();
-    cy.wait(800);
+    cy.wait(1000);
     // cy.get("input[type='email']:visible").should("exist").type(Cypress.env("EMAIL"));
     cy.get("input[type='email']:visible")
       .should('exist')
@@ -12,7 +12,7 @@ describe('Can login to the page', () => {
     cy.get("input[type='password']:visible")
       .should('exist')
       .type('Fakepassword200');
-    cy.wait(200);
+    cy.wait(1000);
     cy.get("button[data-auth='loginButton']:visible").click();
   });
 });
--- cypress/e2e/social-media-test/logout.cy.js
+++ cypress/e2e/social-media-test/logout.cy.js
@@ -2,20 +2,20 @@ describe('Can login to the page and perform an logout + clearing localStorage', 
   it('Logout of the site', () => {
     cy.visit('/');
     cy.clearLocalStorage();
-    cy.wait(800);
+    cy.wait(1000);
     cy.get("button[data-auth='login']:visible").click();
-    cy.wait(800);
+    cy.wait(1000);
     cy.get("input[type='email']:visible")
       .should('exist')
       .type('faketestaccount@stud.noroff.no');
     cy.get("input[type='password']:visible")
       .should('exist')
       .type('Fakepassword200');
-    cy.wait(800);
+    cy.wait(1000);
     cy.get("button[data-auth='loginButton']:visible").click();
     cy.wait(2000);
     cy.get("button[data-auth='logout']:visible").click();
-    cy.wait(600);
+    cy.wait(1000);
     cy.clearLocalStorage();
   });
 });
