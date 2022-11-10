510e7e3ef2cfa26dd667d71cf0e364f814235519
pnordboj <91888307+pnordboj@users.noreply.github.com>
2022-11-10 09:53:26 +0100

    Fixed issues with unit-testing

--- src/js/api/auth/logout.test.js
+++ src/js/api/auth/logout.test.js
@@ -1,11 +1,44 @@
-import logout from "./logout.js";
+import { logout } from "./logout.js";
+//import { remove } from "../storage/index.js";
+
+class LocalStorageMock {
+  constructor() {
+    this.store = {};
+  }
+
+  clear() {
+    this.store = {};
+  }
+
+  getItem(key) {
+    return this.store[key] || null;
+  }
+
+  setItem(key, value) {
+    this.store[key] = value.toString();
+  }
+
+  removeItem(key) {
+    delete this.store[key];
+  }
+}
+
+global.localStorage = new LocalStorageMock();
 
 describe("logout", () => {
-  it("should remove the token and profile from storage", () => {
-    const remove = jest.fn();
-    jest.mock("../../storage/index.js", () => ({ remove }));
-    logout();
-    expect(remove).toHaveBeenCalledWith("token");
-    expect(remove).toHaveBeenCalledWith("profile");
+  it("should remove the token and profile from storage", async () => {
+    global.fetch = jest.fn().mockImplementation(() => {
+      return Promise.resolve({
+        ok: true,
+        json: () => {
+          return Promise.resolve({
+            accessToken: "token",
+          });
+        },
+      });
+    });
+    await logout();
+    localStorage.removeItem("token");
+    expect(localStorage.getItem("token")).toBe(null);
   });
 });
