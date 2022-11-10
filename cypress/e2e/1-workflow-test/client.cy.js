0f94ca16cd2e6b22ffe093fdb6df76fe2c6f56de
pnordboj <91888307+pnordboj@users.noreply.github.com>
2022-11-10 13:30:37 +0100

    Added E2E testing scripts

--- cypress/e2e/1-workflow-test/client.cy.js
+++ cypress/e2e/1-workflow-test/client.cy.js
@@ -1,49 +1,70 @@
-class LocalStorageMock {
-  constructor() {
-    this.store = {};
-  }
-
-  clear() {
-    this.store = {};
-  }
-
-  getItem(key) {
-    return this.store[key] || null;
-  }
-
-  setItem(key, value) {
-    this.store[key] = value.toString();
-  }
-
-  removeItem(key) {
-    delete this.store[key];
-  }
-}
-
-describe("login", () => {
-  it("CAN view website", () => {
+describe("Workflow Media App", () => {
+  beforeEach("login", () => {
     cy.visit("http://localhost:5500");
-  });
-  it("CAN view the register form", () => {
     cy.get("header [data-auth='register']").click({ force: true });
     cy.get("form button").contains("Create Profile").should("be.visible");
-  });
-  it("CAN view the login form", () => {
+    cy.wait(500);
+
     cy.get("#registerModal button")
       .contains("Login")
       .should("be.visible")
       .click({ delay: 1000 });
-  });
-  it("CAN type email in login form", () => {
-    cy.get("#loginEmail").type("cypress@noroff.no", {
+
+    cy.wait(500);
+
+    cy.get("#loginEmail").type("PnordboTest@noroff.no", {
       force: true,
       delay: 100,
     });
-  });
-  it("CAN type password in login form", () => {
-    cy.get("#loginPassword").type("ABCeasyas123", { delay: 100 });
-  });
-  it("CAN submit login form", () => {
+
+    cy.wait(500);
+
+    cy.get("#loginPassword").type("220197Pn", { delay: 100 });
+    cy.wait(500);
+
     cy.get("#loginForm button").contains("Login").should("be.visible").click();
+    cy.wait(3000);
+  });
+
+  it("CAN create a post", () => {
+    cy.get("footer [data-visible='loggedIn']")
+      .contains("New Post")
+      .should("be.visible")
+      .click({ delay: 1000 });
+    cy.wait(500);
+    cy.wait(3000);
+
+    cy.get("#postTitle")
+      .should("be.visible")
+      .type("Test Post", { force: true, delay: 100 });
+    cy.get("#postTags").should("be.visible").type("Test", {
+      force: true,
+      delay: 100,
+    });
+    cy.get("#postMedia")
+      .should("be.visible")
+      .type(
+        "https://banner2.cleanpng.com/20180820/uzt/kisspng-smiley-clip-art-thumb-signal-image-emoticon-thumbs-up-transparent-4-5163-shop-of-clipart-l-5b7b6924b44350.5818812315348145007384.jpg",
+        { force: true, delay: 50 }
+      );
+    cy.get("#postBody")
+      .should("be.visible")
+      .type("Test Body Lorum Ipsum etc...", {
+        force: true,
+        delay: 100,
+      });
+    /* Disabled so it won't post to the API
+        cy.get("#postForm button > span")
+            .contains("Publish")
+            .should("be.visible")
+            .click({ force: true });
+        */
+  });
+
+  it("CAN logout", () => {
+    cy.get("button")
+      .contains("Logout")
+      .should("be.visible")
+      .click({ force: true });
   });
 });
