bef5e94c84399aef329aa95b6c3e8557744bf4ec
Etten Ytten <honeygeeh27@gmail.com>
2022-11-10 15:19:50 +0100

    updating the logout.cy.js

--- cypress/e2e/logout.cy.js
+++ cypress/e2e/logout.cy.js
@@ -1,8 +1,31 @@
-it('can log out by removing token from localStorage', () => {
-  cy.get('button[data-auth=logout]')
-    .contains(/log[\s]?out/i)
-    .click({ force: true })
-    .should(() => {
-      expect(localStorage.getItem('token')).to.be.null;
-    });
+// it('can log out by removing token from localStorage', () => {
+//   cy.get('button[data-auth=logout]')
+//     .contains(/log[\s]?out/i)
+//     .click({ force: true })
+//     .should(() => {
+//       expect(localStorage.getItem('token')).to.be.null;
+//     });
+// });
+
+describe('User can log out', () => {
+  it('Logs inn the user', () => {
+    cy.visit('http://127.0.0.1:5500/');
+    cy.clearLocalStorage();
+    cy.wait(500);
+    cy.get('#registerForm > div.modal-footer > button')
+      .contains('Login')
+      .click();
+    cy.wait(500);
+    cy.get(
+      '#loginForm > div.modal-body > div.form-floating > input[type=email]'
+    ).type('caitlin@noroff.no');
+    cy.get(
+      '#loginForm > div.modal-body > div.form-floating > input[type=password]'
+    ).type('passord1');
+    cy.get('#loginForm > div.modal-footer > button').contains('Login').click();
+  });
+  it('Logs the user out when logout button is clicked', () => {
+    cy.wait(1000);
+    cy.get('div.text-end > button').contains('Logout').click();
+  });
 });
