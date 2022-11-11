ac90f36b156505fa6fd9921398afb266802f2e16
Nattlegend <44141432+Nattlegend@users.noreply.github.com>
2022-11-11 18:26:06 +0100

    Updated login.cy.js with more specific button clicks

--- cypress/e2e/social-media-test/login.cy.js
+++ cypress/e2e/social-media-test/login.cy.js
@@ -3,7 +3,9 @@ describe('Can login to the page', () => {
     cy.visit('/');
     cy.clearLocalStorage();
     cy.wait(1000);
-    cy.get("button[data-auth='login']:visible").click();
+    cy.get("#registerForm button[data-auth='login']:visible")
+      .contains('Login')
+      .click();
     cy.wait(1000);
     // cy.get("input[type='email']:visible").should("exist").type(Cypress.env("EMAIL"));
     cy.get("input[type='email']:visible")
@@ -13,6 +15,6 @@ describe('Can login to the page', () => {
       .should('exist')
       .type('Fakepassword200');
     cy.wait(1000);
-    cy.get("button[data-auth='loginButton']:visible").click();
+    cy.get("#loginForm button[data-auth='loginButton']:visible").click();
   });
 });
