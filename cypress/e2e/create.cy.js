931c0f89d556baccf3753fa9de888737c4ce1acc
Etten Ytten <honeygeeh27@gmail.com>
2022-11-11 16:11:20 +0100

    final overall updates

--- cypress/e2e/create.cy.js
+++ cypress/e2e/create.cy.js
@@ -2,14 +2,12 @@ describe('Authentication', () => {
   beforeEach(() => {
     cy.visit('/');
     cy.clearLocalStorage();
-  });
-
-  it('will login', () => {
     cy.visit('/');
     cy.wait(1000);
     cy.get('.btn-close:visible').click();
+    cy.wait(500);
     cy.get("button[data-auth='login']:visible").click();
-    cy.wait(1500);
+    cy.wait(1000);
     cy.get("input[type='email']:visible")
       .should('exist')
       .type('cocomarcia@noroff.no');
@@ -17,27 +15,33 @@ describe('Authentication', () => {
       .should('exist')
       .type('cocomarcia1');
     cy.get('.btn-success:visible').click();
-    cy.wait(3000);
-    cy.then(
-      () => expect(window.localStorage.getItem('profile')).to.not.be.null
-    );
-    cy.then(() => expect(window.localStorage.getItem('token')).to.not.be.null);
-    cy.url().should('include', 'profile');
+    cy.wait(3500);
+    cy.visit('/');
   });
 
-  it('Can create item', () => {
+  it(' create item form that validates user inputs correctly based on API restrictions', () => {
+    cy.wait(800);
     cy.visit('/?view=post');
-    cy.get('form#postForm')
-      .should('exist')
-      .within(() => {
-        cy.get("input[name='title']")
-          .should('exist')
-          .type('Cypress test title');
-        cy.get("textarea[name='body']")
-          .should('exist')
-          .type('Cypress test body');
-        cy.get("input[name='tags']").should('exist').type('Cypress, test');
-        cy.get("button[data-action='submit']").should('exist').click();
-      });
+    cy.get('#footerActions > a.btn')
+      .contains('New Post')
+      .should('be.visible')
+      .click({ force: true });
+    cy.get("form#postForm  input[name='title']")
+      .should('be.visible')
+      .type('Cypress testing')
+      .should('have.value', 'Cypress testing');
+    cy.get("form#postForm   input[name='tags']")
+      .should('be.visible')
+      .type('cypress');
+    cy.get("form#postForm input[name='media']")
+      .should('be.visible')
+      .type('https://picsum.photos/id/237/200/300');
+
+    cy.get("form#postForm textarea[name ='body']")
+      .should('be.visible')
+      .type('Test cypress');
+    cy.get('form#postForm > div.col-12 > button.btn-success')
+      .should('be.visible')
+      .click();
   });
 });
