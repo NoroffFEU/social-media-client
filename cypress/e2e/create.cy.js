7473b047352a3961feaa4614dfeae01fa899296b
Etten Ytten <honeygeeh27@gmail.com>
2022-11-11 12:55:39 +0100

    Update testing

--- cypress/e2e/create.cy.js
+++ cypress/e2e/create.cy.js
@@ -1,28 +1,43 @@
-describe('User can create an item', () => {
-  it('can make a new post', () => {
-    cy.visit('http://127.0.0.1:5501/');
+describe('Authentication', () => {
+  beforeEach(() => {
+    cy.visit('/');
+    cy.clearLocalStorage();
+  });
+
+  it('will login', () => {
+    cy.visit('/');
+    cy.wait(1000);
+    cy.get('.btn-close:visible').click();
+    cy.get("button[data-auth='login']:visible").click();
+    cy.wait(1500);
+    cy.get("input[type='email']:visible")
+      .should('exist')
+      .type('cocomarcia@noroff.no');
+    cy.get("input[type='password']:visible")
+      .should('exist')
+      .type('cocomarcia1');
+    cy.get('.btn-success:visible').click();
+    cy.wait(3000);
+    cy.then(
+      () => expect(window.localStorage.getItem('profile')).to.not.be.null
+    );
+    cy.then(() => expect(window.localStorage.getItem('token')).to.not.be.null);
+    cy.url().should('include', 'profile');
+  });
+
+  it('Can create item', () => {
     cy.visit('/?view=post');
-    cy.get('#postForm')
+    cy.get('form#postForm')
       .should('exist')
       .within(() => {
         cy.get("input[name='title']")
           .should('exist')
-          .type(`title example with cypress test`);
-        cy.get("input[name='tags']")
+          .type('Cypress test title');
+        cy.get("textarea[name='body']")
           .should('exist')
-          .type(`tag example with cypress test`);
-        cy.get("input[name='media']")
-          .should('exist')
-          .type(
-            ` 'https://www.dreamstime.com/stock-photo-blue-corns-close-up-some-image34319130'/`
-          );
-        cy
-          .get('#postBody')
-          .should('exist')
-          .type(`body example with cypress test`),
-          { delay: 200 };
+          .type('Cypress test body');
+        cy.get("input[name='tags']").should('exist').type('Cypress, test');
         cy.get("button[data-action='submit']").should('exist').click();
       });
-    cy.wait(2000);
   });
 });
--- cypress/e2e/logout.cy.js
+++ cypress/e2e/logout.cy.js
@@ -1,8 +1,50 @@
-it('can log out by removing token from localStorage', () => {
-  cy.get('button[data-auth=logout]')
-    .contains(/log[\s]?out/i)
-    .click({ force: true })
-    .should(() => {
-      expect(localStorage.getItem('token')).to.be.null;
-    });
+export const apiBase = 'https://nf-api.onrender.com/api/v1/';
+export const url = new URL(apiBase);
+
+describe('Authentication', () => {
+  beforeEach(() => {
+    cy.visit('/');
+    cy.clearLocalStorage();
+  });
+
+  it('can login', () => {
+    cy.visit('/');
+    cy.wait(1000);
+    cy.get('.btn-close:visible').click();
+    cy.get("button[data-auth='login']:visible").click();
+    cy.wait(1500);
+    cy.get("input[type='email']:visible")
+      .should('exist')
+      .type('cocomarcia@noroff.no');
+    cy.get("input[type='password']:visible")
+      .should('exist')
+      .type('cocomarcia1');
+    cy.get('.btn-success:visible').click();
+    cy.wait(3000);
+    cy.then(
+      () => expect(window.localStorage.getItem('profile')).to.not.be.null
+    );
+    cy.then(() => expect(window.localStorage.getItem('token')).to.not.be.null);
+    cy.url().should('include', 'profile');
+  });
+
+  it('can logout', () => {
+    cy.get('.btn-close:visible').click();
+    cy.wait(500);
+    cy.get("button[data-auth='login']:visible").click();
+    cy.wait(500);
+    cy.get("input[type='email']:visible")
+      .should('exist')
+      .type(`testersen@noroff.no`);
+    cy.get("input[type='password']:visible")
+      .should('exist')
+      .type(`Mypassword{enter}`);
+    cy.wait(2000);
+    cy.url().should('include', 'profile');
+    cy.get("button[data-auth='logout']:visible")
+      .click({ force: true })
+      .should(() => {
+        expect(localStorage.getItem('token')).to.be.null;
+      });
+  });
 });
