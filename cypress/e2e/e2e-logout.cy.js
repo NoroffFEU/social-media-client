6edb7d088922a253e8178952dc343ebbc92c377b
AnnaHelene01 <91527942+AnnaHelene01@users.noreply.github.com>
2022-11-11 15:50:51 +0100

    testing vite again..

--- cypress/e2e/e2e-logout.cy.js
+++ cypress/e2e/e2e-logout.cy.js
@@ -1,22 +1,17 @@
 describe("E2E 1", () => {
   it("Logout btn logs the user out when clicked - test", () => {
     cy.visit("/");
-    cy.get(".btn-close:visible").click();
-    //cy.wait(1000)
-    cy.get("button[data-auth='login']:visible").click();
-    //cy.wait(1000);
-
-    //Get email input and type
-    cy.get("input[type='email']:visible").type("rotta@noroff.no");
-    //Get password input and type
-    cy.get("input[type='password']:visible").type("rotta123");
-    //Get login btn and log in
-    cy.get(".btn-success:visible").click();
-    //cy.wait(1000);
-    //Get localStorage : token
-    cy.getLocalStorage("token").then((token) => {
-      expect(token).to.not.be.null;
+    cy.get("#registerForm").within(() => {
+      cy.get(".btn-close:visible").click();
     });
+    cy.get("button[data-auth='login']:visible").click();
+    cy.get("#loginForm").within(() => {
+      cy.get("input[type='email']:visible").type("rotta@noroff.no");
+      cy.get("input[type='password']:visible").type("rotta123");
+      cy.get("button[type='submit']").click();
+    });
+
+    cy.then(() => expect(window.localStorage.getItem("token")).to.not.be.null);
 
     //Get logout btn and log out
     cy.get("button").contains("Logout").click();
--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,6 @@ export default {
   },
 
   build: {
-    outdir: "./dist",
+    outdir: "./vite",
   },
 };
