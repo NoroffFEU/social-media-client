7ad3e3ab22dfa7816faffa9690511c16419d1dee
Etten Ytten <honeygeeh27@gmail.com>
2022-11-12 15:15:30 +0100

    specified login in login.cy.js

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -8,7 +8,9 @@ describe('Authentication', () => {
     cy.visit('/');
     cy.wait(1000);
     cy.get('.btn-close:visible').click();
-    cy.get("button[data-auth='login']:visible").click();
+    cy.get("#registerForm button[data-auth='login']:visible")
+      .contains('Login')
+      .click();
     cy.wait(1500);
     cy.get("input[type='email']:visible")
       .should('exist')
