d9567de9107a693c56be2f782eb5d666bb5d846d
Etten Ytten <honeygeeh27@gmail.com>
2022-11-12 20:33:53 +0100

    Updates for the all the test in unit and e2e

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -8,18 +8,15 @@ describe('Authentication', () => {
     cy.visit('/');
     cy.wait(1000);
     cy.get('.btn-close:visible').click();
-    cy.wait(500);
-    cy.get("button.btn-outline-success[data-auth='login']:visible")
-      .contains('Login')
-      .click();
+    cy.get("button[data-auth='login']:visible").click();
     cy.wait(1500);
-    cy.get("#loginModal input[type='email']:visible")
+    cy.get("input[type='email']:visible")
       .should('exist')
       .type('cocomarcia@noroff.no');
-    cy.get("#loginModal input[type='password']#loginPassword:visible")
+    cy.get("input[type='password']:visible")
       .should('exist')
       .type('cocomarcia1');
-    cy.get('button.btn-success:visible').click();
+    cy.get('.btn-success:visible').click();
     cy.wait(3000);
     cy.then(
       () => expect(window.localStorage.getItem('profile')).to.not.be.null
@@ -31,27 +28,26 @@ describe('Authentication', () => {
   it('Validates email input', () => {
     cy.visit('/');
     cy.wait(1000);
-    cy.get('#reg-btn:visible').click();
-    cy.wait(500);
-    cy.get("#registerForm button[data-auth='login']:visible").click();
+    cy.get('.btn-close:visible').click();
+    cy.get("button[data-auth='login']:visible").click();
     cy.wait(1500);
     cy.get("input[type='email']:visible")
       .should('exist')
-      .type('cocomarcia@noroff.no');
+      .type('/https://nf-api.onrender.com');
     cy.get("input[type='password']:visible")
       .should('exist')
       .type('cocomarcia1');
     cy.get('.btn-success:visible').click();
     cy.wait(3000);
-    cy.then(() => expect(window.localStorage.getItem('profile')).to.exist);
-    cy.then(() => expect(window.localStorage.getItem('token')).to.exist);
+    cy.then(() => expect(window.localStorage.getItem('profile')).to.be.null);
+    cy.then(() => expect(window.localStorage.getItem('token')).to.be.null);
+    cy.url().should('not.include', 'profile');
   });
 
   it('Validates password', () => {
     cy.visit('/');
     cy.wait(1000);
-    cy.get('#login-btn:visible').click();
-    cy.wait(500);
+    cy.get('.btn-close:visible').click();
     cy.get("button[data-auth='login']:visible").click();
     cy.wait(1500);
     cy.get("input[type='email']:visible")
