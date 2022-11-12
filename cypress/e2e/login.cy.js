1ea00e1a80a224571ba466efbb4576502aa06a07
Etten Ytten <honeygeeh27@gmail.com>
2022-11-12 18:06:30 +0100

    testing

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -31,7 +31,7 @@ describe('Authentication', () => {
   it('Validates email input', () => {
     cy.visit('/');
     cy.wait(1000);
-    cy.get('#reg-btn .btn-close:visible').click();
+    cy.get('#reg-btn:visible').click();
     cy.wait(500);
     cy.get("#registerForm button[data-auth='login']:visible").click();
     cy.wait(1500);
@@ -50,7 +50,7 @@ describe('Authentication', () => {
   it('Validates password', () => {
     cy.visit('/');
     cy.wait(1000);
-    cy.get('#login-btn .btn-close:visible').click();
+    cy.get('#login-btn:visible').click();
     cy.wait(500);
     cy.get("button[data-auth='login']:visible").click();
     cy.wait(1500);
