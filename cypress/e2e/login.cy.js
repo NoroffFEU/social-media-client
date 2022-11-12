32c5f9a7d1fd702053b4c79e3270fc2ef9e87ec6
Etten Ytten <honeygeeh27@gmail.com>
2022-11-12 15:55:34 +0100

    added wait between clicks

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -8,6 +8,7 @@ describe('Authentication', () => {
     cy.visit('/');
     cy.wait(1000);
     cy.get('.btn-close:visible').click();
+    cy.wait(500);
     cy.get("button[data-auth='login']:visible").contains('Login').click();
     cy.wait(1500);
     cy.get("#loginModal input[type='email']:visible")
@@ -29,6 +30,7 @@ describe('Authentication', () => {
     cy.visit('/');
     cy.wait(1000);
     cy.get('.btn-close:visible').click();
+    cy.wait(500);
     cy.get("button[data-auth='login']:visible").click();
     cy.wait(1500);
     cy.get("input[type='email']:visible")
@@ -48,6 +50,7 @@ describe('Authentication', () => {
     cy.visit('/');
     cy.wait(1000);
     cy.get('.btn-close:visible').click();
+    cy.wait(500);
     cy.get("button[data-auth='login']:visible").click();
     cy.wait(1500);
     cy.get("input[type='email']:visible")
