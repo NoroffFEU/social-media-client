9735ce78873db1f44673b3ca40b1ed87289a73d9
SaraRFerrer <sararferrer@hotmail.com>
2022-11-12 14:23:00 +0100

    changing cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -1,16 +1,9 @@
-require("dotenv").config();
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    URL: "https://fermain.github.io/social-media-client/",
     setupNodeEvents(on, config) {
-      config.env = {
-        ...process.env,
-        ...config.env,
-      };
-      return config;
+      false;
     },
   },
-  video: false,
 });
