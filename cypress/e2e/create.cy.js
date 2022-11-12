a4d2ab5b1633d110d41c5944e4bcf888a4f6ebe8
Etten Ytten <honeygeeh27@gmail.com>
2022-11-12 10:22:19 +0100

    changes applied

--- cypress/e2e/create.cy.js
+++ cypress/e2e/create.cy.js
@@ -5,7 +5,7 @@ describe('Authentication', () => {
     cy.visit('/');
     cy.wait(1000);
     cy.get('.btn-close:visible').click();
-    cy.wait(500);
+    cy.wait(1200);
     cy.get("button[data-auth='login']:visible").click();
     cy.wait(1000);
     cy.get("input[type='email']:visible")
