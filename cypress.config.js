ab29e333b14e687880bbee0bbc271315e9390cf1
Mohammed <allawi465@gmail.com>
2022-11-12 23:09:31 +0100

    cypress baseurl

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,11 @@
-const { defineConfig } = require("cypress");
-require("dotenv").config();
+const { defineConfig } = require('cypress');
+require('dotenv').config();
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       config.env = {
-        URL: "http://127.0.0.1:5500/",
+        URL: 'http://127.0.0.1:8081/',
         ...process.env,
         ...config.env,
       };
