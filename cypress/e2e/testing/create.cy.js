f019e013f74514af9c45d8ede7643f1774cd4d8b
Karla <73695367+Noizenne@users.noreply.github.com>
2022-11-12 22:04:29 +0100

    Cypress testing post

--- cypress/e2e/testing/create.cy.js
+++ cypress/e2e/testing/create.cy.js
@@ -15,7 +15,15 @@ describe('Create post test', () => {
     cy.get('#loginForm input[name=password]').should('exist').type('karla123');
     cy.wait(500);
     cy.get('#loginForm button').contains('Login').should('be.visible').click();
-    cy.wait(2000);
+  });
+
+  it('can create a post', () => {
+    cy.wait(1000);
     cy.visit('http://127.0.0.1:5500/?view=post');
+    cy.get('#postTitle').should('exist').type('Good evening!');
+    cy.get('#postTags').should('exist').type('Cypress');
+    cy.get('#postBody').should('exist').type('Pushing through!');
+    cy.wait(500);
+    cy.get("#postForm [data-action='submit']").should('be.visible').click();
   });
 });
