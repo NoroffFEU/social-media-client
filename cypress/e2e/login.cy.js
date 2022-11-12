175f75ef66815b52e3c953ba4fe4c854ec548a6b
Etten Ytten <honeygeeh27@gmail.com>
2022-11-13 00:45:45 +0100

    add contains

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -8,7 +8,9 @@ describe('Authentication', () => {
     cy.visit('/');
     cy.wait(1000);
     cy.get('.btn-close:visible').click();
-    cy.get("button[data-auth='login']:visible").click({ multiple: true });
+    cy.get("button[data-auth='login']:visible")
+      .contains('Login')
+      .click({ multiple: true });
 
     cy.wait(1500);
     cy.get('#loginForm').within(() => {
