2ea3064445df563e71e80a966ad9696cb47393fb
SaraRFerrer <sararferrer@hotmail.com>
2022-11-12 14:19:10 +0100

    fixing bugs

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,16 @@
+require("dotenv").config();
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    URL: "https://fermain.github.io/social-media-client/",
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      config.env = {
+        ...process.env,
+        ...config.env,
+      };
+      return config;
     },
   },
+  video: false,
 });
