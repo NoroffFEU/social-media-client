d697a740ddf49705fc1a6d3e0be1feaa984e48e4
bushrakalaji <91549377+bushrakalaji@users.noreply.github.com>
2022-11-13 23:04:37 +0100

    Update logout e2e test

--- cypress/e2e/logout.cy.js
+++ cypress/e2e/logout.cy.js
@@ -4,17 +4,17 @@ describe("Logout", () => {
     cy.clearLocalStorage();
   });
 
-  it("login with valid email and password", () => {
+  it("Logout", () => {
+    cy.visit("/");
+    cy.wait(2000);
     cy.get(".btn-outline-success:visible").contains("Login").click();
     cy.wait(2000);
     cy.get("#loginForm").get("h5").contains("Login");
     cy.wait(2000);
     cy.get("#loginForm").get("input#loginEmail").type("bkalaji33@noroff.no");
-    cy.wait(2000);
     cy.get("#loginForm").get("input#loginPassword").type("1234567890");
-    cy.wait(2000);
     cy.get("#loginForm").get(".btn-success").contains("Login").click();
-    cy.wait(2000);
+    cy.wait(4000);
     cy.url()
       .should("include", "profile")
       .then(() => {
@@ -22,6 +22,11 @@ describe("Logout", () => {
       });
     cy.wait(2000);
     cy.get("div.text-end").contains("Logout").click();
+    cy.url()
+      .should("not.include", "profile")
+      .then(() => {
+        expect(window.localStorage.getItem("token")).to.be.null;
+      });
     cy.get("#registerForm:visible");
   });
 });
