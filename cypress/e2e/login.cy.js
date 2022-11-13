4f54ac50640b246291bb13b99dbde38c55ae661e
Etten Ytten <honeygeeh27@gmail.com>
2022-11-13 10:04:53 +0100

    additional changes for testing

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -8,7 +8,7 @@ describe('Authentication', () => {
     cy.visit('/');
     cy.clearLocalStorage();
     cy.wait(1000);
-    cy.get('.btn-close:visible').click({ force: true });
+    cy.get('.btn-close:visible').click({ multiple: true });
     cy.get("button[data-auth='login']:visible")
       .contains('Login')
       .click({ force: true });
@@ -24,7 +24,7 @@ describe('Authentication', () => {
     cy.get("input[type='password']:visible")
       .should('exist')
       .type('cocomarcia1', { force: true }, { delay: 100 });
-    cy.get('.btn-success:visible').click({ force: true });
+    cy.get('.btn-success:visible').click({ multiple: true });
     cy.wait(3000);
     cy.then(
       () => expect(window.localStorage.getItem('profile')).to.not.be.null
@@ -37,7 +37,7 @@ describe('Authentication', () => {
     cy.visit('/');
     cy.wait(1000);
     cy.get('.btn-close:visible').click({ force: true });
-    cy.get("button[data-auth='login']:visible").click({ force: true });
+    cy.get("button[data-auth='login']:visible").click({ multiple: true });
     cy.wait(1500);
     cy.get('#loginForm').within(() => {
       cy.get("input[type='email']").type(
@@ -72,7 +72,7 @@ describe('Authentication', () => {
     cy.get("input[type='password']:visible")
       .should('exist')
       .type('cocomarcia1', { force: true }, { delay: 100 });
-    cy.get('.btn-success:visible').click({ force: true });
+    cy.get('.btn-success:visible').click({ multiple: true });
     cy.wait(3000);
     cy.then(() => expect(window.localStorage.getItem('profile')).to.exist);
     cy.then(() => expect(window.localStorage.getItem('token')).to.exist);
