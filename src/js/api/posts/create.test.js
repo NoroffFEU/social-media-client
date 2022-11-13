40e645281a03f6a1d5261fce4e4936ac1d3b5906
joakimlees <91544170+joakimlees@users.noreply.github.com>
2022-11-13 22:40:13 +0100

    added create post unit test jest

---
+++ src/js/api/posts/create.test.js
@@ -0,0 +1,30 @@
+import { createPost } from "./create";
+
+const TEST_POST = {
+  title: "this is title",
+  body: "boooooooddyyyy",
+  media: "media.url",
+  tags: "testTags",
+};
+
+function fetchOk() {
+  return Promise.resolve({
+    ok: true,
+    status: 200,
+    statusText: "OK",
+    json: () => Promise.resolve(TEST_POST),
+  });
+}
+
+describe("createPost", () => {
+  it("Creates a new item on the API", async () => {
+    global.fetch = jest.fn(() => fetchOk());
+    const post = await createPost(
+      TEST_POST.title,
+      TEST_POST.body,
+      TEST_POST.media,
+      TEST_POST.tags
+    );
+    expect(post).toEqual(TEST_POST);
+  });
+});
