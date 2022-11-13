98c2298d60f6bf2618d705f5ebe7fea851ec49bc
Karla <73695367+Noizenne@users.noreply.github.com>
2022-11-13 12:41:41 +0100

    More cypress testing

--- cypress/e2e/testing/login.cy.js
+++ cypress/e2e/testing/login.cy.js
@@ -24,4 +24,38 @@ describe('Login test', () => {
       () => expect(window.localStorage.getItem('profile')).to.not.be.null
     );
   });
+
+  it("Can't login with invalid email", () => {
+    cy.wait(500);
+    cy.get('#registerModal button')
+      .contains('Login')
+      .should('be.visible')
+      .click();
+    cy.wait(500);
+    cy.get('#loginForm input[name=email]')
+      .should('exist')
+      .type('k@stud.noroff.no');
+    cy.wait(1000);
+    cy.get('#loginForm input[name=password]').type('karla123');
+    cy.wait(500);
+    cy.get('#loginForm button').contains('Login').should('be.visible').click();
+    cy.then(() => expect(window.localStorage.getItem('profile')).to.be.null);
+  });
+
+  it("Can't login with invalid password", () => {
+    cy.wait(500);
+    cy.get('#registerModal button')
+      .contains('Login')
+      .should('be.visible')
+      .click();
+    cy.wait(500);
+    cy.get('#loginForm input[name=email]')
+      .should('exist')
+      .type('karla@stud.noroff.no');
+    cy.wait(1000);
+    cy.get('#loginForm input[name=password]').type('wongpsw');
+    cy.wait(500);
+    cy.get('#loginForm button').contains('Login').should('be.visible').click();
+    cy.then(() => expect(window.localStorage.getItem('profile')).to.be.null);
+  });
 });
