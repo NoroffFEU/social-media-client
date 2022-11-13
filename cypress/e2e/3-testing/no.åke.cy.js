35d267f02abc6048a6322863ed0e909d218ea7b7
Åke Ek <svein.ek@stud.noroff.no>
2022-11-13 18:31:35 +0100

    deletes a test for test

--- cypress/e2e/3-testing/no.åke.cy.js
+++
@@ -1,7 +0,0 @@
-describe("no.wikipedia.org", () => {
-    it("can search for Åke Ek", () => {
-      cy.visit("https://no.wikipedia.org");
-      cy.get("input#searchInput").type("Åke Ek{enter}", { delay: 500 });
-      cy.get('h1').contains("Åke")
-    })
-  })
