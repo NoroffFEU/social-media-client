c62dcb012808ad9e9691ebdf6135bc5041d90453
joakimlees <91544170+joakimlees@users.noreply.github.com>
2022-11-15 00:30:53 +0100

    made/finished e2e tests

--- cypress/e2e/3-custom/social-media-e2e-custom.cy.js
+++ cypress/e2e/3-custom/social-media-e2e-custom.cy.js
@@ -1,40 +1,53 @@
-describe("Social media app", () => {
+describe("Social media app - login user", () => {
   beforeEach(() => {
     cy.visit("http://127.0.0.1:5500/");
     cy.get("#registerForm .modal-header button.btn-close")
       .should("be.visible")
       .click();
+    cy.wait(2000);
     cy.get("#registerForm .modal-header button.btn-close")
       .should("be.visible")
       .click();
-    cy.get("#registerForm .modal-header button.btn-close")
-      .should("be.visible")
-      .click();
-    cy.wait(1000);
     cy.get(".text-end .btn-outline-success").contains("Login").click();
   });
 
   it("CAN login with valid email and password", () => {
-    cy.get("#loginEmail").should("exist").type("test@stud.noroff.no");
-    cy.get("#loginPassword").should("exist").type(`password123{enter}`);
+    cy.get("#loginEmail").should("exist").type("cyp@noroff.no");
+    cy.get("#loginPassword").should("exist").type(`overtime{enter}`);
+    cy.wait(2000);
+    cy.visit("http://127.0.0.1:5500/?view=profile");
+    cy.url().should("include", "profile");
   });
 
   it("CANNOT login with invalid email and password", () => {
     cy.get("#loginEmail").should("exist").type("testing.email.noroff@gmail.no");
     cy.get("#loginPassword").should("exist").type(`password123{enter}`);
+    cy.wait(2000);
+    cy.visit("http://127.0.0.1:5500/?view=profile");
+    cy.url().should("not.include", "profile");
+  });
+
+  it("CAN create a post on the API", () => {
+    cy.get("#loginEmail").should("exist").type("cyp@noroff.no");
+    cy.get("#loginPassword").should("exist").type(`overtime{enter}`);
+    cy.wait(2000);
+    cy.visit("http://127.0.0.1:5500/?view=post");
+    cy.get("#postTitle").should("exist").type("Title");
+    cy.get("#postTags").should("exist").type("tag");
+    cy.get("#postBody").should("exist").type(`post body{enter}`);
+    cy.get("#postForm .btn span").contains("Publish").click();
+    cy.wait(2000);
+    cy.get(".post-actions .btn-danger").contains("Delete").click();
+  });
+
+  it("CAN log out with logout button", () => {
+    cy.get("#loginEmail").should("exist").type("cyp@noroff.no");
+    cy.get("#loginPassword").should("exist").type(`overtime{enter}`);
+    cy.wait(2000);
+    cy.visit("http://127.0.0.1:5500/?view=profile");
+    cy.get(".text-end .btn-outline-warning").contains("Logout").click();
+    cy.wait(1000);
+    cy.visit("http://127.0.0.1:5500/?view=profile");
+    cy.url().should("not.include", "profile");
   });
 });
-
-/*
-describe("The login form VALIDATES user inputs CORRECTLY based on API restrictions", () => {
-  it("validates and logs in", () => {
-    cy.visit("http://127.0.0.1:5500/");
-    cy.get("button").contains("Login").should("be.visible").click();
-    cy.get("input[type='email']").should("exist").type("testtest@noroff.no");
-    cy.get("input[type='password']")
-      .should("exist")
-      .type(`passwordworking{enter}`);
-    cy.wait(2000);
-      cy.get("input[type=email]").should("exist").type("testtest@noroff.no");
-    cy.get("input[type=password]").should("exist").type(`passwordworking{enter}`);
-*/
