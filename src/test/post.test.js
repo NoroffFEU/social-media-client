01d268b31ec3b802ae04b0eae71b4ac8a5fbe163
unknown <Z20gab@gmail.com>
2022-11-14 16:12:37 +0100

    added post create test validation

--- src/test/post.test.js
+++ src/test/post.test.js
@@ -1,1 +1,16 @@
-export * from "./posts/index.js";
+import { createPost } from "../js/api/posts/create.js";
+import { login } from "../js/api/auth/login";
+
+const email = "testingss@noroff.no";
+const password = "asdf1234";
+
+const title = "Testing";
+const body = "testing with unit test";
+
+describe("The create item function creates a new item on the API", function () {
+  it("Can create a new item on the API", async () => {
+    await login(email, password);
+    const data = await createPost(title, body);
+    expect(data.title).toEqual(title);
+  });
+});
