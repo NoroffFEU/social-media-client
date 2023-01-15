8e8b3a3f1622d74a1f61c44ccd8b2ba324bdd4ea
Ethmane Didi <ousman.didibela@gmail.com>
2023-01-15 09:18:27 +0100

    Fix #1234: create item update test cases

---
+++ unit-tests/createitem.js
@@ -0,0 +1,6 @@
+/* eslint-disable no-unused-vars */
+// createItem.js
+export function createItem(name, description) {
+  // code to perform create item logic
+  return { id: 1, name: name, description: description };
+}
