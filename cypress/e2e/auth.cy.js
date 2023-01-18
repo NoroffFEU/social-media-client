899e3646c46980e2d5afacbbffd4bcdca2d6cf52
JoranEngelund <56642663+JoranEngelund@users.noreply.github.com>
2023-01-18 17:01:40 +0100

    Written auth e2e tests ->
    auth e2e test: Cannot view profile page
    
    auth e2e test: Cannot view post page
    
    auth e2e test: Can register with valid credentials
    
    auth e2e test: Can log in with the login form with valid credentials
    
    auth e2e test: Can log out with the logout button
    
    auth e2e test: Cannot submit the login form with invalid email and gets validation message in return
    
    auth e2e test: Cannot submit the login form with invalid password

---
+++ cypress/e2e/auth.cy.js
@@ -0,0 +1,103 @@
+const USER_NAME = "CypressJester";
+const USER_EMAIL = "crypressJest@noroff.no";
+const USER_PASSWORD = "12345678";
+const USER_AVATAR =
+  "https://images.pexels.com/photos/1769271/pexels-photo-1769271.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2";
+
+describe("Authorization", () => {
+  beforeEach(() => {
+    cy.wait(500);
+    cy.visit("http://127.0.0.1:5500");
+  });
+
+  it("Cannot view profile page", () => {
+    cy.visit("http://127.0.0.1:5500/?view=profile");
+  });
+
+  it("Cannot view post page", () => {
+    cy.visit("http://127.0.0.1:5500/?view=posts");
+  });
+
+  it("Can register with valid credentials", () => {
+    cy.get("form#registerForm").wait(500);
+    cy.get(`input[name="name"]`).type(USER_NAME);
+    cy.get(`input#registerEmail[name="email"]`).type(USER_EMAIL);
+    cy.get(`input#registerPassword[type="password"]`).type(USER_PASSWORD);
+    cy.get(`input#registerAvatar[name="avatar"]`).type(USER_AVATAR);
+    cy.get(`button[type="submit"]`).contains("Create Profile").click();
+  });
+
+  it("Can log in with the login form with valid credentials", () => {
+    cy.get("#registerModal button")
+      .contains("Login")
+      .should("be.visible")
+      .wait(500)
+      .click()
+      .wait(500);
+    cy.get(`input#loginEmail[name="email"]`).type(USER_EMAIL);
+    cy.get(`input#loginPassword[name="password"]`).type(USER_PASSWORD);
+    cy.get(`button[type="submit"]`).contains("Login").click();
+    cy.wait(2000);
+    cy.get(`button[type="button"][data-visible="loggedIn"]`)
+      .contains("Logout")
+      .click();
+  });
+
+  it("Can log out with the logout button", () => {
+    cy.get("#registerModal button")
+      .contains("Login")
+      .should("be.visible")
+      .wait(500)
+      .click()
+      .wait(500);
+    cy.get(`input#loginEmail[name="email"]`).type(USER_EMAIL);
+    cy.get(`input#loginPassword[name="password"]`).type(USER_PASSWORD);
+    cy.get(`button[type="submit"]`).contains("Login").click();
+    cy.wait(1000);
+    cy.get(`button[type="button"][data-visible="loggedIn"]`)
+      .contains("Logout")
+      .click();
+  });
+
+  it("Cannot submit the login form with invalid email and gets validation message in return", () => {
+    cy.get("#registerModal button")
+      .contains("Login")
+      .should("be.visible")
+      .wait(500)
+      .click()
+      .wait(500);
+
+    cy.get(`input#loginEmail[name="email"]`)
+      .type("cakeEmail@cake.no")
+      .then(($invalidEmail) => {
+        expect($invalidEmail[0].validationMessage).to.be.not.empty;
+      });
+
+    cy.get(`input#loginPassword[name="password"]`).type("few");
+
+    cy.get(`button[type="submit"]`).contains("Login").click({ force: true });
+  });
+
+  it("Cannot submit the login form with invalid password", () => {
+    cy.get("#registerModal button")
+      .contains("Login")
+      .should("be.visible")
+      .wait(500)
+      .click()
+      .wait(500);
+
+    cy.get(`input#loginEmail[name="email"]`)
+      .type(USER_EMAIL)
+      .then(($validEmail) => {
+        expect($validEmail[0].validationMessage).to.be.empty;
+      });
+
+    cy.get(`input#loginPassword[name="password"]`)
+      .type("few")
+      .then(($invalidPassword) => {
+        expect(`input[type="password"]`);
+      });
+    cy.wait(500);
+    cy.get(`button[type="submit"]`).contains("Login").click({ force: true });
+  });
+});
