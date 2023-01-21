2c2f1fcec1fc459a432c2b21b7093b5c55929f81
lhvk <77023253+lhvk@users.noreply.github.com>
2023-01-21 15:27:40 +0100

    reverted file back to original

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    supportFile: false,
-    setupNodeEvents() {
+    setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
