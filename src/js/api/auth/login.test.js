a3e8677bc1c3b5e696bd95c535ef6e57d1d77464
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2023-01-24 00:42:39 +0100

    creates unit testing for login function

---
+++ src/js/api/auth/login.test.js
@@ -0,0 +1,64 @@
+import { login } from "./login";
+
+/*
+Import from stackoverflow.com
+*/
+
+class LocalStorageMock {
+  constructor() {
+    this.store = {};
+  }
+
+  clear() {
+    this.store = {};
+  }
+
+  getItem(key) {
+    return this.store[key] || null;
+  }
+
+  setItem(key, value) {
+    this.store[key] = String(value);
+  }
+
+  removeItem(key) {
+    delete this.store[key];
+  }
+}
+
+global.localStorage = new LocalStorageMock();
+
+// The login function stores a token when provided with valid credentials
+
+const test_user_email = "mark.robinson@noroff.no";
+const test_user_password = "mark1234";
+const test_user_name = "mark_robinson";
+const test_access_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9";
+const test_profile = {
+  name: test_user_name,
+  email: test_user_email,
+  accessToken: test_access_token,
+};
+
+/**
+ * A function that fakes an approved API fetch call
+ */
+function fetchMockSuccess() {
+  return Promise.resolve({
+    status: 200,
+    ok: true,
+    statusText: "Approved",
+    json: () => Promise.resolve(test_profile),
+  });
+}
+
+describe("login", () => {
+  it("should store a token when provided valid email id and password", async () => {
+    global.fetch = jest.fn(() => fetchMockSuccess());
+    const result = await login(test_user_email, test_user_password);
+    expect(result).toEqual(test_profile);
+    expect(JSON.parse(global.localStorage.getItem("token"))).toEqual(
+      test_access_token
+    );
+  });
+});
