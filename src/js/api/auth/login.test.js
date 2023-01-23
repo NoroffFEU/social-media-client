9b13b03dad4dbdb91f71d3f3f2ca26099ee5dcd7
Rohit kumar Amdahl <rohit_designer@outlook.com>
2023-01-23 12:22:27 +0100

    create login.test.js

--- src/js/api/auth/login.test.js
+++ src/js/api/auth/login.test.js
@@ -1,1 +1,45 @@
 import { login } from './login.js';
+
+const MOCK_USER_DATA = {
+  name: 'testing',
+  email: 'testing@noroff.no',
+  banner: 'null',
+  avatar: 'null',
+  accessToken: 'test workflow',
+};
+
+const mockFetchSuccess = jest.fn().mockResolvedValue({
+  ok: true,
+  json: jest.fn().mockResolvedValue(MOCK_USER_DATA),
+});
+
+global.fetch = mockFetchSuccess;
+
+class LocalStorageMock {
+  constructor() {
+    this.storage = {};
+  }
+
+  clear() {
+    this.storage = {};
+  }
+
+  setItem(key, value) {
+    this.storage[key] = String(value);
+  }
+
+  getItem(key) {
+    return this.storage[key] || null;
+  }
+}
+
+global.localStorage = new LocalStorageMock();
+
+describe('userLogin', () => {
+  it('should login and store a token', async () => {
+    localStorage.clear();
+    await login('checking@stud.noroff.no', '');
+    const token = localStorage.getItem('token');
+    expect(token).not.toEqual('test workflow');
+  });
+});
