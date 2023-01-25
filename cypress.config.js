499fa02116a86c88a9c728f5551aa1d9a7a6fde9
Ethmane Didi <ousman.didibela@gmail.com>
2023-01-25 21:04:08 +0100

    Update cypress config to improve end-to-end tests

--- cypress.config.js
+++ cypress.config.js
@@ -1,14 +1,24 @@
-/* eslint-disable no-undef */
-module.exports = {
-  ...(on, config) => {
+require("dotenv").config();
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
+  modifyObstructiveCode: true,
+   ...(on, config) => {
     config.testFiles = "**/*.e2e.js";
     return config;
   },
 
   e2e: {
-    // eslint-disable-next-line no-unused-vars
+    
     setupNodeEvents(on, config) {
+      config.baseUrl = process.env.CYPRESS_BASE_URL;
+      config.env.API_URL = process.env.CYPRESS_API_URL;
+      config.env.API_TOKEN = process.env.Cypress_APT_TOKEN
+
       // implement node event listeners here
+
+      return config;
     },
   },
-};
+
+})
