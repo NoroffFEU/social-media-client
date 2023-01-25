9fe0fe2cf4bfede992e027c09f8f9f3671c36d26
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2023-01-26 00:18:39 +0100

    deleted testing function

--- cypress/e2e/3-testing/no.wikipedia.cy.js
+++
@@ -1,7 +0,0 @@
-describe("no.wikipedia.org", () => {
-  it("can search for Noroff", () => {
-    cy.visit("https://no.wikipedia.org");
-    cy.get("input#searchInput").type("Noroff{enter}", { delay: 500 });
-    cy.get("h1").contains("Noroff");
-  });
-});
