0ecd81c487c3f0f7b8e2984bce1fa5cbd968bd3e
sayeda-chattopadhyay <83353551+sayeda-chattopadhyay@users.noreply.github.com>
2023-01-26 00:19:24 +0100

    creates logout e2e func

---
+++ cypress/e2e/3-workflow-CA-testing/logout.cy.js
@@ -0,0 +1,30 @@
+describe("testing logout function", () => {
+  beforeEach(() => {
+    cy.visit("https://sayeda-chattopadhyay.github.io/social-media-client/");
+    cy.clearLocalStorage();
+    cy.wait(500);
+    cy.get("#registerForm .btn-close").click();
+    cy.get(`header button[data-auth="login"]`).click();
+    cy.wait(600);
+  });
+  it("log in with valid credentials and then logout", () => {
+    cy.get("#loginForm #loginEmail").type("anny.robinson@noroff.no");
+    cy.get("#loginForm #loginPassword").type("anny1234");
+    cy.get(`#loginForm button[type="submit"]`).click();
+    cy.url()
+      .should("include", "/?view=profile")
+      .then(() => {
+        expect(localStorage.getItem("token")).to.exist;
+        expect(localStorage.getItem("profile")).to.exist;
+      });
+    cy.wait(500);
+    cy.get('[data-auth="logout"]')
+      .click()
+      .then(() => {
+        expect(localStorage.getItem("token")).to.not.exist;
+        expect(localStorage.getItem("profile")).to.not.exist;
+      });
+    cy.wait(500);
+    cy.get("#registerForm .btn-close").click();
+  });
+});
