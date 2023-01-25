8cf319157a5ae4628fa56f931e8cba165396e4da
Rohit kumar Amdahl <rohit_designer@outlook.com>
2023-01-25 14:51:24 +0100

    editing code and passing login but invalid is not passing

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -14,20 +14,23 @@ describe('auth login', () => {
     cy.title().should('eq', 'Test Client');
     cy.get('#registerForm .btn-close:visible').click();
     cy.wait(2000);
+    cy.get("button[data-auth='login']:visible").click();
+    cy.wait(2500);
     cy.get("input[type='email']:visible")
       .type('rohit_kumar@stud.noroff.no')
       .should('have.value', 'rohit_kumar@stud.noroff.no');
     cy.get('#loginForm #loginPassword')
       .type('rohit123456')
       .should('have.value', 'rohit123456');
-    cy.get("button[data-auth='login']:visible").click();
-    cy.wait(2500);
+    cy.get('.btn-success:visible').click();
+    cy.wait(2900);
     cy.then(
       () => expect(window.localStorage.getItem('profile')).to.not.be.null
     );
     cy.then(() => expect(window.localStorage.getItem('token')).to.not.be.null);
     cy.url().should('include', 'profile');
   });
+
   it('cannot submit the login form with invalid credentials', () => {
     cy.visit('https://rohitamdahl.github.io/social-media-client-ca/', {
       delay: 1500,
@@ -37,8 +40,15 @@ describe('auth login', () => {
     cy.get('#registerForm .btn-close:visible').click();
     cy.wait(2000);
     cy.get("button[data-auth='login']:visible").click();
-    cy.get("input[type='email']:visible").type('tiger_user@stud.noroff.com');
-    cy.get('#loginForm #loginPassword').type('rohit123456');
+    cy.wait(2500);
+    cy.get("input[type='email']:visible")
+      .type('syss@syss.com')
+      .should('have.value', 'syss@syss.com');
+    cy.get('#loginForm #loginPassword')
+      .type('rohit123456')
+      .should('have.value', 'rohit123456');
+    cy.wait(2500);
+    cy.get("button[data-auth='login']:visible").click();
     cy.wait(2500);
     cy.then(() => expect(window.localStorage.getItem('profile')).to.be.null);
     cy.then(() => expect(window.localStorage.getItem('token')).to.be.null);
