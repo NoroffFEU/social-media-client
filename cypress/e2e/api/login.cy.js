07a615a84e2dfe8504cb203bad27ee47e55d9b59
Pilusjin <peter.ilusjin@stud.noroff.no>
2023-01-26 21:57:59 +0100

    added some e2e-test

---
+++ cypress/e2e/api/login.cy.js
@@ -0,0 +1,23 @@
+describe("Social Media login", () => {
+  beforeEach(() => {
+    cy.visit("./");
+  });
+
+  it("can log in or user Authenticated", () => {
+    cy.get("#registerModal button")
+      .contains("Login")
+      .should("be.visible")
+      .wait(1000)
+      .click();
+    cy.wait(1000);
+    const email = "enirose@noroff.no";
+    cy.get('input#loginEmail[name="email"]').type(`${email}`);
+    const password = "enirose123";
+    cy.get('input#loginPassword[name="password"]').type(`${password}`);
+    cy.wait(2000);
+    cy.get('button[type="submit"]').contains("Login").click({ force: true });
+    cy.wait(1000);
+    cy.then(() => expect(localStorage.getItem("token")).to.not.be.null);
+    cy.wait(1000);
+  });
+});
---
+++ cypress/e2e/api/logout.cy.js
@@ -0,0 +1,23 @@
+describe("Social Media logout", () => {
+  beforeEach(() => {
+    cy.visit("./");
+  });
+
+  it("can logout the user", () => {
+    cy.get("#registerModal button")
+      .contains("Login")
+      .should("be.visible")
+      .wait(1000)
+      .click();
+    cy.wait(1000);
+    const email = "enirose@noroff.no";
+    cy.get('input#loginEmail[name="email"]').type(`${email}`);
+    const password = "enirose123";
+    cy.get('input#loginPassword[name="password"]').type(`${password}`);
+    cy.get('button[type="submit"]').contains("Login").click({ force: true });
+    cy.wait(1000);
+    cy.then(() => expect(localStorage.getItem("token")).to.not.be.null);
+    cy.wait(3000);
+    cy.get("button").contains("Logout").click();
+  });
+});
