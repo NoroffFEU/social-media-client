6fa2f0dd52eacda2133a0c51e96d1b9a6a29a82c
Ethmane Didi <ousman.didibela@gmail.com>
2023-01-26 11:05:34 +0100

    Add Cypress configuration for end-to-end testing

--- cypress.config.js
+++ cypress.config.js
@@ -1,24 +1,32 @@
-require("dotenv").config();
-const { defineConfig } = require('cypress');
 
-module.exports = defineConfig({
-  modifyObstructiveCode: true,
-   ...(on, config) => {
+// eslint-disable-next-line no-unused-vars
+const cypress = require("cypress");
+
+
+module.exports = {
+  ...(_on, config) => {
+
+    config.baseUrl = "http://127.0.0.1:8080/";
     config.testFiles = "**/*.e2e.js";
+    config.env.apiKey = "abc123";
+    config.env.users = {
+      admin: {
+        username: "admin",
+        password: "password",
+      },
+      user: {
+        username: "user",
+        password: "password",
+      },
+    };
+    config.modifyObstructiveCode = true;
+
     return config;
   },
 
   e2e: {
-    
-    setupNodeEvents(on, config) {
-      config.baseUrl = process.env.CYPRESS_BASE_URL;
-      config.env.API_URL = process.env.CYPRESS_API_URL;
-      config.env.API_TOKEN = process.env.Cypress_APT_TOKEN
-
+    setupNodeEvents(_on, _config) {
       // implement node event listeners here
-
-      return config;
     },
   },
-
-})
+};
