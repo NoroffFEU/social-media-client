f9d72780883bb0fca15243eee078366b8a630d67
Ethmane Didi <ousman.didibela@gmail.com>
2023-01-26 11:08:03 +0100

    Added eslint config for Jest

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,16 @@
-// eslint-disable-next-line no-undef
 module.exports = {
-  setupFilesAfterEnv: ['jest-localstorage-mock'],
+  roots: ['<rootDir>/src'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testEnvironment: 'jsdom',
+  testPathIgnorePatterns: ['/node_modules/'],
+  testURL: 'http://localhost',
+  globals: {
+    'ts-jest': {
+      tsConfig: 'tsconfig.jest.json',
+    },
+  },
 };
