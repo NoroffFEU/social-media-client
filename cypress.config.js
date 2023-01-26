2b6f6073e5fd823d765d2cf416a80d7fd7c0baef
Ethmane Didi <ousman.didibela@gmail.com>
2023-01-26 11:05:46 +0100

    Add Cypress configuration for end-to-end testing

--- cypress.config.js
+++ cypress.config.js
@@ -1,22 +1,18 @@
-
-// eslint-disable-next-line no-unused-vars
-const cypress = require("cypress");
-
+const cypress = require('cypress');
 
 module.exports = {
   ...(_on, config) => {
-
-    config.baseUrl = "http://127.0.0.1:8080/";
-    config.testFiles = "**/*.e2e.js";
-    config.env.apiKey = "abc123";
+    config.baseUrl = 'http://127.0.0.1:8080/';
+    config.testFiles = '**/*.e2e.js';
+    config.env.apiKey = 'abc123';
     config.env.users = {
       admin: {
-        username: "admin",
-        password: "password",
+        username: 'admin',
+        password: 'password',
       },
       user: {
-        username: "user",
-        password: "password",
+        username: 'user',
+        password: 'password',
       },
     };
     config.modifyObstructiveCode = true;
