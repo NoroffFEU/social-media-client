194f03c709984cfe9a6ca0b10a263ea33911f652
Ethmane Didi <ousman.didibela@gmail.com>
2023-01-27 11:01:41 +0100

    Update cypress config - browser & base URL

--- cypress.config.js
+++ cypress.config.js
@@ -1,18 +1,16 @@
-const cypress = require('cypress');
-
 module.exports = {
-  ...(_on, config) => {
-    config.baseUrl = 'http://127.0.0.1:8080/';
-    config.testFiles = '**/*.e2e.js';
-    config.env.apiKey = 'abc123';
+  ...(on, config) => {
+    config.baseUrl = "http://127.0.0.1:8080/";
+    config.testFiles = "**/*.e2e.js";
+    config.env.apiKey = "abc123";
     config.env.users = {
       admin: {
-        username: 'admin',
-        password: 'password',
+        username: "admin",
+        password: "password",
       },
       user: {
-        username: 'user',
-        password: 'password',
+        username: "user",
+        password: "password",
       },
     };
     config.modifyObstructiveCode = true;
@@ -21,7 +19,7 @@ module.exports = {
   },
 
   e2e: {
-    setupNodeEvents(_on, _config) {
+    setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
