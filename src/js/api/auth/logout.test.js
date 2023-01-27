a5f74690a148aea9a496f5a02dd0024c71573cf8
Cdragvik <cndragvik@gmail.com>
2023-01-27 20:45:55 +0100

    Adjustments

--- src/js/api/auth/logout.test.js
+++ src/js/api/auth/logout.test.js
@@ -3,24 +3,20 @@ import { logout } from "./logout";
 // MockStorage
 
 export default class LocalStorageMock {
-    constructor() {
-      this.store = {};
-    }
-    clear() {
-      this.store = {};
-    }
-    getItem(key) {
-      return this.store[key] || null;
-    }
-    setItem(key, value) {
-      this.store[key] = String(value);
-    }
-    removeItem(key) {
-      delete this.store[key];
-    }
+  constructor() {
+    this.store = {};} 
+  clear() {
+    this.store = {};}
+  getItem(key) {
+    return this.store[key] || null;}
+  setItem(key, value) {
+    this.store[key] = String(value);}
+  removeItem(key) {
+    delete this.store[key];}
 }
 
 global.localStorage = new LocalStorageMock();
+
 
 
 // The logout function clears the accessToken from localStorage
