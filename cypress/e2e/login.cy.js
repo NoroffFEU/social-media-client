bbc010735330e1bcd5009b98671fe72e1b93cf9e
VaNomad <sjur.hassel@gmail.com>
2023-01-27 16:33:07 +0000

    Adds e2e login test

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,14 +1,23 @@
 
-describe("Login Test", () => {
-  it("Login with valid credentials", () => {
-    const email = "sjur.hassel@gmail.com"
-    const pswrd = "@SjurHassel2023"
+const USER_NAME = "sjur"
+const USER_EMAIL = "sjur@noroff.no"
+const USER_PASSWORD = "12345678"
 
-    cy.visit("/")
-    cy.title().should("contain", "Client")
-    cy.title().should("eq", "Test Client")
-    cy.get("input#loginEmail[name='email']").type(`${email}`)
-    cy.get("input#loginPassword[name='password']").type(`${pswrd}`)
-    cy.get("button[type='submit']").contains("Login").click()
+describe("Login Test", () => {
+  beforeEach(() => {
+    cy.visit("https://vanomad.github.io/social-media-client-ca/");
+    cy.wait(1000);
+    cy.clearLocalStorage();
   })
-})
+  it("Login with valid credentials", () => {
+    cy.title().should("eq", "Test Client");
+    cy.get("#registerModal button").should("be.visible").contains("Login").click().wait(1000);
+    cy.get("input#loginEmail[name='email']").type(USER_EMAIL);
+    cy.get("input#loginPassword[name='password']").type(USER_PASSWORD);
+    cy.get("button[type='submit']").should("be.visible").contains("Login").click({ force: true });
+    cy.wait(2000);
+    cy.then(() => {
+      expect(localStorage.getItem("token")).to.exist;
+    });
+  });
+});
