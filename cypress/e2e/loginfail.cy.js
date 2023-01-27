7917d79cbfbdc8be31c8d10cec1916e6bbe72cb7
oBraenna91 <95278401+oBraenna91@users.noreply.github.com>
2023-01-27 17:12:06 +0100

    Update loginfail.cy.js

--- cypress/e2e/loginfail.cy.js
+++ cypress/e2e/loginfail.cy.js
@@ -45,7 +45,7 @@ describe("LogInFailWithWrongPassword", () => {
             expect(`input[type="password"]`);
         });
 
-        cy.wait(500);
+        cy.wait(1000);
         cy.get(`button[type="submit"]`).contains("Login").click({ force: true });
     });
 });
