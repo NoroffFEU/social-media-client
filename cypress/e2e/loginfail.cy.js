05da2a0f836d263d4770362808b05a9a89b6678c
oBraenna91 <95278401+oBraenna91@users.noreply.github.com>
2023-01-27 17:13:50 +0100

    Update loginfail.cy.js

--- cypress/e2e/loginfail.cy.js
+++ cypress/e2e/loginfail.cy.js
@@ -11,15 +11,12 @@ describe("LogInFailWithWrongMail", () => {
         .wait(1000)
         .click()
         .wait(1000);
-
         cy.get(`input#loginEmail[name="email"]`)
         .type("test@test.test")
         .then(($invalidEmail) => {
             expect($invalidEmail[0].validationMessage).to.be.not.empty;
         });
-
         cy.get(`input#loginPassword[name="password"]`).type(password);
-
         cy.get(`button[type="submit"]`).contains("Login").click({ force: true });
     });
 });
@@ -44,7 +41,6 @@ describe("LogInFailWithWrongPassword", () => {
         .then(($invalidPassword) => {
             expect(`input[type="password"]`);
         });
-
         cy.wait(1000);
         cy.get(`button[type="submit"]`).contains("Login").click({ force: true });
     });
