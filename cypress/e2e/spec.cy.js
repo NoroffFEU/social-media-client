8756b9677fb7cf220b5615da75db756c9849408b
Ethmane Didi <ousman.didibela@gmail.com>
2023-01-27 18:16:21 +0100

    Removed unused files for better organization

--- cypress/e2e/spec.cy.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable no-undef */
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
--- cypress/integration/support/e2e.js
+++
@@ -1,4 +0,0 @@
-// e2e.js
-
-// This file is where you can configure and set up Cypress
-// before running the tests.
