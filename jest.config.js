cfc83101a2df03cb4323af13923f02bb642076ef
Ethmane Didi <ousman.didibela@gmail.com>
2023-01-27 11:02:24 +0100

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,9 @@
 module.exports = {
-  moduleFileExtensions: ['js', 'jsx', 'json', 'ts', 'tsx', 'js'],
   testEnvironment: 'node',
   testMatch: ['**/*.test.js'],
-  testMatch: ['**/__tests__/**/*.js?(x)', '**/?(*.)+(spec|test).js?(x)'],
   testPathIgnorePatterns: ['/node_modules/'],
   transform: {
-    '^.+\\.jsx?$': 'babel-jest',
+    '^.+.jsx?$': 'babel-jest',
   },
   collectCoverage: true,
   coverageDirectory: 'coverage',
