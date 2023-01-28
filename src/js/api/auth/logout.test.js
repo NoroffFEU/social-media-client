2717c0c4f6a6476812e36d628feadfc38a5ba1db
VaNomad <sjur.hassel@gmail.com>
2023-01-28 17:27:07 +0000

    Adds logout unit-test file

---
+++ src/js/api/auth/logout.test.js
@@ -0,0 +1,12 @@
+import { logout } from "./logout";
+import { LocalStorageMock } from "../../storage/mock";
+
+global.localStorage = new LocalStorageMock();
+
+describe("Logout Test", () => {
+  it("The logout function clears the token from browser storage", () => {
+    logout();
+    expect(localStorage.getItem("token")).toEqual(null);
+    expect(localStorage.getItem("profile")).toEqual(null);
+  })
+})
