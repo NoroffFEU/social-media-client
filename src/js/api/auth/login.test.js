1ea7c27b8630adfad13fa5c7d2a2eaef2664aab0
Ingsy <ingeborg_mellingen89@hotmail.com>
2023-03-21 11:40:13 +0100

    unit-testing mock-fail-test

--- src/js/api/auth/login.test.js
+++ src/js/api/auth/login.test.js
@@ -1,6 +1,8 @@
 import { LocalStorageMock } from "../../storage/LocalStorage.mock";
 import { login } from "./login";
 
+
+
 const Credentials = { email: "ingeborg@noroff.no", password: "23456" };
 const ACCESSTOKEN = "ABCDE";
 const PROFILE = {
@@ -9,9 +11,14 @@ const PROFILE = {
     accessToken: ACCESSTOKEN,
 };
 
+//expect failure
+const FAIL_EMAIL_TEST = "ingeborg@hotmail.com"
+
+
 global.localStorage = new LocalStorageMock();
 
-function validCredentials() {
+//expect test-pass
+function validFetch() {
     return Promise.resolve({
         ok: true,
         status: 200,
@@ -20,14 +27,33 @@ function validCredentials() {
     });
 }
 
+//expect test-fail
+function failFetch(statusCode = 404, status = "Not Found") {
+    return Promise.resolve({
+        ok: false,
+        statusCode,
+        status,
+    });
+}
+
 
 describe("login", () => {
     it("logs in user with email and password, and stores a token in localstorage", async () => {
         global.localStorage.clear();
-        global.fetch = jest.fn(() => validCredentials());
+        global.fetch = jest.fn(() => validFetch());
         await login(Credentials.email, Credentials.password);
 
         expect(JSON.parse(global.localStorage.getItem("token"))).toBe(ACCESSTOKEN);
         expect(JSON.parse(global.localStorage.getItem("profile"))).toEqual(PROFILE);
     });
-});
+    it("Returns HTTP 401 error when user has wrong credentials", async () => {
+        global.fetch = jest.fn(() => failFetch(401, "Not authorized"));
+        await expect(login(FAIL_EMAIL_TEST)).rejects.toThrow(new Error()
+        );
+    });
+    it("Returns HTTP 404 error when no data is set", async () => {
+        global.fetch = jest.fn(() => failFetch(400, ""));
+        await expect(login()).rejects.toThrow(new Error()
+        );
+    });
+})
