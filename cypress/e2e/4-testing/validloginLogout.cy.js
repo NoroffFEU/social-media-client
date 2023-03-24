7ef2ab1927fd5d8718e4e8a9a905678b2040f4b3
Ingsy <ingeborg_mellingen89@hotmail.com>
2023-03-24 10:40:55 +0100

    cypress coonfig and changes

--- cypress/e2e/4-testing/validloginLogout.cy.js
+++ cypress/e2e/4-testing/validloginLogout.cy.js
@@ -1,18 +1,43 @@
 describe("valid login", () => {
-    it("signup and login user", () => {
-        cy.visit("https://nf-api.onrender.com/api/v1/social/auth/login")
+    it("user can log in with valid credentials", () => {
+        cy.visit("https://github.com/Ingsy/social-media-client.git");
 
-        cy.get("input[name="email"]").type("ingeborg@noroff.no")
-        cy.get("input[name="password"]").type("12345678")
-        cy.get("#signup-button").click()
+        cy.wait(1000);
 
-        cy.location("pathname").should("eq", "/login")
+        cy.get(".modal-footer button[data-bs-target='#loginModal']", {
+            timeout: 3000,
+        })
 
-        cy.get("input[name="email"]").type("ingeborg@noroff.no")
-        cy.get("input[name="password"]").type("12345678")
-        cy.get("#login-button").click()
+            .should("be.visible")
+            .click();
+        cy.get("#registerModal", { timeout: 3000 }).should("not.be..visible");
 
-        cy.location("pathname").should("eq", "/profile")
+        cy.wait(1000);
+
+        cy.get("#loginModal", { timeout: 3000 }).should("be.visible");
+        cy.get("input#loginEmail", { timeout: 4000 }).type(Cypress.env("user_password"), { delay: 200 });
+
+        cy.get("#loginModal .modal-footer button[type='submit']", { timeout: 4000 })
+            .should("be.visible")
+            .click();
+
+        cy.intercept(
+            "https://ingsy.github.io/social-media-client/?view=profile&name=Erik")
+            .as("profileView");
+        cy.wait("@profileView")
+
+        cy.get("header button[data-auth='logut']", { timeout: 4000 })
+            .should("be.visible")
+            .click();
+
+        cy.wait(1000);
+        cy.get("registerModal", { timeout: 4000 }).should("be.visible");
+
     })
 })
+
+
+
+
+
 
