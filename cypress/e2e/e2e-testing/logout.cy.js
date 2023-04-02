10c014bb7bef05872f25eed583d0e0ad7aa32f80
Maria Rolstad-Martinez <mariarolstad-martinez@Marias-MBP.home>
2023-04-02 03:51:50 +0200

    modified

--- cypress/e2e/e2e-testing/logout.cy.js
+++ cypress/e2e/e2e-testing/logout.cy.js
@@ -13,10 +13,11 @@ describe("User log out functionality", () => {
       cy.wait(1000);
       cy.get("input[type='email']:visible")
         .should("exist")
-        .type("user@example.com");
-      cy.get("input[type='password']:visible").should("exist").type("12345678");
+        .type("user@example.com"); // Make sure this email is correct and registered
+      cy.get("input[type='password']:visible").should("exist").type("12345678"); // Make sure this password is correct
       cy.get(".btn-success:visible").click({ multiple: true });
       cy.wait(3000);
+      // The following line is where the error occurs. Make sure the login process is working correctly.
       cy.then(() => expect(window.localStorage.getItem("token")).to.not.be.null);
       cy.then(
         () => expect(window.localStorage.getItem("profile")).to.not.be.null
@@ -26,3 +27,4 @@ describe("User log out functionality", () => {
       cy.then(() => expect(window.localStorage.getItem("profile")).to.be.null);
     });
   });
+  
