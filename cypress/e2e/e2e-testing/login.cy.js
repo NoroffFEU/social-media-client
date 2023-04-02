362ba949a24caa326d1498ff271a624ea8b32cd9
Maria Rolstad-Martinez <mariarolstad-martinez@Marias-MBP.home>
2023-04-02 02:43:30 +0200

    modified file

--- cypress/e2e/e2e-testing/login.cy.js
+++ cypress/e2e/e2e-testing/login.cy.js
@@ -1,19 +1,20 @@
-describe("User login and profile access", () => {
+describe("user authentication", () => {
     beforeEach(() => {
       cy.visit("/");
       cy.clearLocalStorage();
     });
   
-    it("Allows the user to log in and access their profile", () => {
+    it("allows user to log in successfully", () => {
+      cy.visit("/");
       cy.wait(1000);
       cy.get(".btn-close:visible").click();
       cy.wait(1000);
       cy.get("button[data-auth='login']:visible").click();
       cy.wait(1000);
       cy.get("input[type='email']:visible")
-        .should("exist")
+        .should("be.visible")
         .type("m.slagsvold@gmail.com");
-      cy.get("input[type='password']:visible").should("exist").type("12345678");
+      cy.get("input[type='password']:visible").should("be.visible").type("12345678");
       cy.get(".btn-success:visible").click();
       cy.wait(3000);
       cy.then(() => expect(window.localStorage.getItem("token")).to.not.be.null);
@@ -22,16 +23,17 @@ describe("User login and profile access", () => {
       );
     });
   
-    it("valid credentials for email and password", () => {
+    it("checks email and password validation", () => {
+      cy.visit("/");
       cy.wait(1000);
       cy.get(".btn-close:visible").click();
       cy.wait(1000);
       cy.get("button[data-auth='login']:visible").click();
       cy.wait(1000);
       cy.get("input[type='email']:visible")
-        .should("exist")
+        .should("be.visible")
         .type("m.slagsvold@gmail.com");
-      cy.get("input[type='password']:visible").should("exist").type("1234");
+      cy.get("input[type='password']:visible").should("be.visible").type("1234");
       cy.get(".btn-success:visible").click();
       cy.wait(3000);
       cy.then(() => expect(window.localStorage.getItem("token")).to.not.be.null);
