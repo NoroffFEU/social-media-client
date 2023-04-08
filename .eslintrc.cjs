1c0c2d5da08cc495faff713b1e14b819f09a7b9a
Kent Erik <kent.erik.hole@outlook.com>
2023-04-08 18:54:17 +0200

    adding eslintrc file

---
+++ .eslintrc.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": "eslint:recommended",
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "lastest",
+        "sourceType": "module"
+    },
+    "rules": {
+    }
+}
