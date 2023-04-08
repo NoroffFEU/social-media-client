bf61f45e8e816081bdf417ffdba63faf346aba2e
Kent Erik <kent.erik.hole@outlook.com>
2023-04-08 20:56:11 +0200

    adding cypress to file

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,16 @@ module.exports = {
             "plugins": ["jest"],
             "extends": ["plugin:jest/recommenden"],
             "rules": {"jest/prefer-expect-assertions": "off" }
+        }, 
+        {
+            "files": ["**/*.cy.js"],
+            "env": { "cypress/globals": true},
+            "plugins": ["cypress"],
+            "extends": ["plugin:cypress/recommended"],
+            "rules": {
+                "cypress/no-unnecessary-waiting": "off",
+                "no-unused-vars": "off"
+            }
         }
     ],
     "parserOptions": {
@@ -19,4 +29,5 @@ module.exports = {
     },
     "rules": {
     }
+    
 }
