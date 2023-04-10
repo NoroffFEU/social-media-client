6eea23039c2312a79dae46ba0f11e0e67f642623
Kent Erik <kent.erik.hole@outlook.com>
2023-04-10 16:00:40 +0200

    work on cypress

--- cypress/e2e/1-getting-started/exit&login.cy.js
+++ cypress/e2e/1-getting-started/exit&login.cy.js
@@ -2,7 +2,7 @@ describe("http://localhost:5173/", () => {
     it("can search for Noroff", () => {
       cy.visit("http://localhost:5173/");
       cy.wait(400)
-      cy.get('#registerModal').contains('Close').click()
-      cy.get('#')
+      cy.get('#registerForm').reset()
+      
     })
   })
--- cypress/e2e/1-getting-started/formlogin.cy.js
+++ cypress/e2e/1-getting-started/formlogin.cy.js
@@ -3,8 +3,9 @@ describe("http://localhost:5173/", () => {
       cy.visit("http://localhost:5173/");
       cy.wait(400)
       cy.get('#registerModal').contains('Login').click()
-      cy.wait(200)
-      cy.get('input#loginEmail').type('test12@noroff.no', {delay: 100})
-      cy.get('input#password').type('testtest1', {delay: 100})
+      cy.wait(500)
+      cy.get('input#loginEmail').type('type{enter}')
+      cy.get('input#loginPassword').type('test1234')
+      cy.get('#loginForm').submit()
     })
   })
