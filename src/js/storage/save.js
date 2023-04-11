81eb377d22b4e0e4a91ff7d2338603bd33a07fdd
Joakim Tveter <joakim@tveter.net>
2023-04-11 22:41:51 +0200

    Fixed double quotemarks in local storage

--- src/js/storage/save.js
+++ src/js/storage/save.js
@@ -1,5 +1,4 @@
 export const save = (key, value) => {
-  console.log("save", key, typeof value, value)
   if (typeof value !== "string") {
     localStorage.setItem(key, JSON.stringify(value))
   } else {
