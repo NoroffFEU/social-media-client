d31d45d41424cbd08f80ac0ab5c3b56fe48ca7f5
lkmelberg <100446631+lkmelberg@users.noreply.github.com>
2023-04-14 21:12:26 +0200

    unit testing for login function

---
+++ src/js/test/localStorageMock.js
@@ -0,0 +1,25 @@
+export const localStorageMock = (function () {
+  let store = {};
+
+  return {
+    getItem(key) {
+      return store[key];
+    },
+
+    setItem(key, value) {
+      store[key] = value;
+    },
+
+    clear() {
+      store = {};
+    },
+
+    removeItem(key) {
+      delete store[key];
+    },
+
+    getAll() {
+      return store;
+    },
+  };
+})();
