26768929d658cbd4150915e1195850aae08b3499
fof002 <80104002+fof002@users.noreply.github.com>
2023-04-15 08:14:39 +0200

    cypress

--- cypress/e2e/3-testing/no.wikipedia.cy.js
+++
@@ -1,7 +0,0 @@
-describe('no.wikipedia.org', () => {
-  it('can search for Noroff', () => {
-    cy.visit('http://127.0.0.1:5500/index.html');
-    cy.get('input#passwordInput').type('{Pasvikveien18enter}', { delay: 500 });
-    cy.get('input#loginInput').type('bernt22@noroff.no{enter}', { delay: 500 });
-  });
-});
