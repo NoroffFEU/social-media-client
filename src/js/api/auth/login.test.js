a108ce08d450fc45754bdc445bfdfd29bb1f32e7
Yggdrass <78912844+Yggdrass@users.noreply.github.com>
2023-04-15 17:12:36 +0200

    Created tests within these files to check the function for both successful and 404 errors, then ran those tests to check if they all worked properly.

--- src/js/api/auth/login.test.js
+++ src/js/api/auth/login.test.js
@@ -1,3 +1,6 @@
+
+
+
 import { login } from "./login";
 
 const global = globalThis;
@@ -18,10 +21,43 @@ const mockFetchSuccess = jest.fn().mockResolvedValue({
 global.fetch = mockFetchSuccess;
 global.localStorage = localStorageMock;
 
+
+const MOCK_TOKEN = localStorageMock.setItem();
+
+
+function fetchSuccess() {
+  return Promise.resolve({
+    ok: true,
+    status: 200,
+    statusText: "OK",
+    json: () => Promise.resolve(MOCK_TOKEN)
+  })
+}
+
+
+function fetchFailure(status = 404, statusText = "Not Found") {
+  return Promise.resolve({
+    ok: false,
+    status,
+    statusText
+  })
+}
+
+
 describe("login", () => {
-  it("should fetch and store an access token", async () => {
+  it("should fetch and store a valid access token", async () => {
     const data = await login(mockEmail, mockPassword);
     localStorage.setItem("token", data.token);
     expect(localStorage.getItem("token")).toBe(data.token);
+  });
+  it("Returns a valid item object when the token is a valid object", async () => {
+    global.fetch = jest.fn(() => fetchSuccess());
+    const mockItem = await localStorageMock.getItem(1);
+    expect(mockItem).toEqual(MOCK_TOKEN);
+  });
+  it("Returns undefined when an HTTP 404 error is recieved", async () => {
+    global.fetch = jest.fn(() => fetchFailure());
+    const mockItem = await localStorageMock.getItem(1);
+    expect(mockItem).toEqual(undefined)
   });
 });
--- src/js/api/auth/logout.test.js
+++ src/js/api/auth/logout.test.js
@@ -12,11 +12,44 @@ const global = globalThis;
 
 global.localStorage = localStorageMock;
 
+const MOCK_TOKEN = localStorageMock.setItem();
+
+
+function fetchSuccess() {
+  return Promise.resolve({
+    ok: true,
+    status: 200,
+    statusText: "OK",
+    json: () => Promise.resolve(MOCK_TOKEN)
+  })
+}
+
+
+function fetchFailure(status = 404, statusText = "Not Found") {
+  return Promise.resolve({
+    ok: false,
+    status,
+    statusText
+  })
+}
+
+
+
 describe("logout", () => {
   it("clears the token from browser storage", async () => {
     const mockToken = "token";
     localStorage.removeItem("token", mockToken);
     logout();
     expect(localStorage.getItem("token")).toBeUndefined();
+  });
+  it("Returns a valid item object when the token is a valid object", async () => {
+    global.fetch = jest.fn(() => fetchSuccess());
+    const mockItem = await localStorageMock.getItem(1);
+    expect(mockItem).toEqual(undefined);
+  });
+  it("Returns undefined when an HTTP 404 error is recieved", async () => {
+    global.fetch = jest.fn(() => fetchFailure());
+    const mockItem = await localStorageMock.getItem(1);
+    expect(mockItem).toEqual(MOCK_TOKEN)
   });
 });
