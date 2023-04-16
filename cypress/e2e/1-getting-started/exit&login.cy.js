701700788873a4eb52eab4c306b6a5aabdb8ad4a
Kent Erik <kent.erik.hole@outlook.com>
2023-04-16 10:06:45 +0200

    removing and add new product test

--- cypress/e2e/1-getting-started/exit&login.cy.js
+++
@@ -1,8 +0,0 @@
-describe("http://localhost:5173/", () => {
-    it("can search for Noroff", () => {
-      cy.visit("http://localhost:5173/");
-      cy.wait(400)
-      cy.get('#registerForm').reset()
-      
-    })
-  })
