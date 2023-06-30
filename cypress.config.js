340581d8f200963db51217d01cde22749c683266
Anastassia Sommer <anastassia.sommer@gmail.com>
2023-06-30 20:34:21 +0200

    Create cypress.config.js

---
+++ cypress.config.js
@@ -0,0 +1,13 @@
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      config.env = {
+        ...process.env,
+        ...config.env,
+      };
+      return config;
+    },
+  },
+});
