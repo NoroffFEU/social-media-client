a9ca6ab640715d2ebe629b902e5cc158eed0d4dc
Anastassia Sommer <anastassia.sommer@gmail.com>
2023-06-30 15:25:06 +0200

    e2e login test completed

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,19 +1,61 @@
-describe('Login with valid credentials', () => {
+describe('Authentication', () => {
   beforeEach(() => {
-    cy.clearLocalStorage();
     cy.visit('/');
-    cy.wait(500);
+    cy.clearLocalStorage();
   });
 
-  it('logs in user', () => {
-    cy.get('#registerForm button')
-      .contains('Login')
-      .should('be.visible')
-      .click();
-    cy.wait(500);
-    cy.get('input#loginEmail').type('nastiksvastik10@stud.noroff.no');
-    cy.get('input#loginPassword').type('12345678');
-    cy.wait(500);
-    cy.get('#loginForm button').contains('Login').should('be.visible').click();
+  it('can login', () => {
+    cy.visit('/');
+    cy.wait(1000);
+    cy.get('.btn-close:visible').click({ force: true });
+    cy.get("button[data-auth='login']:visible").click({ force: true });
+    cy.wait(1500);
+    cy.get("input[type='email']:visible")
+      .should('exist')
+      .type('nastiksvastik10@stud.noroff.no');
+    cy.get("input[type='password']:visible").should('exist').type('12345678');
+    cy.get('.btn-success:visible').click({ force: true });
+    cy.wait(3000);
+    cy.then(
+      () => expect(window.localStorage.getItem('profile')).to.not.be.null
+    );
+    cy.then(() => expect(window.localStorage.getItem('token')).to.not.be.null);
+    cy.url().should('include', 'profile');
+  });
+
+  it('Validates email input', () => {
+    cy.visit('/');
+    cy.wait(1000);
+    cy.get('.btn-close:visible').click({ force: true });
+    cy.get("button[data-auth='login']:visible").click({ force: true });
+    cy.wait(1500);
+    cy.get("input[type='email']:visible")
+      .should('exist')
+      .type('fake@email.com');
+    cy.get("input[type='password']:visible")
+      .should('exist')
+      .type('fakepassword');
+    cy.get('.btn-success:visible').click({ force: true });
+    cy.wait(3000);
+    cy.then(() => expect(window.localStorage.getItem('profile')).to.be.null);
+    cy.then(() => expect(window.localStorage.getItem('token')).to.be.null);
+    cy.url().should('not.include', 'profile');
+  });
+
+  it('Validates password', () => {
+    cy.visit('/');
+    cy.wait(1000);
+    cy.get('.btn-close:visible').click();
+    cy.get("button[data-auth='login']:visible").click({ force: true });
+    cy.wait(1500);
+    cy.get("input[type='email']:visible")
+      .should('exist')
+      .type('nastiksvastik10@stud.noroff.no');
+    cy.get("input[type='password']:visible").should('exist').type('12345678');
+    cy.get('.btn-success:visible').click({ force: true });
+    cy.wait(3000);
+    cy.then(() => expect(window.localStorage.getItem('profile')).to.exist);
+    cy.then(() => expect(window.localStorage.getItem('token')).to.exist);
+    cy.url().should('include', 'profile');
   });
 });
