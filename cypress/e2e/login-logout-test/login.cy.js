3a4602566ca3e937be9b0312e8f9c51e1a4af209
Eirik Michielsen <eirik.michielsen@gmail.com>
2023-08-13 22:21:11 +0200

    Added test folder with login test

---
+++ cypress/e2e/login-logout-test/login.cy.js
@@ -0,0 +1,29 @@
+// cypress/integration/login_spec.js
+
+describe('Login Test', () => {
+    beforeEach(() => {
+        cy.visit('http://localhost:8080'); 
+    });
+
+    it('should login using the modal form', () => {
+        cy.get("#registerModal").should("be.visible").find("[data-auth='login']").click();
+        cy.get("#registerModal").invoke('hide'); 
+        
+
+    
+
+         //Wait for modal to close
+         cy.get(1000);
+
+        // Type in email and password
+        cy.get('#loginEmail').type('test@stud.noroff.no');
+        cy.get('#loginPassword').type('cypress-test');   
+
+        // Submitting the form
+        cy.get("#loginForm").submit();
+
+        // Checking if the URL includes dashboard after login
+        cy.url().should("include", "/dashboard");
+    });
+});
+
