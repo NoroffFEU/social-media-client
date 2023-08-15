4fceee130b8c2329a1b48b997e5591111d97b6e3
Eirik Michielsen <eirik.michielsen@gmail.com>
2023-08-15 13:49:10 +0200

    Added login credentials test

--- cypress/e2e/tests-social-media-client/login-logout.cy.js
+++ cypress/e2e/tests-social-media-client/login-logout.cy.js
@@ -5,7 +5,7 @@ describe('Login Test', () => {
         cy.visit("http://localhost:8080"); 
     });
 
-    it('should login using the modal form', () => {
+    it('should login using the modal form and log out afterwards', () => {
         cy.get("#registerModal").should("be.visible").find("[data-auth='login']").click();
         cy.get("#registerModal").invoke("hide"); 
         
@@ -28,6 +28,14 @@ describe('Login Test', () => {
 
         // Checking if the URL includes dashboard after login
         cy.get("[data-visible='loggedIn']").should("be.visible");
+        
+        // This is only so that the person running the test in Cypress can acctualy see the profil dashboard before cypress go to the next step of the testing
+        cy.wait(1000);
+
+        cy.get("[data-auth='logout']").click();
+
+
+        cy.get("[data-visible='loggedIn']").should("not.be.visible");
 
     });
 });
