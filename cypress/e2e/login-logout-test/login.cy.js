1ae2a437c987a18d8f109e2bc64f49614aee86bd
Eirik Michielsen <eirik.michielsen@gmail.com>
2023-08-15 12:41:13 +0200

    Cypress test success - fixed username error

--- cypress/e2e/login-logout-test/login.cy.js
+++ cypress/e2e/login-logout-test/login.cy.js
@@ -2,28 +2,33 @@
 
 describe('Login Test', () => {
     beforeEach(() => {
-        cy.visit('http://localhost:8080'); 
+        cy.visit("http://localhost:8080"); 
     });
 
     it('should login using the modal form', () => {
         cy.get("#registerModal").should("be.visible").find("[data-auth='login']").click();
-        cy.get("#registerModal").invoke('hide'); 
+        cy.get("#registerModal").invoke("hide"); 
         
 
     
 
          //Wait for modal to close
-         cy.get(1000);
+         cy.wait(1000);
+
 
         // Type in email and password
-        cy.get('#loginEmail').type('test@stud.noroff.no');
-        cy.get('#loginPassword').type('cypress-test');   
+        cy.get("#loginEmail").type("test@stud.noroff.no");
+        cy.get("#loginPassword").type("cypress-test");
+        
+      
 
         // Submitting the form
         cy.get("#loginForm").submit();
 
+
         // Checking if the URL includes dashboard after login
-        cy.url().should("include", "/dashboard");
+        cy.get("[data-visible='loggedIn']").should("be.visible");
+
     });
 });
 
