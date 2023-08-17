d3da48503b94459bfbf047f90b912a64ccdd2feb
Emilyrf <104759694+Emilyrf@users.noreply.github.com>
2023-08-17 15:19:16 +0200

    Tests logout remove token with jest

--- src/js/api/auth/logout.test.js
+++ src/js/api/auth/logout.test.js
@@ -1,1 +1,24 @@
-// import { logout } from "./logout";
+import { logout } from './logout';
+import localStorageMock from '../../mocks/localStorage.mock';
+
+const fakeToken = 'token';
+const fakeProfile = {
+  name: 'Student Name',
+  email: 'student@stud.noroff.no',
+};
+
+beforeAll(() => {
+  global.localStorage = new localStorageMock();
+});
+
+describe('logout function', () => {
+  it('should clear the token from browser storage', () => {
+    localStorage.setItem('token', JSON.stringify(fakeToken));
+    localStorage.setItem('profile', JSON.stringify(fakeProfile));
+
+    logout();
+
+    expect(localStorage.getItem('token')).toBeNull();
+    expect(localStorage.getItem('profile')).toBeNull();
+  });
+});
