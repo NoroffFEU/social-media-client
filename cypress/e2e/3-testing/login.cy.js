1456f90e531c302f393d0a8244d59f3429085697
corrado1982 <104769882+corrado1982@users.noreply.github.com>
2023-08-18 09:34:46 +0200

    cy_it log in fake

--- cypress/e2e/3-testing/login.cy.js
+++ cypress/e2e/3-testing/login.cy.js
@@ -21,4 +21,43 @@ describe("login test", () => {
     // check if it enter to che profile page
     cy.url().should("include", "profile");
   });
+
+  it("Should fail login with invalid credential and shown a message", () => {
+    // visit the login page
+    cy.visit("http://127.0.0.1:5500/"); // Replace 3000 with your port number
+
+    // check if the register modal exist
+    cy.get("#registerModal .btn").contains("Login").should("exist");
+    cy.wait(1000);
+
+    // move to the login modal
+    cy.get("#registerModal .btn").contains("Login").click();
+    cy.get("#loginForm .btn").contains("Login").should("exist");
+    cy.wait(1000);
+
+    // type fake email and fake password and submit
+    cy.get("#loginEmail").type("fakeemail@stud.noroff.no");
+    cy.wait(1000);
+    cy.get("#loginPassword").type("fakepass");
+    cy.get("#loginForm").submit();
+    cy.wait(2000);
+    // check if is stock on the index page
+    cy.url().should("not.include", "profile");
+    cy.wait(2000);
+
+    cy.window().get(".alert").should("exist");
+    cy.wait(2000);
+
+    // Find the input field with id "username" and type the incorrect username
+    // cy.get("#username").type("incorrectUsername");
+
+    // Find the input field with id "password" and type the incorrect password
+    // cy.get("#password").type("incorrectPassword");
+
+    // Find the form with id "loginForm" and submit it
+    // cy.get("#loginForm").submit();
+
+    // Error message should be displayed
+    // cy.get("#message .alert.alert-danger").should("contain", "Error: Either the username or password is incorrect");
+  });
 });
