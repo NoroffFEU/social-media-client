fc71ee596903a19adc0f3c861878f78ab79e42ec
Lasse <lasseronnlie@hotmail.com>
2023-08-18 05:27:51 +0200

    update vite eslint

---
+++ vite.config.js
@@ -0,0 +1,36 @@
+import eslint from 'vite-plugin-eslint'
+import { defineConfig } from "vite";
+import path, {resolve} from 'path'
+export default defineConfig ({
+  plugin:[
+    // default settings on build (i.e. fail on error)
+    {
+      ...eslint(),
+      apply: 'build',
+    },
+    {
+
+      ...eslint({
+        failOnWarning:false,
+        failOnError: false,
+      }),
+      apply: 'serve',
+      enfoce: 'post'
+    }
+  ],
+  build: {
+    rollupOptions: {
+
+    }
+  },
+  server: {
+    hot: true,
+    port: 5555,
+    host: '127.0.0.1'
+  },
+  resolve: {
+    alias: {
+      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+    }
+  }
+})
