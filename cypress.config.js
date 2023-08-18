a264fabfc4f323ce9d3a94fdc7e19cbaa5fe0366
Emilyrf <104759694+Emilyrf@users.noreply.github.com>
2023-08-18 11:54:22 +0200

    Revert "Fixes keyboard event failure bug"
    This reverts commit a35e575b8e38c5e11cdd2fcf9a6bcc5b4546c221.
    
    Revert uwanted modifications

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,5 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    video: false,
-    screenshotOnRunFailure: false,
   },
 });
