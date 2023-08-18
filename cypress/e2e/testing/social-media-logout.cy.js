f1e141af66edc024bd14c58be50528bc273222c3
Emilyrf <104759694+Emilyrf@users.noreply.github.com>
2023-08-18 20:04:23 +0200

    Implementing PRs suggestions

--- cypress/e2e/testing/social-media-logout.cy.js
+++ cypress/e2e/testing/social-media-logout.cy.js
@@ -1,13 +1,20 @@
 describe('norrofsocialmedia.no', () => {
   it('the user can log out with the logout button', () => {
     cy.visit('http://localhost:8080');
-    cy.wait(3000);
+    cy.wait(2000);
     cy.get('[data-bs-target="#loginModal"]').last().click();
     cy.wait(2000);
-    cy.get('#loginEmail').type('emily@stud.noroff.no');
-    cy.get('#loginPassword').type('123456789{enter}');
-    cy.wait(8000);
+    cy.get("input[type='email']:visible")
+      .should('exist')
+      .type('emily@stud.noroff.no');
+    cy.get("input[type='password']:visible")
+      .should('exist')
+      .type('123456789{enter}');
+    cy.wait(2000);
+    cy.url().should('include', 'profile');
+    cy.wait(2000);
     cy.get('[data-auth="logout"]').click();
-    cy.wait(8000);
+    cy.wait(2000);
+    cy.url().should('not.include', 'profile');
   });
 });
