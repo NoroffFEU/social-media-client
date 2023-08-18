fc0ccf8260cd89220c55f033b23fecff3aeccf19
corrado1982 <104769882+corrado1982@users.noreply.github.com>
2023-08-18 07:03:52 +0200

    cy_it log in

--- cypress/e2e/3-testing/login.cy.js
+++ cypress/e2e/3-testing/login.cy.js
@@ -1,31 +1,24 @@
 describe("login test", () => {
-  it("can search for Social Media Website", () => {
+  it("can search for Social Media Website, swich from register page to login page, log in", () => {
+    // visit the page
     cy.visit("http://127.0.0.1:5500/");
 
+    // check if the register modal exist
     cy.get("#registerModal .btn").contains("Login").should("exist");
-
     cy.wait(1000);
 
     // move to the login modal
-
     cy.get("#registerModal .btn").contains("Login").click();
-
     cy.get("#loginForm .btn").contains("Login").should("exist");
-
     cy.wait(1000);
 
-    //
-
+    // type email and password and submit
     cy.get("#loginEmail").type("cor@stud.noroff.no");
-
     cy.wait(1000);
-
     cy.get("#loginPassword").type("loginpass");
-
     cy.get("#loginForm").submit();
 
+    // check if it enter to che profile page
     cy.url().should("include", "profile");
-
-    // // cy.get("h1").contains("Noroff");
   });
 });
