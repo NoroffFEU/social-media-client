cf10d032df3681c64755c30e574a37ad2919f41d
Emilyrf <104759694+Emilyrf@users.noreply.github.com>
2023-08-19 13:24:55 +0200

    Tests eslint

---
+++ src/js/testlint.js
@@ -0,0 +1,12 @@
+// This code intentionally contains ESLint violations for demonstration purposes
+
+const example = 'example';
+
+function doSomething() {
+  if (true == true) {
+    // Incorrect usage of equality comparison
+    console.log('Hello World');
+  }
+}
+
+console.log(example); // Unused variable
