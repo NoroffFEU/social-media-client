21cdae50a2bd5410365510c6f5350d65d23e7a76
saibia8 <saibia.design@gmail.com>
2023-08-20 09:14:38 +0200

    Creted logout and login with bad password tests

--- cypress/e2e/User-login-and-access-profile/login.cy.js
+++ cypress/e2e/User-login-and-access-profile/login.cy.js
@@ -1,10 +1,27 @@
-describe("Social media client: login and access profile", () => {
-  it("Login test", () => {
+describe("Social media client: login and access profile, logout, try to login with invalid password", () => {
+  it("Login to profile account", () => {
     cy.visit("http://127.0.0.1:5500/index.html");
     cy.get("#registerForm > .modal-header > .btn-close").click();
     cy.get(".text-end > .btn-outline-success").click({ force: true });
     cy.get("#loginEmail").type("sabKut73328@stud.noroff.no", { force: true });
     cy.get("#loginPassword").type("kukuku16", { force: true });
+    cy.get("#loginForm > .modal-footer > .btn-success").click({ force: true });
+  });
+  it("Logout from profile account", () => {
+    cy.visit("http://127.0.0.1:5500/index.html");
+    cy.get("#registerForm > .modal-header > .btn-close").click();
+    cy.get(".text-end > .btn-outline-success").click({ force: true });
+    cy.get("#loginEmail").type("sabKut73328@stud.noroff.no", { force: true });
+    cy.get("#loginPassword").type("kukuku16", { force: true });
+    cy.get("#loginForm > .modal-footer > .btn-success").click({ force: true });
+    cy.get(".btn-outline-warning").click();
+  });
+  it("Try to login with bad password", () => {
+    cy.visit("http://127.0.0.1:5500/index.html");
+    cy.get("#registerForm > .modal-header > .btn-close").click();
+    cy.get(".text-end > .btn-outline-success").click({ force: true });
+    cy.get("#loginEmail").type("sabKut73328@stud.noroff.no", { force: true });
+    cy.get("#loginPassword").type("kukuku18", { force: true });
     cy.get("#loginForm > .modal-footer > .btn-success").click({ force: true });
   });
 });
