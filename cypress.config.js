eeb28e22c40c0a25df6e4ece52eac68e0d50dfa5
saibia8 <saibia.design@gmail.com>
2023-08-20 09:52:59 +0200

    Updated cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,7 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    video: false,
+    screenshotOnRunFailure: false,
   },
 });
