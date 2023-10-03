5ce737e2e58bc0f9a54c4a13db8bc3ae67407517
lindatarod85 <lindatarod@gmail.com>
2023-10-03 12:37:36 +0200

    Updates eslint configuration for jest

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,32 +1,23 @@
 module.exports = {
     "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
-    "overrides": [
+          "browser": true,
+          "es2021": true
+      },
+      "extends": "eslint:recommended",
+      "overrides": [
         {
-            "env": {
-                "node": true
-            },
-            "files": [
-                ".eslintrc.{js,cjs}"
-            ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
+          "files": ["**/*.test.js"],
+          "env": { "jest": true },
+          "plugins": ["jest"],
+          "extends": ["plugin:jest/recommended"],
+          "rules": { "jest/prefer-expect-assertions": "off" }
         }
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-    }
-}
+      ],
+      "parserOptions": {
+          "ecmaVersion": "latest",
+          "sourceType": "module"
+      },
+      "rules": {
+      }
+  }
+  
