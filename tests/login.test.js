0128469c860a9920c23149c5bc45a3f6144e22e3
Emily <69647535+Emii91@users.noreply.github.com>
2023-10-22 15:05:55 +0200

    Update login.test.js

--- tests/login.test.js
+++ tests/login.test.js
@@ -1,1 +1,25 @@
+import { authGuard } from "../src/js/router/index.js";
 
+jest.mock("../src/js/api/index.js", () => {
+  return {
+    isLoggedIn: jest.fn(() => true)
+  };
+});
+
+describe("AuthGuard", () => {
+  it("should call the callback when the user is logged in", () => {
+    const callback = jest.fn();
+    const result = authGuard(callback);
+
+    expect(result).toEqual(callback);
+    expect(callback).toHaveBeenCalled();
+  });
+
+  it("should show a message and redirect when the user is not logged in", () => {
+    const callback = jest.fn();
+    const message = authGuard(callback, "post");
+
+    expect(callback).not.toHaveBeenCalled();
+    expect(message).toMatchSnapshot();
+  });
+});
