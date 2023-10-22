3da85c7f54cff7d11b6fbced25bc51cc2c671d81
Emily <69647535+Emii91@users.noreply.github.com>
2023-10-22 16:42:13 +0200

    Update logout.test.js

--- tests/logout.test.js
+++ tests/logout.test.js
@@ -1,1 +1,25 @@
+import { logout } from "../src/js/api/index.js";
 
+jest.mock("../src/js/router/index.js", () => {
+  return {
+    authGuard: jest.fn((callback) => callback())
+  };
+});
+
+describe("Logout", () => {
+  it("logs out the user and redirects", () => {
+    const callback = jest.fn();
+    logout(callback);
+
+    expect(callback).toHaveBeenCalled();
+  });
+
+  it("does not log out if user is not logged in", () => {
+    const callback = jest.fn();
+    jest.spyOn(jest.requireActual("../src/js/api/index.js"), "isLoggedIn").mockReturnValue(false);
+
+    logout(callback);
+
+    expect(callback).not.toHaveBeenCalled();
+  });
+});
