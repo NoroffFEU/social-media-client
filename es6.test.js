dae1383f3a46d7bf261c7e75c3c33a1b8c52032d
Korny94 <karlmagnusnokling@hotmail.no>
2023-10-26 19:17:56 +0200

    delete cypress examples

--- es6.test.js
+++ es6.test.js
@@ -8,18 +8,53 @@ test("It resolves with 'abc' value after 1 second", async () => {
   expect(end - start).toBeGreaterThanOrEqual(1000);
 });
 
-import { remove } from "/src/js/storage/remove.js";
+import { login } from "/src/js/api/auth/login.js"; // Replace with the actual import path
 import { save } from "/src/js/storage/save.js";
 
-test("It saves a value to localStorage", () => {
-  const key = "test-save";
-  save(key, "abc");
-  expect(localStorage.getItem(key)).toEqual("abc");
-});
+// Mock the fetch function
+global.fetch = jest.fn(() =>
+  Promise.resolve({
+    ok: true,
+    json: () => Promise.resolve({ accessToken: "your-test-token" }),
+    statusText: "OK",
+  })
+);
 
-test("It removes a value from localStorage", () => {
-  const key = "test-remove";
-  localStorage.setItem(key, "abc");
-  remove(key);
-  expect(localStorage.getItem(key)).toBeNull();
+// Mock the storage functions
+jest.mock("/src/js/storage/save.js", () => ({
+  save: jest.fn(),
+}));
+
+describe("login function", () => {
+  it("fetches and stores a token in browser storage", async () => {
+    const email = "cypressTest@noroff.no";
+    const password = "cypresstest";
+
+    const result = await login(email, password);
+
+    // Verify that the fetch function was called with the expected URL and data
+    expect(global.fetch).toHaveBeenCalledWith(
+      expect.stringContaining("social/auth/login"),
+      {
+        method: "post",
+        body: JSON.stringify({ email, password }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    // Verify that the storage function save was called with the token
+    expect(save).toHaveBeenCalledWith("token", "your-test-token");
+
+    // Verify that the token was deleted from the profile object
+    expect(save).toHaveBeenCalledWith("profile", {
+      /* your profile data without accessToken */
+    });
+
+    // Verify that the function returns the profile object
+    expect(result).toEqual({
+      /* your profile data without accessToken */
+    });
+  });
 });
