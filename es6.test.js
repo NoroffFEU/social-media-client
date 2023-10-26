5240be2d45f808eda674be760e467d4a79af0954
Korny94 <karlmagnusnokling@hotmail.no>
2023-10-27 01:01:16 +0200

    Update es6.test.js

--- es6.test.js
+++ es6.test.js
@@ -8,27 +8,40 @@ test("It resolves with 'abc' value after 1 second", async () => {
   expect(end - start).toBeGreaterThanOrEqual(1000);
 });
 
-import { login } from "/src/js/api/auth/login.js";
+import { loginListener } from "/src/js/listeners/auth/login"; // Replace with the actual import path
 
-describe("login function", () => {
-  it("fetches and stores a token in local storage", async () => {
-    // Mock the global localStorage object
+describe("loginListener function", () => {
+  it("checks for 'token' in local storage after a successful login", async () => {
+    // Mock a successful login scenario
+    const event = {
+      preventDefault: jest.fn(),
+      target: document.createElement("form"),
+    };
+
+    // Mock the auth.login function to simulate a successful login
+    const loginResult = { name: "Test User" };
+    const authMock = {
+      login: jest.fn().mockResolvedValue(loginResult),
+    };
+
+    // Mock the localStorage getItem method
     const localStorageMock = {
-      setItem: jest.fn(),
+      getItem: jest.fn(),
     };
 
     // Replace the real localStorage with the mock
     Object.defineProperty(window, "localStorage", { value: localStorageMock });
 
-    const email = "cypressTest@noroff.no";
-    const password = "cypresstest";
+    // Call the loginListener function
+    await loginListener(event);
 
-    await login(email, password);
+    // Verify that preventDefault was called
+    expect(event.preventDefault).toHaveBeenCalled();
 
-    // Verify that the "token" key was set in local storage
-    expect(localStorageMock.setItem).toHaveBeenCalledWith(
-      "token",
-      expect.any(String)
-    );
+    // Verify that auth.login was called
+    expect(authMock.login).toHaveBeenCalled();
+
+    // Verify that local storage getItem was called with "token"
+    expect(localStorageMock.getItem).toHaveBeenCalledWith("token");
   });
 });
