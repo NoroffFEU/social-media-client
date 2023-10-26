a8314f78eabd7509c6db9387ef02296fdc565438
Korny94 <karlmagnusnokling@hotmail.no>
2023-10-27 01:47:48 +0200

    Update es6.test.js

--- es6.test.js
+++ es6.test.js
@@ -30,17 +30,42 @@ describe("login function", () => {
     Object.defineProperty(window, "localStorage", { value: localStorageMock });
 
     // Mock the fetch response
-    fetchMock.mockResponse(JSON.stringify({ accessToken: "your-test-token" }));
+    fetchMock.mockResponse(JSON.stringify({ accessToken: "your-test-token" }), {
+      status: 200,
+    });
 
     const email = "cypressTest@noroff.no";
     const password = "cypresstest";
 
-    await login(email, password);
+    const result = await login(email, password);
 
     // Verify that the "token" key was set in local storage
     expect(localStorageMock.setItem).toHaveBeenCalledWith(
       "token",
       "your-test-token"
     );
+
+    // Verify that the "profile" key was set in local storage
+    expect(localStorageMock.setItem).toHaveBeenCalledWith(
+      "profile",
+      expect.any(String)
+    );
+
+    // Verify that the function returns the profile object
+    expect(result).toEqual(
+      expect.objectContaining({ accessToken: "your-test-token" })
+    );
+  });
+
+  it("throws an error when the response is not OK", async () => {
+    // Mock the fetch response with an error status
+    fetchMock.mockResponse(JSON.stringify({ error: "Invalid credentials" }), {
+      status: 401,
+    });
+
+    const email = "invalid@test.com";
+    const password = "invalidpassword";
+
+    await expect(login(email, password)).rejects.toThrow("Invalid credentials");
   });
 });
