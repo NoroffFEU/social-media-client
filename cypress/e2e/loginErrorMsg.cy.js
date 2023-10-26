2ca6ef8e6ed265a42ccf4f0158835ee934707798
Korny94 <karlmagnusnokling@hotmail.no>
2023-10-26 19:46:01 +0200

    Update loginErrorMsg.cy.js

--- cypress/e2e/loginErrorMsg.cy.js
+++ cypress/e2e/loginErrorMsg.cy.js
@@ -1,5 +1,5 @@
-describe("Cannot Submit Login Form with Invalid Credentials", () => {
-  it("displays an error message for invalid credentials", () => {
+describe("Cannot Submit Login Form with Invalid email Credentials", () => {
+  it("displays an error message for invalid email credentials", () => {
     // Load the login page
     cy.visit("../../index.html");
 
@@ -8,13 +8,31 @@ describe("Cannot Submit Login Form with Invalid Credentials", () => {
 
     // Fill out the login form with invalid credentials
     cy.get("#loginEmail").type("invalid@email.com");
-    cy.get("#loginPassword").type("invalid");
 
     // Submit the form
     cy.get("#loginBtn").click();
 
     // Wait for the error message to be displayed
     cy.get("#loginEmail:invalid").should("be.visible");
+  });
+});
+
+describe("Cannot Submit Login Form with Invalid password Credentials", () => {
+  it("displays an error message for invalid password credentials", () => {
+    // Load the login page
+    cy.visit("../../index.html");
+
+    // Find and click the "Login" button to navigate to the login form
+    cy.contains("Login").click();
+
+    // Fill out the login form with invalid credentials
+    cy.get("#loginEmail").type("cypressTest@noroff.no");
+    cy.get("#loginPassword").type("invalid");
+
+    // Submit the form
+    cy.get("#loginBtn").click();
+
+    // Wait for the error message to be displayed
     cy.get("#loginPassword:invalid").should("be.visible");
   });
 });
