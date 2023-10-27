0ec10d313ad0e4fa66bb4ae90e8f3eec59b3fdb4
MariuszRozycki <mariansc83@icloud.com>
2023-10-27 13:11:08 +0200

    Automatically created cypress.config.js

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'cypress';
+/* eslint-disable */
+
+export default defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
