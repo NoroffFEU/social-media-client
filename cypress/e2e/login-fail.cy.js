b57e2e8059c314958cc374617357eba20f1c4b5f
MariuszRozycki <mariansc83@icloud.com>
2023-10-30 16:25:23 +0100

    Add login-fail.cy.js

---
+++ cypress/e2e/login-fail.cy.js
@@ -0,0 +1,35 @@
+describe('The user cannot submit the login form when invalid credentials and is shown a message', () => {
+  it('can not submit when invalid credentials', () => {
+    const faiEmail = `fail@noroff.no`;
+    const failPassword = `failPassword`;
+    const expectedEmail = `guest@noroff.no`;
+    const expectedPassword = `123456789`;
+
+    cy.visit('http://127.0.0.1:8080/');
+    cy.wait(2000);
+
+    cy.get('#registerForm [data-bs-target="#loginModal"]').should('be.visible').click();
+    cy.get('#loginForm').should('be.visible');
+
+    cy.on('window:alert', str => {
+      expect(str).to.equal('Either your username was not found or your password is incorrect');
+    });
+
+    cy.get('#loginEmail').wait(1000).should('have.attr', 'title', 'Only Noroff student or teacher emails are valid.').and('be.visible').type(faiEmail);
+    cy.get('#loginPassword').should('be.visible').type(failPassword);
+    cy.get('#loginForm .btn.btn-success').should('be.visible').click();
+    cy.intercept(
+      {
+        method: 'POST',
+        url: `https://nf-api.onrender.com/api/v1/social/auth/login`
+      },
+      request => {
+        const userEmail = request.body.email;
+        const userPassword = request.body.password;
+
+        const isEmpty = !userEmail || !userPassword;
+        const isInvalid = userEmail !== expectedEmail || userPassword !== expectedPassword;
+      }
+    );
+  });
+});
