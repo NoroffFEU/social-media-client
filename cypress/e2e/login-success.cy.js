1e644a6b47d4505dc6518135efbfe007d8ef6ffd
MariuszRozycki <mariansc83@icloud.com>
2023-10-30 14:24:21 +0100

    Change file name to login-success.cy.js

---
+++ cypress/e2e/login-success.cy.js
@@ -0,0 +1,39 @@
+describe('Login and access profile', () => {
+  beforeEach(() => {
+    cy.visit('http://127.0.0.1:8080/');
+    cy.wait(1000);
+    cy.get('#registerForm [data-bs-target="#loginModal"]').should('be.visible').click();
+    cy.get('#loginForm').should('be.visible');
+    cy.get('#loginEmail').wait(1000).type('guest@noroff.no');
+    cy.get('#loginPassword').should('be.visible').type('123456789');
+    cy.get('#loginForm .btn.btn-success').should('be.visible').click();
+    cy.wait(2000);
+  });
+
+  it('should log in successfully', () => {
+    cy.window().then(win => {
+      const token = win.JSON.parse(localStorage.getItem('token'));
+      expect(token).to.exist;
+    });
+  });
+
+  it('should access and verify the profile', () => {
+    cy.window().then(win => {
+      const token = win.JSON.parse(localStorage.getItem('token'));
+      cy.request({
+        method: 'GET',
+        url: 'https://nf-api.onrender.com/api/v1/social/profiles/guest?&_followers=true&_posts=true&_following=true',
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      }).then(response => {
+        const nameFromApi = response.body.name;
+        cy.url().then(currentUrl => {
+          const url = new URL(currentUrl);
+          const nameFromUrl = url.searchParams.get('name');
+          expect(nameFromApi).to.equal(nameFromUrl);
+        });
+      });
+    });
+  });
+});
