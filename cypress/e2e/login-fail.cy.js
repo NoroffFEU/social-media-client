4fe99db175340fbf495b0931d75a31b1b1995cd7
MariuszRozycki <mariansc83@icloud.com>
2023-10-31 06:12:39 +0100

    Add beforeEach and share test for three smaller tests

--- cypress/e2e/login-fail.cy.js
+++ cypress/e2e/login-fail.cy.js
@@ -1,10 +1,10 @@
 describe('The user cannot submit the login form when invalid credentials and is shown a message', () => {
-  it('can not submit when invalid credentials', () => {
-    const failEmail = `fail@noroff.no`;
-    const failPassword = `failPassword`;
-    const expectedEmail = `guest@noroff.no`;
-    const expectedPassword = `123456789`;
+  const failEmail = `fail@noroff.no`;
+  const failPassword = `failPassword`;
+  const expectedEmail = `guest@noroff.no`;
+  const expectedPassword = `123456789`;
 
+  beforeEach(() => {
     cy.intercept(
       {
         method: 'POST',
@@ -37,15 +37,27 @@ describe('The user cannot submit the login form when invalid credentials and is 
 
     cy.visit('http://127.0.0.1:8080/');
     cy.wait(2000);
-
     cy.get('#registerForm [data-bs-target="#loginModal"]').should('be.visible').click();
     cy.get('#loginForm').should('be.visible');
+  });
 
+  it('can not submit when invalid credentials', () => {
+    cy.get('#loginEmail').wait(1000).should('be.visible').type(failEmail);
+    cy.get('#loginPassword').should('be.visible').type(failPassword);
+    cy.get('#loginForm .btn.btn-success').should('be.visible').click();
+    cy.get('#loginEmail').invoke('attr', 'title').should('equal', 'Only Noroff student or teacher emails are valid.');
+  });
+
+  it('should display the tooltip and message on invalid email format', () => {
+    cy.get('#loginEmail').invoke('attr', 'title').should('equal', 'Only Noroff student or teacher emails are valid.');
+  });
+
+  it('should display alert message on invalid email format or incorrect password', () => {
     cy.on('window:alert', str => {
       expect(str).to.equal('Either your username was not found or your password is incorrect');
     });
 
-    cy.get('#loginEmail').wait(1000).should('have.attr', 'title', 'Only Noroff student or teacher emails are valid.').and('be.visible').type(failEmail);
+    cy.get('#loginEmail').wait(1000).should('be.visible').type(failEmail);
     cy.get('#loginPassword').should('be.visible').type(failPassword);
     cy.get('#loginForm .btn.btn-success').should('be.visible').click();
   });
