99ea58635892a4dbd6a20dfca94ba9a26cc58265
Ernesto Jr Fundal Osorio <114483893+nyolarraklay@users.noreply.github.com>
2023-10-31 15:23:16 +0100

    edit html

--- cypress/e2e/3-testing/workflow-ca.cy.js
+++ cypress/e2e/3-testing/workflow-ca.cy.js
@@ -1,10 +1,9 @@
-describe("https://nyolarraklay.github.io/social-media-client-CA-workflow/", () => {
-  it("can create a profile", () => {
+describe("Social Media App: Unauthorized user", () => {
+  it("CANNOT submit the log in form", () => {
     cy.visit("https://nyolarraklay.github.io/social-media-client-CA-workflow/");
-    cy.get("input#registerName").type("examplename", { delay: 500 });
-    cy.get("input#registerEmail").type("example@stud.noroff.no", {
-      delay: 500,
+
+    cy.get("#closeButton").click({
+      force: true,
     });
-    cy.get("input#registerPassword").type("12345678{enter}", { delay: 500 });
   });
 });
