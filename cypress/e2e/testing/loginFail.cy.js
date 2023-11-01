33f4dfb4064d78e2423e32c0e0439ae954b175c5
Kim Rune Møller <kimrune@hotmail.no>
2023-11-01 13:32:12 +0100

    adds assertion for js alert

--- cypress/e2e/testing/loginFail.cy.js
+++ cypress/e2e/testing/loginFail.cy.js
@@ -7,10 +7,18 @@ describe("Login", () => {
       cy.contains("button", "Login").click();
     });
     cy.wait(2000);
+    // fill out the form
     cy.get("#loginForm").within(() => {
-      cy.get("input#loginEmail").type("invalid@invalid.inv");
-      cy.get("input#loginPassword").type("asdasdasd");
+      cy.get("input#loginEmail").type("invalid@noroff.no");
+      cy.get("input#loginPassword").type("123132123");
       cy.contains("button", "Login").click();
+    });
+    cy.wait(2000);
+    // assert that a alert is shown
+    cy.on("window:alert", (str) => {
+      expect(str).equal(
+        "Either your username was not found or your password is incorrect",
+      );
     });
   });
 });
