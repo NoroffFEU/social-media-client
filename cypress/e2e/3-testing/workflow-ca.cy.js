1158461f25a12d060e06cb1eb13a5899ac0be7a3
Ernesto Jr Fundal Osorio <114483893+nyolarraklay@users.noreply.github.com>
2023-11-01 09:39:30 +0100

    completed the e2e testing

--- cypress/e2e/3-testing/workflow-ca.cy.js
+++ cypress/e2e/3-testing/workflow-ca.cy.js
@@ -1,9 +1,51 @@
 describe("Social Media App: Unauthorized user", () => {
-  it("CANNOT submit the log in form", () => {
-    cy.visit("https://nyolarraklay.github.io/social-media-client-CA-workflow/");
+  beforeEach(() => {
+    cy.visit("https://nyolarraklay.github.io/social-media-client-CA-workflow");
+  });
 
-    cy.get("#closeButton").click({
-      force: true,
-    });
+  it("CAN submit the log in form and access profile page", () => {
+    cy.get("#registerModal");
+    cy.wait(1000);
+    cy.get("#registerForm > .modal-footer > .btn-outline-success").dblclick();
+    cy.get("#loginEmail").type("klay@noroff.no", { force: true, delay: 100 });
+    cy.get("#loginPassword").type("password", { force: true, delay: 100 });
+
+    cy.get("#loginForm").submit();
+    cy.visit(
+      "https://nyolarraklay.github.io/social-media-client-CA-workflow/?view=profile&name=klay"
+    );
+  });
+
+  it("CAN logout the website", () => {
+    cy.get("#registerModal");
+    cy.wait(1000);
+    cy.get("#registerForm > .modal-footer > .btn-outline-success").dblclick();
+    cy.get("#loginEmail").type("klay@noroff.no", { force: true, delay: 100 });
+    cy.get("#loginPassword").type("password", { force: true, delay: 100 });
+    cy.get("#loginForm").submit();
+
+    cy.visit(
+      "https://nyolarraklay.github.io/social-media-client-CA-workflow/?view=profile&name=klay"
+    );
+    cy.wait(1000);
+    cy.get(".btn-outline-warning").dblclick();
+  });
+
+  it("CANNOT submit the log in form", () => {
+    cy.get("#registerModal");
+    cy.wait(1000);
+    cy.get("#registerForm > .modal-footer > .btn-outline-success").dblclick();
+    cy.get("#loginEmail")
+      .type("fake@stud.noroff.no", { force: true, delay: 100 })
+      .should("have.value", "fake@stud.noroff.no");
+    cy.get("#loginPassword").type("fakepassword", { force: true, delay: 100 });
+
+    cy.get("#loginForm")
+      .submit()
+      .next()
+      .should(
+        "contain",
+        "Either your username was not found or your password is incorrect"
+      );
   });
 });
