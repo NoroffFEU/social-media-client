5ce2d585faebb1e4f5eaff9547c3ee6695e6934f
Ernesto Jr Fundal Osorio <114483893+nyolarraklay@users.noreply.github.com>
2023-11-01 18:04:12 +0100

    fix test in cypress

--- cypress/e2e/3-testing/workflow-ca.cy.js
+++ cypress/e2e/3-testing/workflow-ca.cy.js
@@ -3,6 +3,29 @@ describe("Social Media App: Unauthorized user", () => {
     cy.visit("https://nyolarraklay.github.io/social-media-client-CA-workflow");
   });
 
+  it("CANNOT submit the log in form", () => {
+    cy.get("#registerModal");
+    cy.wait(1000);
+    cy.get("#registerForm > .modal-footer > .btn-outline-success").dblclick();
+    cy.get("#loginEmail")
+      .type("fake@stud.noroff.no", { force: true, delay: 100 })
+      .should("have.value", "fake@stud.noroff.no");
+    cy.get("#loginPassword").type("fakepassword", { force: true, delay: 100 });
+
+    cy.get("#loginForm")
+      .submit()
+      .next()
+      .should(
+        "contain",
+        "Either your username was not found or your password is incorrect"
+      );
+  });
+});
+
+describe("Social Media App: authorized user", () => {
+  beforeEach(() => {
+    cy.visit("https://nyolarraklay.github.io/social-media-client-CA-workflow");
+  });
   it("CAN submit the log in form and access profile page", () => {
     cy.get("#registerModal");
     cy.wait(1000);
@@ -29,23 +52,5 @@ describe("Social Media App: Unauthorized user", () => {
     );
     cy.wait(1000);
     cy.get(".btn-outline-warning").dblclick();
-  });
-
-  it("CANNOT submit the log in form", () => {
-    cy.get("#registerModal");
-    cy.wait(1000);
-    cy.get("#registerForm > .modal-footer > .btn-outline-success").dblclick();
-    cy.get("#loginEmail")
-      .type("fake@stud.noroff.no", { force: true, delay: 100 })
-      .should("have.value", "fake@stud.noroff.no");
-    cy.get("#loginPassword").type("fakepassword", { force: true, delay: 100 });
-
-    cy.get("#loginForm")
-      .submit()
-      .next()
-      .should(
-        "contain",
-        "Either your username was not found or your password is incorrect"
-      );
   });
 });
