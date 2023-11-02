e186465d16984ed7657809f69f320de038bb1e5f
rydlande <rydlande@hotmail.com>
2023-11-02 20:46:20 +0100

    test

--- test.js
+++ test.js
@@ -1,10 +1,11 @@
-require("dotenv/config");
+/* require('dotenv/config');
 
 function test(expectedSecret) {
   const secretMatches = process.env.MEANING_OF_LIFE == expectedSecret;
   console.log(`The meaning of life is ${process.env.MEANING_OF_LIFE}`);
-  console.log(`The secret does${secretMatches ? "" : " not"} match!`);
+  console.log(`The secret does${secretMatches ? '' : ' not'} match!`);
   return secretMatches;
 }
 
 test(42);
+ */
