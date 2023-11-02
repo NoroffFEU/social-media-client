482853198b405c69b30ca94e584555669ac01c6b
Miksel90 <114400071+Miksel90@users.noreply.github.com>
2023-11-02 19:35:50 +0100

    changed cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,3 @@
-const { defineConfig } = require("cypress");
+import { defineConfig } from "cypress";
 
-module.exports = defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-  },
-});
+export default defineConfig({});
