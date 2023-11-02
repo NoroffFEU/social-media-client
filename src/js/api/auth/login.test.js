36db6b81648126505bf36883c49f1050554629ca
Miksel90 <114400071+Miksel90@users.noreply.github.com>
2023-11-02 13:20:27 +0100

    fixed the test

--- src/js/api/auth/login.test.js
+++ src/js/api/auth/login.test.js
@@ -11,12 +11,20 @@ const profile = {
   email: GOOD_EMAIL,
 };
 
-function fetchSuccess(status = 201, statusText = "Success!") {
+function fetchSuccess(status = 201, statusText = "Success") {
   return Promise.resolve({
     ok: true,
     status,
     statusText,
     json: () => Promise.resolve({ ...profile, TOKEN: FAKETOKEN }),
+  });
+}
+
+function fetchFailure(status = 404, statusText = "Unsuccessful") {
+  return Promise.resolve({
+    ok: false,
+    status,
+    statusText,
   });
 }
 
@@ -30,31 +38,23 @@ describe("login", () => {
     localStorage.clear();
   });
 
-  it("returns a valid token when logging in and stores the token in local storage", async () => {
-    global.fetch = jest.fn(() => fetchSuccess());
-    const data = await login(GOOD_EMAIL, FAKEPASSWORD);
+  describe("successful login", () => {
+    it("returns a valid token when logging in and stores the token in local storage", async () => {
+      global.fetch = jest.fn(() => fetchSuccess());
+      const data = await login(GOOD_EMAIL, FAKEPASSWORD);
 
-    expect(GOOD_EMAIL).toMatch("@noroff.no");
-    expect(FAKEPASSWORD.length).toBeGreaterThanOrEqual(8);
-    expect(data.TOKEN).toEqual(FAKETOKEN);
-
-    setTokenInLocalStorage(FAKETOKEN);
-    const storedToken = localStorage.getItem("TOKEN");
-    expect(storedToken).toEqual(FAKETOKEN);
+      setTokenInLocalStorage(FAKETOKEN);
+      const storedToken = localStorage.getItem("TOKEN");
+      expect(storedToken).toEqual(FAKETOKEN);
+    });
   });
 
-  function fetchFailure(status = 404, statusText = "Unsuccessful") {
-    return Promise.resolve({
-      ok: false,
-      status,
-      statusText,
+  describe("failed login", () => {
+    it("throws an error when provided with invalid credentials", async () => {
+      global.fetch = jest.fn(() => fetchFailure());
+      await expect(login(BAD_EMAIL, FAKEPASSWORD)).rejects.toThrow(
+        "Unsuccessful"
+      );
     });
-  }
-
-  it("throws an error when provided when login is invalid", async () => {
-    global.fetch = jest.fn(() => fetchFailure());
-    await expect(login(BAD_EMAIL, FAKEPASSWORD)).rejects.toThrow(
-      "Unsuccessful"
-    );
   });
 });
