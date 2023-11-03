6b5cf3d527303500918a6b4587ed4e81a402f4c8
Miksel90 <114400071+Miksel90@users.noreply.github.com>
2023-11-03 11:43:13 +0100

    adding code

--- src/js/api/auth/login.test.js
+++ src/js/api/auth/login.test.js
@@ -1,8 +1,50 @@
+// import { login } from "./login";
+// import "jest-localstorage-mock";
+
+// const GOOD_EMAIL = "IAmGod@noroff.no";
+// const FAKEPASSWORD = "GoldSilver";
+// const FAKETOKEN = "xY7ZpR9qK1wJ2vT3uF8sG6hM4nL5pQ";
+
+// const profile = {
+//   name: "randomUser",
+//   email: GOOD_EMAIL,
+// };
+
+// function fetchSuccess(status = 201, statusText = "Success") {
+//   return Promise.resolve({
+//     ok: true,
+//     status,
+//     statusText,
+//     json: () => Promise.resolve({ ...profile, TOKEN: FAKETOKEN }),
+//   });
+// }
+
+// function setTokenInLocalStorage(token) {
+//   localStorage.setItem("TOKEN", FAKETOKEN);
+// }
+
+// describe("login", () => {
+//   beforeEach(() => {
+//     jest.clearAllMocks();
+//     localStorage.clear();
+//   });
+
+//   describe("successful login", () => {
+//     it("returns a valid token when logging in and stores the token in local storage", async () => {
+//       global.fetch = jest.fn(() => fetchSuccess());
+//       const data = await login(GOOD_EMAIL, FAKEPASSWORD);
+
+//       setTokenInLocalStorage(FAKETOKEN);
+//       const storedToken = localStorage.getItem("TOKEN");
+//       expect(storedToken).toEqual(FAKETOKEN);
+//     });
+//   });
+// });
+
 import { login } from "./login";
 import "jest-localstorage-mock";
 
 const GOOD_EMAIL = "IAmGod@noroff.no";
-const BAD_EMAIL = "rofl@mao.com";
 const FAKEPASSWORD = "GoldSilver";
 const FAKETOKEN = "xY7ZpR9qK1wJ2vT3uF8sG6hM4nL5pQ";
 
@@ -20,8 +62,8 @@ function fetchSuccess(status = 201, statusText = "Success") {
   });
 }
 
-function setTokenInLocalStorage(token) {
-  localStorage.setItem("TOKEN", token);
+function setTokenInLocalStorage(key, token) {
+  localStorage.setItem(key, token);
 }
 
 describe("login", () => {
@@ -35,7 +77,7 @@ describe("login", () => {
       global.fetch = jest.fn(() => fetchSuccess());
       const data = await login(GOOD_EMAIL, FAKEPASSWORD);
 
-      setTokenInLocalStorage(FAKETOKEN);
+      setTokenInLocalStorage("TOKEN", FAKETOKEN); // Set the token with a specific key
       const storedToken = localStorage.getItem("TOKEN");
       expect(storedToken).toEqual(FAKETOKEN);
     });
