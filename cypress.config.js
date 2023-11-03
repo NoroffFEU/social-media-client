430bb22c0b3da9cb122a90345fdebbad2778ad42
rydlande <rydlande@hotmail.com>
2023-11-03 14:28:18 +0100

    node in css

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,7 @@ export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      //hent fra env, gjør om og send til cypress/e2e
     },
   },
 });
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,3 +1,5 @@
+// import cypress.env
+
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://example.cypress.io');
