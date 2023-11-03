ce1a0fba5b7f54b019641317ccf6ba412002a90b
StineNygren <107799331+StineNygren@users.noreply.github.com>
2023-11-03 11:07:24 +0100

    installed cypress

---
+++ cypress.config.js
@@ -0,0 +1,11 @@
+//Fix later
+
+// const { defineConfig } = require("cypress");
+
+// module.exports = defineConfig({
+//   e2e: {
+//     setupNodeEvents(on, config) {
+//       // implement node event listeners here
+//     },
+//   },
+// });
