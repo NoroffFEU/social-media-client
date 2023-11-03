475df543eb93eb871815485026cee88f8f787495
Miksel90 <114400071+Miksel90@users.noreply.github.com>
2023-11-03 11:50:45 +0100

    fixing cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,3 +1,16 @@
-import { defineConfig } from "cypress";
+require("dotenv").config();
+const { defineConfig } = require("cypress");
 
-export default defineConfig({});
+module.exports = defineConfig({
+  e2e: {
+    URL: "https://fermain.github.io/social-media-client/",
+    setupNodeEvents(on, config) {
+      config.env = {
+        ...process.env,
+        ...config.env,
+      };
+      return config;
+    },
+  },
+  video: false,
+});
