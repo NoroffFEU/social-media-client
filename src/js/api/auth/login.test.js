28c740183a7021bb058ddac452476352728cb90f
Eirik Kirkeli <knirkeli88@msn.com>
2023-11-08 10:00:52 +0100

    logout test created

--- src/js/api/auth/login.test.js
+++ src/js/api/auth/login.test.js
@@ -1,6 +1,6 @@
 import { login } from "./login.js";
 const GOOD_EMAIL = "testing@noroff.no";
-const BAD_EMAIL = "poo@pee.com";
+const BAD_EMAIL = "towl@ee.com";
 const PASSWORD = "12345678";
 const TOKEN = "dsgf87775dejgf294549de";
 const profile = {
@@ -8,6 +8,14 @@ const profile = {
     email: GOOD_EMAIL,
   };
 describe("login", () => {
+    beforeEach(() => {
+        global.localStorage = {
+          setItem: jest.fn(),
+          getItem: jest.fn(),
+          removeItem: jest.fn(),
+          clear: jest.fn(),
+        };
+        });
     it("it fetches and stores token in browser", async () => {
         global.fetch = jest.fn(() => fetchSuccess());
         const data = await login(GOOD_EMAIL, PASSWORD); 
@@ -23,13 +31,5 @@ function fetchSuccess(status = 201, statusText = "Success!") {
       status,
       statusText,
       json: () => Promise.resolve({ ...profile, TOKEN }),
-    });
-  }
-  
-  function fetchFailure(status = 404, statusText = "Unsuccessful") {
-    return Promise.resolve({
-      ok: false,
-      status,
-      statusText,
     });
   }
