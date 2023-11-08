b65b26008f5fcb5e5c4fe002c3b261e2d35904b3
Roar Falch Hanssen <roar.f.hanssen@gmail.com>
2023-11-08 18:13:11 +0100

    added joker.test.js

--- src/js/joker.js
+++ src/js/joker.js
@@ -1,3 +1,7 @@
 import joker from "give-me-a-joke";
 
-joker.getRandomDadJoke(console.log);
+export function dadJoke() {
+  return new Promise(function (resolve, reject) {
+    joker.getRandomDadJoke(resolve);
+  });
+}
---
+++ src/js/joker.test.js
@@ -0,0 +1,11 @@
+import { dadJoke } from "./joker.js";
+
+export async function testDadJoke() {
+  const joke = await dadJoke();
+  const testPasses = Boolean(joke);
+  console.log(joke);
+  console.log("Dad Joke Test:", testPasses ? "PASSED" : "FAILED");
+  return testPasses;
+}
+
+testDadJoke();
