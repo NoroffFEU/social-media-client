3b98ee906e4274c636c14b9745e5d2cfe417d17d
griphaugland <griphaugland@gmail.com>
2023-11-09 14:41:04 +0100

    removed unnecessary code

--- src/jstests/storage/remove.test.js
+++
@@ -1,7 +0,0 @@
-// Tests for remove.js
-/* import remove from '../../js/storage/remove.js'; */
-
-describe('Tests of remove.js', () => {
-  test('Logs the user in if credentials are valid', () => {});
-  console.log('Not implemented');
-});
--- src/jstests/storage/save.test.js
+++
@@ -1,7 +0,0 @@
-// Tests for save.js
-/* import { save } from '../../js/storage/index.js'; */
-
-describe('Tests of save.js', () => {
-  test('Saves to localstorage', () => {});
-  console.log('Not implemented');
-});
