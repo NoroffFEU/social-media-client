e85dc7c012ca24447a64c4b046b6132341828ae0
griphaugland <griphaugland@gmail.com>
2023-11-09 23:22:42 +0100

    Removed waits in cy files to avoid unnceassary waititing time

--- cypress/e2e/1-getting-started/1-End-to-end/form-check.cy.js
+++ cypress/e2e/1-getting-started/1-End-to-end/form-check.cy.js
@@ -1,14 +1,11 @@
 describe('Checking form rules', () => {
   it('Check if unauthorized', () => {
     cy.visit('https://griphaugland.github.io/ca_workflow/');
-    cy.wait(1000);
     cy.get(
       '#registerForm > div.modal-footer > button.btn.btn-outline-success',
     ).click();
-    cy.wait(1000);
     cy.get('#loginEmail').type(`thisirnadomteskt@noroff.no`);
     cy.get('#loginPassword').type(`aisr!3!pASjdmnef{enter}`);
-    cy.wait(1000);
     cy.on('window:alert', (text) => {
       expect(text).to.contains(
         'Either your username was not found or your password is incorrect',
@@ -17,11 +14,9 @@ describe('Checking form rules', () => {
   });
   it('Check for validation message', () => {
     cy.visit('https://griphaugland.github.io/ca_workflow/');
-    cy.wait(1000);
     cy.get(
       '#registerForm > div.modal-footer > button.btn.btn-outline-success',
     ).click();
-    cy.wait(1000);
     // Not possible to check for validation message as there is none. Only a browser standard.
     cy.get('#loginEmail').type('invalidemail');
   });
--- cypress/e2e/1-getting-started/1-End-to-end/log-in.cy.js
+++ cypress/e2e/1-getting-started/1-End-to-end/log-in.cy.js
@@ -5,14 +5,11 @@ const password = Cypress.env('LOGIN_PASSWORD');
 describe('logging in and checking profile name', () => {
   it('log in', () => {
     cy.visit('https://griphaugland.github.io/ca_workflow/');
-    cy.wait(1000);
     cy.get(
       '#registerForm > div.modal-footer > button.btn.btn-outline-success',
     ).click();
-    cy.wait(1000);
     cy.get('#loginEmail').type(`${email}`);
     cy.get('#loginPassword').type(`${password}{enter}`);
-    cy.wait(1000);
     cy.get('.profile-name').should('have.text', 'eirik');
   });
 });
