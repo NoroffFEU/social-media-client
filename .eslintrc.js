65a6b4c7c2705f027688f275829fa7c279b24095
griphaugland <griphaugland@gmail.com>
2023-11-09 14:46:04 +0100

    Changed yml file to run on push/pr to master

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   parser: '@babel/eslint-parser',
   parserOptions: {
-    requireConfigFile: false, // This line can be needed if you don't have a Babel config file
+    requireConfigFile: false,
   },
 };
