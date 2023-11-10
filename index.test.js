8737e9315df9c6099b257ea6882ecca746a0eda1
Johan Lossius <johan.lossius@hotmail.com>
2023-11-10 11:45:54 +0100

    recent jest and cypress work

--- index.test.js
+++ index.test.js
@@ -1,3 +1,12 @@
+// import { login } from "./src/js/api/auth/login";
+
 test("It adds 1 and 2 and gets 3 as a result", () => {
   expect(1 + 2).toEqual(3);
 });
+
+// test("It fetches the login input data from the login data from the user, and if successful, stores the access token in the localstorage, so that the user is logged in and can perform the various functions of a logged in user.",
+// () => {
+//   expect( if(response.ok) {
+//     window.localStorage;
+//   })
+// });
