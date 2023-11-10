6df8ee591228b8bc8275ddbc4e63ef1ba0bb42bb
Roar Falch Hanssen <roar.f.hanssen@gmail.com>
2023-11-11 00:01:55 +0100

    updated logout.test.js

--- src/js/logout.test.js
+++ src/js/logout.test.js
@@ -1,10 +1,7 @@
-// logout.test.js
-
 const logout = require('./logout');
 
 describe('logout function', () => {
   beforeEach(() => {
-    // Set up a mock localStorage before each test
     const localStorageMock = {
       getItem: jest.fn(),
       setItem: jest.fn(),
@@ -14,21 +11,16 @@ describe('logout function', () => {
   });
 
   it('should clear the token from local storage', () => {
-    // Set a token in local storage before calling the logout function
     localStorage.setItem('token', 'exampleToken');
 
-    // Call the logout function
     logout();
 
-    // Check if removeItem was called with the correct key
     expect(localStorage.removeItem).toHaveBeenCalledWith('token');
   });
 
   it('should not throw an error if token is not in local storage', () => {
-    // Call the logout function without setting a token in local storage
     logout();
 
-    // Check if removeItem was not called
     expect(localStorage.removeItem).not.toHaveBeenCalled();
   });
 });
