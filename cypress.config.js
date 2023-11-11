41e0b5f0dba7870b1c11c87e4b3a622d2e6e807e
Jeanett Melsom <din@email.comjeanettk.m@hotmail.com>
2023-11-11 16:16:58 +0100

    debugged failed run result from cypress immigration

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  projectId: "tc37ox",
   video: false,
   screenshots: false,
 
   e2e: {
     setupNodeEvents(on, config) {},
+    specPattern: "tests",
   },
 });
--- tests/es6.test.js
+++ tests/es6.test.js
@@ -1,9 +1,9 @@
 import { wait } from "./es6";
 
-test("It resolves with 'abc' value after 1 second", async () => {
+it("It resolves with 'abc' value after 1 second", async () => {
   const start = Date.now();
   const data = await wait(1000, "abc");
-  expect(data).toEqual("abc");
+  expect(data).to.equal("abc");
   const end = Date.now();
-  expect(end - start).toBeGreaterThanOrEqual(1000);
+  expect(end - start).to.be.gte(1000);
 });
