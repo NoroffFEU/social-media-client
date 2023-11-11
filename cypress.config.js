bca4a309dcd81013650086b7ae4d4dc43c8bca83
Jeanett Melsom <din@email.comjeanettk.m@hotmail.com>
2023-11-11 15:14:09 +0100

    opened cypress for the first time and configured it the correct way following the modules again

---
+++ cypress.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  video: false,
+  screenshots: false,
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
