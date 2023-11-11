f3b623160fe41a589fb14d8caee24309611a6b2b
rebekkanordheim <RebNor46673@stud.noroff.no>
2023-11-11 22:27:41 +0100

    trying a test

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,33 +2,9 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true,
-    jest: true,
-    'cypress/globals': true,
   },
   extends: 'eslint:recommended',
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  rules: {},
   overrides: [
-    {
-      files: ['**/*.cy.js'],
-      plugins: ['cypress'],
-      extends: ['plugin:cypress/recommended'],
-      rules: {
-        'cypress/no-unnecessary-waiting': 'off',
-        'no-unused-vars': 'off',
-      },
-    },
-    {
-      files: ['**/*.test.js'],
-      env: { jest: true },
-      plugins: ['jest'],
-      extends: ['plugin:jest/recommended'],
-      rules: { 'jest/prefer-expect-assertions': 'off' },
-    },
     {
       env: {
         node: true,
@@ -39,4 +15,9 @@ module.exports = {
       },
     },
   ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {},
 };
