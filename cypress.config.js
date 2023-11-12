aad1e7705fe531251a75e6de91ac391b7ea01f37
Jeanett Melsom <din@email.comjeanettk.m@hotmail.com>
2023-11-12 12:26:17 +0100

    made changes to the cypress.config.js file for testing

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,7 @@
-import { defineConfig } from "cypress";
-
-export default defineConfig({
-  projectId: "tc37ox",
-  video: false,
-  screenshots: false,
-
+const cypressConfig = {
   e2e: {
     setupNodeEvents(on, config) {},
-    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
   },
-});
+};
+
+export default cypressConfig;
