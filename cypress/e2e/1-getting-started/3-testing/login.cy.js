7da80f363fc8fc15b02d012552a6788fbc77bf91
Eirik Kirkeli <knirkeli88@msn.com>
2023-11-12 21:34:12 +0100

    Updated tests

--- cypress/e2e/1-getting-started/3-testing/login.cy.js
+++ cypress/e2e/1-getting-started/3-testing/login.cy.js
@@ -1,28 +1,15 @@
-// describe("loginTest", () => {
-//     beforeEach(() => {
-//       cy.visit("/");
-//     });    
-//     it("can log in user", () => {
-//       cy.visit("/");
-//       cy.wait(500);
-//       cy.get("#registerModal").contains("Login").click();
-//       // cy.get("button[type=submit]").contains("Login").click();
-//       cy.get("#loginEmail").type("vladmi@noroff.no,{enter}");
-//       // cy.wait(1000);
-//       cy.get("#loginPassword").type("password123");
-//         cy.get("#loginModal button[type=submit]").click();
-//         cy.wait(2000);
-//     })
-//   });
 describe("Login Form", () => {
+  const email = "towlietest3@noroff.no";
+  const password = "Towlietest123";
+
   it("should give access to profile page when logged in", () => {
     cy.visit("/");
     cy.wait(1000);
     cy.get("#registerModal").contains("Login").click();
     cy.get("#loginForm").should("be.visible");
     cy.wait(1000);
-    cy.get("#loginEmail").type("towlietest3@noroff.no");
-    cy.get("#loginPassword").type("Towlietest123");
+    cy.get("#loginEmail").type(email);
+    cy.get("#loginPassword").type(password);
     cy.get("button[type=submit]").contains("Login").click();
 
     cy.wait(2000);
--- cypress/e2e/1-getting-started/3-testing/logout.cy.js
+++ cypress/e2e/1-getting-started/3-testing/logout.cy.js
@@ -1,19 +1,22 @@
 describe("Login Form", () => {
-    it("should give access to profile page when logged in then log the user out", () => {
-      cy.visit("/");
-      cy.wait(1000);
-      cy.get("#registerModal").contains("Login").click();
-      cy.get("#loginForm").should("be.visible");
-      cy.wait(1000);
-      cy.get("#loginEmail").type("towlietest3@noroff.no");
-      cy.get("#loginPassword").type("Towlietest123");
-      cy.get("button[type=submit]").contains("Login").click();
-  
-      cy.wait(2000);
-      cy.location("href").should("include", "view=profile&name");
-        
-        cy.get("header button[type=button]").contains("Logout").click();
-        cy.wait(1000);
-        cy.getAllLocalStorage().should("be.empty");       
-    });
-    });
+  const email = "towlietest3@noroff.no";
+  const password = "Towlietest123";
+
+  it("should give access to profile page when logged in then log the user out", () => {
+    cy.visit("/");
+    cy.wait(1000);
+    cy.get("#registerModal").contains("Login").click();
+    cy.get("#loginForm").should("be.visible");
+    cy.wait(1000);
+    cy.get("#loginEmail").type(email);
+    cy.get("#loginPassword").type(password);
+    cy.get("button[type=submit]").contains("Login").click();
+
+    cy.wait(2000);
+    cy.location("href").should("include", "view=profile&name");
+
+    cy.get("header button[type=button]").contains("Logout").click();
+    cy.wait(1000);
+    cy.getAllLocalStorage().should("be.empty");
+  });
+});
