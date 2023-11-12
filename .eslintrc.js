d5e3705745d84d67fa588824e92048e192b5eaaa
rebekkanordheim <RebNor46673@stud.noroff.no>
2023-11-12 14:56:29 +0100

    cleaning up

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,20 +2,9 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true,
   },
-  extends: ['eslint:recommended'],
+  extends: 'eslint:recommended',
   overrides: [
-    {
-      files: ['**/*.cy.js'],
-      env: { 'cypress/globals': true },
-      plugins: ['cypress'],
-      extends: ['plugin:cypress/recommended'],
-      rules: {
-        'cypress/no-unnecessary-waiting': 'off',
-        'no-unused-vars': 'off',
-      },
-    },
     {
       env: {
         node: true,
