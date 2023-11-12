75789bd5e3df5de0fc145f1968bffc943f2977a9
Roar Falch Hanssen <roar.f.hanssen@gmail.com>
2023-11-12 20:47:51 +0100

    updated logout.cy.js

--- cypress/e2e/test/logout.cy.js
+++ cypress/e2e/test/logout.cy.js
@@ -3,34 +3,21 @@ const correctPassword = 'Pass1234';
 
 describe('Logout test', () => {
   it('should allow a valid user to log out', () => {
-    // Visit the login page
     cy.visit('/');
     cy.get('#registerModal').contains('Login').click();
     cy.get('#loginForm').should('be.visible');
 
-    // Login
     cy.get('#loginEmail').type(correctEmail);
     cy.get('#loginPassword').type(correctPassword);
     cy.get('button[type=submit]').contains('Login').click();
 
     // Wait for the token to be set in the local storage
-    cy.wait(2000);
+    cy.getCookie('token', { timeout: 10000 }).should('exist');
 
-    // Check storage for token
-    cy.window().then((window) => {
-      const token =
-        window.localStorage.getItem('token') ||
-        window.sessionStorage.getItem('token');
-      expect(token).to.be.a('string');
+    // Logout
+    cy.get('button[data-auth=logout]').contains('Logout').click();
 
-      // Logout
-      cy.get('button[data-auth=logout]').contains('Logout').click();
-
-      // Check storage for token deletion
-      const tokenAfterLogout =
-        window.localStorage.getItem('token') ||
-        window.sessionStorage.getItem('token');
-      expect(tokenAfterLogout).to.be.null;
-    });
+    // Check storage for token deletion
+    cy.getCookie('token', { timeout: 10000 }).should('not.exist');
   });
 });
