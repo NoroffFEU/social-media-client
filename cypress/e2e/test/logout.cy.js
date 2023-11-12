454962f6ba58e9cbbc96f8e1931e254460adc694
Roar Falch Hanssen <roar.f.hanssen@gmail.com>
2023-11-12 20:40:12 +0100

    updated logout.cy.js

--- cypress/e2e/test/logout.cy.js
+++ cypress/e2e/test/logout.cy.js
@@ -4,32 +4,29 @@ const correctPassword = 'Pass1234';
 describe('Logout test', () => {
   it('should allow a valid user to log out', () => {
     cy.visit('/');
-    cy.wait(500);
-    cy.get('#registerModal').contains('Login').click(); // Fix typo: contains
-    cy.wait(500);
+    cy.get('#registerModal').contains('Login').click();
     cy.get('#loginForm').should('be.visible');
     cy.get('#loginEmail').type(correctEmail);
     cy.get('#loginPassword').type(correctPassword);
     cy.get('button[type=submit]').contains('Login').click();
-    cy.wait(2000);
 
-    // check storage for token
+    // Check storage for token
     cy.window().then((window) => {
       const token =
         window.localStorage.getItem('token') ||
         window.sessionStorage.getItem('token');
       expect(token).to.be.a('string');
-      cy.wait(2000);
-      cy.get('button[data-auth=logout]').contains('Logout').click(); // Fix typo: contains
-      cy.wait(500);
-
-      // check storage for token deletion
-      cy.window().then((window) => {
-        const tokenAfterLogout =
-          window.localStorage.getItem('token') ||
-          window.sessionStorage.getItem('token');
-        expect(tokenAfterLogout).to.be.null; // Fix: to.be.null
-      });
     });
-  }); // Fix: Missing parenthesis
+
+    // Logout
+    cy.get('button[data-auth=logout]').contains('Logout').click();
+
+    // Check storage for token deletion
+    cy.window().then((window) => {
+      const tokenAfterLogout =
+        window.localStorage.getItem('token') ||
+        window.sessionStorage.getItem('token');
+      expect(tokenAfterLogout).to.be.null;
+    });
+  });
 });
