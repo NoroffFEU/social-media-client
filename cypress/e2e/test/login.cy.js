87b94515b4a0f1573165a51cf8c7cfa9ed7ae6a8
Roar Falch Hanssen <roar.f.hanssen@gmail.com>
2023-11-12 20:14:45 +0100

    updated login.cy.js

--- cypress/e2e/test/login.cy.js
+++ cypress/e2e/test/login.cy.js
@@ -4,51 +4,39 @@ const wrongEmail = 'RoaHanWRONG@stud.noroff.no';
 const wrongPassword = 'Wrong1234';
 
 describe('Login flow', () => {
-  it('should deny empty input submit', () => {
+  beforeEach(() => {
     cy.visit('/');
     cy.wait(500);
     cy.get('#registerModal').contains('Login').click();
     cy.wait(500);
     cy.get('#loginForm').should('be.visible');
+  });
+
+  it('should deny empty input submit', () => {
     cy.get('button[type=submit]').contains('Login').click();
     cy.get('#loginEmail:invalid').should('exist');
   });
 
   it('should deny empty password input', () => {
-    cy.visit('/');
-    cy.wait(500);
-    cy.get('#registerModal').contains('Login').click();
-    cy.wait(500);
-    cy.get('#loginForm').should('be.visible');
-    cy.get('#loginEmail').type(correctEmail);
     cy.get('button[type=submit]').contains('Login').click();
     cy.get('#loginPassword:invalid').should('exist');
   });
 
   it('should deny invalid email', () => {
-    cy.visit('/');
-    cy.wait(500);
-    cy.get('#registerModal').contains('Login').click();
-    cy.wait(500);
-    cy.get('#loginForm').should('be.visible');
     cy.get('#loginEmail').type(wrongEmail);
     cy.get('button[type=submit]').contains('Login').click();
     cy.get('#loginEmail:invalid').should('exist');
   });
 
   it('should deny wrong password', () => {
-    cy.visit('/');
-    cy.wait(500);
-    cy.get('#registerModal').contains('Login').click();
-    cy.wait(500);
-    cy.get('#loginForm').should('be.visible');
     cy.get('#loginEmail').type(correctEmail);
     cy.get('#loginPassword').type(wrongPassword);
-    cy.wait(500);
     cy.get('button[type=submit]').contains('Login').click();
-    cy.get('window:alert', (text) => {
-        expect(text).to.contains("Either your username was not found or your password is incorrect",
-        );
-    };
+
+    cy.on('window:alert', (text) => {
+      expect(text).to.include(
+        'Either your username was not found or your password is incorrect',
+      );
+    });
   });
 });
