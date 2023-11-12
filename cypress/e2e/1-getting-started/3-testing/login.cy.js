6c6e43efb90a1126bce815ecd346919e36af0550
Eirik Kirkeli <knirkeli88@msn.com>
2023-11-12 22:36:09 +0100

    fixer error

--- cypress/e2e/1-getting-started/3-testing/login.cy.js
+++ cypress/e2e/1-getting-started/3-testing/login.cy.js
@@ -1,18 +1,19 @@
-describe("Login Form", () => {
-  const email = "towlietest3@noroff.no";
-  const password = "Towlietest123";
+describe('Login Form', () => {
+  const email = 'towlietest3@noroff.no';
+  const password = 'Towlietest123';
 
-  it("should give access to profile page when logged in", () => {
-    cy.visit("/");
+  it('should give access to profile page when logged in', () => {
+    cy.visit('/');
     cy.wait(1000);
-    cy.get("#registerModal").contains("Login").click();
-    cy.get("#loginForm").should("be.visible");
+    cy.get('#registerModal').contains('Login').click();
+    cy.get('#loginForm').should('be.visible');
     cy.wait(1000);
-    cy.get("#loginEmail").type(email);
-    cy.get("#loginPassword").type(password);
-    cy.get("button[type=submit]").contains("Login").click();
+    cy.get('#loginEmail').type(email);
+    cy.get('#loginPassword').type(password);
+    cy.get('button[type=submit]').contains('Login').click();
 
     cy.wait(2000);
-    cy.location("href").should("include", "view=profile&name");
+    cy.location('href').should('include', 'view=profile&name');
   });
 });
+// fixing the test online
