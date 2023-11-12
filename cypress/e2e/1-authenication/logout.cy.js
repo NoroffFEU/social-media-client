423d89318d399ddf55d19e18a7728d6092675ed2
Ida Nguyen <115324491+idanguyen@users.noreply.github.com>
2023-11-12 21:32:58 +0100

    Add testing for logout localstorage

--- cypress/e2e/1-authenication/logout.cy.js
+++ cypress/e2e/1-authenication/logout.cy.js
@@ -5,6 +5,7 @@ describe("Logout", () => {
   it("Perform a login and logout action, check the state at every turn", () => {
     cy.visit("/");
     cy.wait(500);
+    cy.getAllLocalStorage().should("be.empty");
     cy.get("body").should("not.have.class", "logged-in");
     cy.get("#registerModal").contains("Close").click();
     cy.wait(500);
@@ -15,13 +16,16 @@ describe("Logout", () => {
     cy.wait(3000);
     cy.on("window:alert", (str) => {
       expect(str).to.not.equal(
-        "Either your username was not found or your password is incorrect"
+        "Either your username was not found or your password is incorrect",
       );
     });
     cy.wait(500);
     cy.get("body").should("have.class", "logged-in");
+    cy.getAllLocalStorage().should("not.be.empty");
+
     cy.get("button[data-auth=logout]").contains("Logout").click();
     cy.wait(500);
     cy.get("body").should("not.have.class", "logged-in");
+    cy.getAllLocalStorage().should("be.empty");
   });
 });
