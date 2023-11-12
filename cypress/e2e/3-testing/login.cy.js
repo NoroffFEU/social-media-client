6cb0260cc400e7a8aba72cc88a344c143428986a
chrisbekk <christianbekkelund@gmail.com>
2023-11-12 21:15:49 +0100

    first e2e test done

--- cypress/e2e/3-testing/login.cy.js
+++ cypress/e2e/3-testing/login.cy.js
@@ -1,14 +1,13 @@
-// describe('no.wikipedia.org', () => {
-//   it('can search for Noroff', () => {
-//     cy.visit('https://no.wikipedia.org');
-//     cy.get('input#searchInput').type('Noroff{enter}', { delay: 5000 });
-//     cy.get('h1').contains('Noroff');
-//   });
-// });
+const email = 'cbtest3@noroff.no';
+const validPassword = 'abcd1234';
+const invalidPassword = 'abcd123';
+const url = 'https://chrisbekk.github.io/social-media-client/';
 
 describe('noroff login', () => {
   it('can log in and visit profile page', () => {
-    cy.visit('https://chrisbekk.github.io/social-media-client/');
-    cy.get('#login-button');
+    cy.visit(url), { delay: 1000 };
+    cy.get('#login-button').click();
+    cy.get('#loginEmail').type(`${email}`);
+    cy.get('#loginPassword').type(`${validPassword}{enter}`);
   });
 });
