c93d88aa3161361c38a28f323420eea562cb4e68
Thomas N. Jensen <thomasnorupjensen@gmail.com>
2023-12-04 16:41:54 +0100

    Adds () to invoke click events in tests

--- cypress/e2e/1-login/login_error.cy.js
+++ cypress/e2e/1-login/login_error.cy.js
@@ -3,8 +3,9 @@ describe('login user', () => {
     cy.visit('http://localhost:5501');
     cy.get('input#registerEmail').type('ThoJen84480@stud.noroff.no{enter}');
     cy.get('input#registerPassword').type('!Yzems225{enter}');
-    cy.get('form#registerForm.modal-content button.btn.btn-outline-success')
-      .click;
+    cy.get(
+      'form#registerForm.modal-content button.btn.btn-outline-success',
+    ).click();
     cy.get('#loginModal.modal.fade').invoke('css', 'display', 'block');
     cy.get('input#registerName').invoke('css', 'display', 'none');
     cy.get('input#registerEmail').invoke('css', 'display', 'none');
@@ -12,6 +13,6 @@ describe('login user', () => {
     cy.get('.modal-header').invoke('css', 'display', 'none');
     cy.get('input#loginEmail').type('ThoJen84480@stud.noroff.no{enter}');
     cy.get('input#loginPassword').type('!Yzems225{enter}');
-    cy.get('form#loginForm.modal-content button.btn.btn-success').click;
+    cy.get('form#loginForm.modal-content button.btn.btn-success').click();
   });
 });
