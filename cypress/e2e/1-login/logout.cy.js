9fbda6c0d6f4a1046174a04a2eb574a34f8953f8
Thomas N. Jensen <thomasnorupjensen@gmail.com>
2023-12-04 16:24:34 +0100

    Adds logout.test.js

---
+++ cypress/e2e/1-login/logout.cy.js
@@ -0,0 +1,23 @@
+describe('logout user', () => {
+  it('can log a user out', () => {
+    cy.visit('http://localhost:5501');
+    cy.get('input#registerEmail').type('ThoJen84480@stud.noroff.no{enter}');
+    cy.get('input#registerPassword').type('!Yzems224{enter}');
+    cy.get('form#registerForm.modal-content button.btn.btn-outline-success')
+      .click;
+    cy.get('#loginModal.modal.fade').invoke('css', 'display', 'block');
+    cy.get('input#registerName').invoke('css', 'display', 'none');
+    cy.get('input#registerEmail').invoke('css', 'display', 'none');
+    cy.get('#registerModal').invoke('css', 'display', 'none');
+    cy.get('.modal-header').invoke('css', 'display', 'none');
+    cy.get('input#loginEmail').type('ThoJen84480@stud.noroff.no{enter}');
+    cy.get('input#loginPassword').type('!Yzems224{enter}');
+    cy.get('form#loginForm.modal-content button.btn.btn-success').click;
+    cy.intercept('http://localhost:5501/?view=profile&name=ThoJen84480').as(
+      'profilePage',
+    );
+    cy.visit('http://localhost:5501/?view=profile&name=ThoJen84480');
+    cy.wait('@profilePage');
+    cy.get('button.btn.btn-outline-warning.me-2').click;
+  });
+});
