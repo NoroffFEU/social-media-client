0fdd2ca0cc796162181728d647286c307152265b
Thomas N. Jensen <thomasnorupjensen@gmail.com>
2023-12-05 14:30:13 +0100

    Edits the e2e tests

--- cypress/e2e/1-login/login-error.cy.js
+++ cypress/e2e/1-login/login-error.cy.js
@@ -1,5 +1,5 @@
 describe('login user', { keystrokeDelay: 50 }, () => {
-  it('can log a user in', () => {
+  it('cannot log a user in with wrong credentials', () => {
     cy.visit(Cypress.env('baseUrl'));
     cy.get('[data-cy=register-email]').type(`${Cypress.env('userEmail')}`);
     cy.get('[data-cy=register-password]').type(
@@ -7,20 +7,17 @@ describe('login user', { keystrokeDelay: 50 }, () => {
     );
     cy.get('[data-cy=register-submit]').click();
     cy.get('[data-cy=login-email]').type(`${Cypress.env('userEmail')}`);
-    cy.get('[data-cy=login-password]').type(`${Cypress.env('userPassword')}`);
+    cy.get('[data-cy=login-password]').type(
+      `${Cypress.env('userWrongPassword')}`,
+    );
     cy.get('[data-cy=login-submit]').click();
-    /* cy.intercept(
+    cy.intercept(
       `${Cypress.env('baseUrl')}/?view=profile&name=${Cypress.env('userName')}`,
-    )
-      .as('profilePage')
-      .then(() => {
-        cy.visit(
-          `${Cypress.env('baseUrl')}/?view=profile&name=${Cypress.env(
-            'userName',
-          )}`,
-        );
-      }); */
-    /* cy.visit(`${Cypress.env('baseUrl')}/?view=profile&name=${Cypress.env('userName')}`
-    ) */
+    ).as('profilePage');
+    cy.visit(
+      `${Cypress.env('baseUrl')}/?view=profile&name=${Cypress.env('userName')}`,
+    );
+    cy.wait('@profilePage');
+    cy.contains('Logout').should('be.visible');
   });
 });
--- cypress/e2e/1-login/logout.cy.js
+++ cypress/e2e/1-login/logout.cy.js
@@ -1,18 +1,16 @@
-describe('logout user', () => {
+describe('logout user', { keystrokeDelay: 50 }, () => {
   it('can log a user out', () => {
     cy.visit(Cypress.env('baseUrl'));
-    cy.get('input#registerEmail').type(`${Cypress.env('userEmail')}`);
-    cy.get('input#registerPassword').type(`${Cypress.env('userPassword')}`);
-    cy.get(
-      'form#registerForm.modal-content button.btn.btn-outline-success',
-    ).click();
-    cy.get('#loginModal.modal.fade').invoke('css', 'display', 'block');
-    cy.get('input#registerName').invoke('css', 'display', 'none');
-    cy.get('input#registerEmail').invoke('css', 'display', 'none');
-    cy.get('#registerModal').invoke('css', 'display', 'none');
-    cy.get('input#loginEmail').type(`${Cypress.env('userEmail')}`);
-    cy.get('input#loginPassword').type(`${Cypress.env('userPassword')}`);
-    cy.get('form#loginForm.modal-content button.btn.btn-success').click();
+    cy.get('[data-cy=register-email]').type(`${Cypress.env('userEmail')}`);
+    cy.get('[data-cy=register-password]').type(
+      `${Cypress.env('userPassword')}`,
+    );
+    cy.get('[data-cy=register-submit]').click();
+    cy.get('[data-cy=login-email]').type(`${Cypress.env('userEmail')}`);
+    cy.get('[data-cy=login-password]').type(
+      `${Cypress.env('userWrongPassword')}`,
+    );
+    cy.get('[data-cy=login-submit]').click();
     cy.intercept(
       `${Cypress.env('baseUrl')}/?view=profile&name=${Cypress.env('userName')}`,
     ).as('profilePage');
