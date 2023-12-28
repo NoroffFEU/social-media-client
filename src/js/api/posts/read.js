4c023d626e5fe274025559126f5d056dc61fcc48
Connor O'Brien <connorbobrien@gmail.com>
2023-12-28 20:45:07 -0300

    Format the files
    All files opened and formatted with default Prettier extension settings

--- src/js/api/posts/read.js
+++ src/js/api/posts/read.js
@@ -2,19 +2,23 @@ import { apiPath } from "../constants.js";
 import { headers } from "../headers.js";
 
 export async function getPosts(limit = 20, offset = 0) {
-  const response = await fetch(`${apiPath}/social/posts?limit=${limit}&offset=${offset}&_reactions=true&_author=true&_comments=true`, { headers: headers() });
-  if (response.ok) {
-    return await response.json()
-  }
+	const response = await fetch(`${apiPath}/social/posts?limit=${limit}&offset=${offset}&_reactions=true&_author=true&_comments=true`, {
+		headers: headers(),
+	});
 
-  throw new Error(response.statusText);
+	if (response.ok) {
+		return await response.json();
+	}
+
+	throw new Error(response.statusText);
 }
 
 export async function getPost(id) {
-  const response = await fetch(`${apiPath}/social/posts/${id}?_reactions=true&_author=true&_comments=true`, { headers: headers() });
-  if (response.ok) {
-    return await response.json()
-  }
+	const response = await fetch(`${apiPath}/social/posts/${id}?_reactions=true&_author=true&_comments=true`, { headers: headers() });
 
-  throw new Error(response.statusText);
+	if (response.ok) {
+		return await response.json();
+	}
+
+	throw new Error(response.statusText);
 }
--- src/js/api/profiles/read.js
+++ src/js/api/profiles/read.js
@@ -2,19 +2,21 @@ import { apiPath } from "../constants.js";
 import { headers } from "../headers.js";
 
 export async function getProfiles() {
-  const response = await fetch(`${apiPath}/social/profiles`, { headers: headers() });
-  if (response.ok) {
-    return await response.json()
-  }
+	const response = await fetch(`${apiPath}/social/profiles`, { headers: headers() });
 
-  throw new Error(response.statusText);
+	if (response.ok) {
+		return await response.json();
+	}
+
+	throw new Error(response.statusText);
 }
 
 export async function getProfile(name) {
-  const response = await fetch(`${apiPath}/social/profiles/${name}?&_followers=true&_posts=true&_following=true`, { headers: headers() });
-  if (response.ok) {
-    return await response.json()
-  }
+	const response = await fetch(`${apiPath}/social/profiles/${name}?&_followers=true&_posts=true&_following=true`, { headers: headers() });
 
-  throw new Error(response.statusText);
+	if (response.ok) {
+		return await response.json();
+	}
+
+	throw new Error(response.statusText);
 }
