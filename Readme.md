c6b9d33851be6c7f241bfe0ff5588c3b928ee370
Kenneth Carlson <carlson90@hotmail.com>
2024-01-03 12:13:27 +0100

    Add autotest - and update readme

--- Readme.md
+++ Readme.md
@@ -9,7 +9,17 @@
 # How to Run
 
 
+Build: `npm run build`
+
 Start: `npm run start`
 
 Unit test: `npm run test-unit`
+
+Test E2E test : `npm run test-e2e-cli`
+
+Test: `npm run test`
+
+Test E2E in browser: `npm run cypress:open`
+
+ 
 
--- cypress/e2e/login_failed.cy.js
+++ cypress/e2e/login_failed.cy.js
@@ -1,17 +1,16 @@
-describe("Login failed", () => {
-  it("shows an error message for invalid login credentials", () => {
-    cy.visit("/");
+describe('Login failed', () => {
+  it('shows an error message for invalid login credentials', () => {
+    cy.visit('/');
 
     cy.wait(2000);
 
-    cy.get("#registerForm button.btn.btn-outline-success").contains("Login").click();
+    cy.get('#registerForm button.btn.btn-outline-success')
+      .contains('Login')
+      .click();
 
-    cy.get("input#loginEmail").type("hotmale@hotmail.com", { force: true });
-    cy.get("input#loginPassword").type("123456789", { force: true });
+    cy.get('input#loginEmail').type('hotmale@hotmail.com', { force: true });
+    cy.get('input#loginPassword').type('123456789', { force: true });
 
-    cy.get("#loginForm button").contains("Login").should("be.visible").click();
-
-  
-
+    cy.get('#loginForm button').contains('Login').should('be.visible').click();
   });
 });
