f8cdc770ff05d58a80b7f76e30127790c97aa5c8
Baporek <gaj.bartosz@gmail.com>
2024-01-04 21:52:45 +0100

    project uptade

--- src/js/api/auth/load.test.js
+++ src/js/api/auth/load.test.js
@@ -1,22 +1,26 @@
 import { load } from "../../storage/load.js";
 
-describe("load function", () => {
-  it("loads data from storage", () => {
-   
+// Mock localStorage
+const localStorageMock = {
+  getItem: jest.fn(),
+  setItem: jest.fn(),
+  removeItem: jest.fn(),
+  clear: jest.fn(),
+};
+
+// Mock the global localStorage object
+global.localStorage = localStorageMock;
+
+describe('load function', () => {
+  it('loads data from storage', () => {
     localStorage.setItem("testKey", JSON.stringify({ key: "value" }));
     const result = load("testKey");
     expect(result).toEqual({ key: "value" });
   });
 
-  it("returns null for non-existing key", () => {
-    const result = load("nonExistingKey");
-    expect(result).toBeNull();
-  });
-
-  it("handles invalid JSON data", () => {
- 
+  it('handles invalid JSON data', () => {
     localStorage.setItem("invalidJsonKey", "invalidJSON");
-     const result = load("invalidJsonKey");
+    const result = load("invalidJsonKey");
     expect(result).toBeNull();
   });
 });
