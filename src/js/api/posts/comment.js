e21269fe26dc5d83c34538b9cad1134a7ea02a71
CharlotteValset <119309299+CharlotteValset@users.noreply.github.com>
2024-01-12 11:53:12 +0100

    Runs the formatter

--- src/js/api/posts/comment.js
+++ src/js/api/posts/comment.js
@@ -5,12 +5,12 @@ export async function comment(postId, body, replyToId) {
   const response = await fetch(`${apiPath}/social/posts/${postId}/comment`, {
     method: "post",
     body: JSON.stringify({ body, replyToId }),
-    headers: headers("application/json")
-  })
+    headers: headers("application/json"),
+  });
 
   if (response.ok) {
-    return await response.json()
+    return await response.json();
   }
 
-  throw new Error(response.statusText)
+  throw new Error(response.statusText);
 }
--- src/js/templates/profile/followers.js
+++ src/js/templates/profile/followers.js
@@ -2,11 +2,11 @@ import { profileThumbnail } from "./thumbnail.js";
 
 export const profileFollowers = (profile) => {
   if (profile && profile.followers && profile.followers.length) {
-    const element = document.createElement('div');
-    element.classList.add('followers');
-    element.append("Followers", ...profile.followers.map(profileThumbnail))
-    return element
+    const element = document.createElement("div");
+    element.classList.add("followers");
+    element.append("Followers", ...profile.followers.map(profileThumbnail));
+    return element;
   }
 
-  return "\r\n"
-}
+  return "\r\n";
+};
