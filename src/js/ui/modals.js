6b06c45c74dfcb084ddb3f8f3653ddaa43910d51
piasun <77275966+piasun@users.noreply.github.com>
2024-01-13 18:49:24 +0100

    HOTFIX - tested prettier formatting

--- src/js/ui/modals.js
+++ src/js/ui/modals.js
@@ -1,7 +1,10 @@
 import { loginListener, registerListener } from "../listeners/index.js";
 
 export const modals = () => {
-  document.querySelector('form#loginForm').addEventListener('submit', loginListener);
-  document.querySelector('form#registerForm').addEventListener('submit', registerListener);
+  document
+    .querySelector("form#loginForm")
+    .addEventListener("submit", loginListener);
+  document
+    .querySelector("form#registerForm")
+    .addEventListener("submit", registerListener);
 };
-
