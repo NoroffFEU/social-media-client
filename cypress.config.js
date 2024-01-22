3239e0f271bd91f167c493f774eecd33f3b28d6a
absim29 <simon.bianca29@gmail.com>
2024-01-22 20:39:59 +0100

    added eslint exception rule

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,6 @@
-import { defineConfig } from "cypress";
+const { defineConfig } = require("cypress");
 
-export default defineConfig({
+module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
