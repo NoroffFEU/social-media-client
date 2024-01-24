34f17d80eb5ba841dd67570d4a7fc3dc1bbd83be
FadeCap <stian94lb@gmail.com>
2024-01-24 02:13:56 +0100

    Updated Node, Husky

--- src/js/views/profilePage.js
+++ src/js/views/profilePage.js
@@ -1,11 +1,11 @@
-import { isLoggedIn } from "../api/index.js";
-import { getProfile } from "../api/profiles/read.js";
-import { profilePageTemplate } from "../templates/index.js";
+import { isLoggedIn } from '../api/index.js';
+import { getProfile } from '../api/profiles/read.js';
+import { profilePageTemplate } from '../templates/index.js';
 
 export const profilePage = async (name) => {
   if (!isLoggedIn()) {
-    window.location.href = "./";
+    window.location.href = './';
     return;
   }
-  return profilePageTemplate(await getProfile(name));
+    return profilePageTemplate(await getProfile(name));
 };
