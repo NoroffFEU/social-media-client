9f4df8ca086cafd37e4ea0f09add5822dbc7f493
absim29 <simon.bianca29@gmail.com>
2024-01-27 12:18:43 +0100

    Valid login cy test

--- cypress/e2e/auth-test/valid-login.cy.js
+++ cypress/e2e/auth-test/valid-login.cy.js
@@ -1,14 +1,22 @@
 describe("Login Test with valid credentials", () => {
   beforeEach(() => {
+    Cypress.on("uncaught:exception", (err, runnable) => {
+      // Returning false prevents Cypress from failing the test
+      return false;
+    });
     cy.visit("/");
     cy.wait(500);
-    cy.get("#registerForm > .modal-footer > .btn-outline-success").click();
   });
   it("logs in if correct credentials are provided", () => {
-    cy.get("#loginEmail").should("be.visible").clear();
+    cy.get('#registerForm button[data-auth="login"]').click();
     cy.wait(500);
     cy.get("#loginEmail").type("bibi@stud.noroff.no");
-    // cy.get("#loginEmail").clear();
-    cy.get("#loginPassword").type(`blabla123{enter}`);
+    cy.get("#loginPassword").type(`blabla123`);
+    cy.get('button[type="submit"]').contains("Login").click();
+    cy.wait(1000);
+    cy.get('button[data-auth="logout"]')
+      .contains("Logout")
+      .should("be.visible");
+    cy.log("Login ok!");
   });
 });
