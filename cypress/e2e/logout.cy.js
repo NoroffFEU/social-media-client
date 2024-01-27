a288c8e555f8f147ba8ee17d0c1bb4d5c2d26a72
EF-Pami <pamijoelpami@gmail.com>
2024-01-27 17:56:32 +0100

    added logout.cy.js

--- cypress/e2e/logout.cy.js
+++ cypress/e2e/logout.cy.js
@@ -1,13 +1,26 @@
-describe('Logout Button', () => {
-    it('logs out a user', () => {
-        cy.visit('./');
-        cy.get('input[name=email]').first().type('mane@suiteTeardown.noroff.no');
-        cy.get('input[name=password]').first().type('mane12345');
-        cy.get('#loginForm').submit();
+describe("User logout", () => {
+  beforeEach(() => {
+    cy.visit("./");
+    cy.clearLocalStorage();
+  });
 
-        cy.get('[data-auth="logout"]').click();
-
-        cy.get('#loginForm').should('be.visible');
-        cy.url().should('equal', 'http://localhost:55499/');
-    });
+  it("successfully logs out the user", () => {
+    cy.visit("/");
+    cy.wait(1500);
+    cy.get(".btn-close:visible").click({ multiple: true });
+    cy.wait(1500);
+    cy.get("button[data-auth='login']:visible").click({ multiple: true });
+    cy.wait(1500);
+    cy.get("input[type='email']:visible")
+      .should("exist")
+      .type("name@example.com");
+    cy.get("input[type='password']:visible").should("exist").type("mane12345");
+    cy.get(".btn-success:visible").click({ multiple: true });
+    cy.wait(2000);
+    cy.get(window.localStorage.getItem("token")).should("not.to.be.empty");
+    cy.get(window.localStorage.getItem("profile")).should("not.to.be.empty");
+    cy.get("button[data-auth='logout']").click({ multiple: true });
+    cy.then(() => expect(window.localStorage.getItem("token")).to.be.null);
+    cy.then(() => expect(window.localStorage.getItem("profile")).to.be.null);
+  });
 });
