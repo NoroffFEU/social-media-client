8b5b9a32bfacb68cb70ebdeb6f8a5f3c6ece7048
Ramsnes <115258511+Ramsnes@users.noreply.github.com>
2024-01-28 16:09:49 +0100

    added cypress login and logout

--- cypress/e2e/morten.cy.js
+++
@@ -1,6 +0,0 @@
-describe("template spec", () => {
-  it("The user can log in with the login form with valid credentials", () => {
-    cy.visit("/");
-    cy.contains("button", "Login").eq(1).click();
-  });
-});
