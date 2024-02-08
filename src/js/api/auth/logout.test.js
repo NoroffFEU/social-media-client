3a52f27ddecea7f4c31ec6e29b0e7d816a41b873
bjornhaavard <bjornhaavard@hotmail.com>
2024-02-08 11:59:32 +0100

    got logout test to work

--- src/js/api/auth/logout.test.js
+++ src/js/api/auth/logout.test.js
@@ -1,40 +1,12 @@
-// import { logout } from "../auth/logout.js";
-// import { createMockLocation } from "../../jest-mock/mock.js";
-
-// describe("Logout Function", () => {
-//   beforeEach(() => {
-//     global.localStorage = {
-//       clear: jest.fn(),
-//     };
-//   });
-
-//   afterEach(() => {
-//     // Reset global.localStorage after each test
-//     global.localStorage = undefined;
-//   });
-//   it("should clear local storage and update window.location", () => {
-//     const mockEvent = {
-//       preventDefault: jest.fn(),
-//     };
-
-//     const mockLocation = createMockLocation("index.html");
-
-//     logout(mockEvent);
-
-//     expect(localStorage.clear).toHaveBeenCalledTimes(1);
-
-//   });
-// });
-
-import StorageMock from "../../api/jest-mock/storage-mock.js";
+import StorageMock from "../../jest-mock/storage-mock.js";
 import { logout } from "./logout.js";
 
 global.localStorage = new StorageMock();
 
-it("logout", () => {
+describe("logout", () => {
   test("removes the token", () => {
     localStorage.setItem("token", "doesn't matter");
     logout();
-    expect(localStorage.getItem("token")).to.be.null;
+    expect(localStorage.getItem("token")).toBeNull;
   });
 });
--- src/js/jest-mock/storage-mock.js
+++ src/js/jest-mock/storage-mock.js
@@ -1,4 +1,4 @@
-export class StorageMock {
+export default class StorageMock {
   constructor() {
     this.store = {};
 
