9c2c25f839c56a922085785cd81bfffaee763792
Ahed <ahed.jaguar@gmail.com>
2024-03-03 01:07:20 +0100

    Configure Cypress for end-to-end testing

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,34 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
-  extends: [
-    'standard',
-    'plugin:react/recommended'
-  ],
+  extends: ['standard', 'plugin:react/recommended'],
   overrides: [
     {
       env: {
-        node: true
+        node: true,
       },
-      files: [
-        '.eslintrc.{js,cjs}'
-      ],
+      files: ['.eslintrc.{js,cjs}'],
       parserOptions: {
-        sourceType: 'script'
-      }
-    }
+        sourceType: 'script',
+      },
+    },
+    {
+      files: ['**/*.cy.js'],
+      env: { 'cypress/globals': true },
+      plugins: ['cypress'],
+      extends: ['plugin:cypress/recommended'],
+      rules: {
+        'cypress/no-unnecessary-waiting': 'off',
+        'no-unused-vars': 'off',
+      },
+    },
   ],
   parserOptions: {
     ecmaVersion: 'latest',
-    sourceType: 'module'
+    sourceType: 'module',
   },
-  plugins: [
-    'react'
-  ],
-  rules: {
-  }
+  plugins: ['react'],
+  rules: {},
 }
