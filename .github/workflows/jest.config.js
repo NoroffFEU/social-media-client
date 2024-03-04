42f7c17390ef8a9cd2e3bf17abb63d4f138329cd
Aous Alhajjaj <artist89.ah@gmail.com>
2024-03-05 00:21:10 +0100

    jest
    cypress

---
+++ .github/workflows/jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    testEnvironment: 'node',
+    coverageThreshold: {
+      global: {
+        branches: 80,
+        functions: 80,
+        lines: 80,
+        statements: 80
+      }
+    }
+  };
