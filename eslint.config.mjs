3127b4a12dcd04260212f79bcc2d5c0e75c24b59
alexander <alexanderhardeland@gmail.com>
2024-03-17 12:28:00 +0100

    Adds a Prettier and Eslint config files with bare bones configs inside of them

---
+++ eslint.config.mjs
@@ -0,0 +1,12 @@
+export default {
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: 'eslint:recommended',
+	parseOptions: {
+		ecmaVersion: 12,
+		sourceType: 'module',
+	},
+	rules: {},
+};
---
+++ prettier.config.mjs
@@ -0,0 +1,7 @@
+const config = {
+	trailingComma: "es5",
+	tabWidth: 2,
+	semi: false,
+	singleQuote: true
+};
+export default config;
