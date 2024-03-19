fe5e974403aba93edc1f843d6beb254f24097ef1
martindyr <md@triark.no>
2024-03-19 11:24:04 +0100

    clean up files

--- cypress/e2e/logout/logout.cy.js
+++ cypress/e2e/logout/logout.cy.js
@@ -1,8 +1,8 @@
-describe("Login to app with valid credentials", () => {
+describe("Logout and clear local storage token value", () => {
   const mail = "ustest_432@stud.noroff.no";
   const password = "12345678";
 
-  it("Loads", () => {
+  it("Login and logout", () => {
     cy.visit("/");
     cy.clearLocalStorage();
     cy.get("#registerModal").find("button.btn-close").wait(500).click();
@@ -11,7 +11,7 @@ describe("Login to app with valid credentials", () => {
     cy.get("#loginEmail").type(mail, { delay: 200 });
     cy.get("#loginPassword").type(password);
     cy.get("button[type=submit]").contains("Login").wait(200).click();
-    cy.wait(500);
+    cy.wait(2000);
     cy.get('button[data-auth="logout"]').wait(500).click();
     cy.then(() => expect(window.localStorage.getItem("token")).to.be.null);
   });
