ce295bed10f35bdf419610141b684196a4a65a90
alexander <alexanderhardeland@gmail.com>
2024-03-21 12:45:02 +0100

    Adds the recommended package, but are not using it because then all variables are not defined

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,11 @@
 export default [
 	{
 		rules: {
+			semi: ['warn', 'always'],
 			'no-console': 'warn',
 			'no-alert': 'warn',
 			'dot-notation': 'error',
 			'prefer-const': 'warn'
 		}
 	}
-]
+];
