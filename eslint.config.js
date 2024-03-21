e167db92e6783af0523f216473231f2d9922a9a7
alexander <alexanderhardeland@gmail.com>
2024-03-21 12:56:08 +0100

    Adds the eslint recommended plugin in the eslint config file, then switched the no-undef rule to off

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,14 @@
+import js from '@eslint/js';
 export default [
+	js.configs.recommended,
 	{
 		rules: {
 			semi: ['warn', 'always'],
 			'no-console': 'warn',
 			'no-alert': 'warn',
 			'dot-notation': 'error',
-			'prefer-const': 'warn'
+			'prefer-const': 'warn',
+			'no-undef': 'off'
 		}
 	}
 ];
