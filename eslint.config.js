6962886ff468523737002caa8ef92cf1bb91dc14
alexander <alexanderhardeland@gmail.com>
2024-03-26 11:42:26 +0100

    Adds jest eslint plugin in eslint config file

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,18 @@
 import js from '@eslint/js';
 export default [
-	js.configs.recommended,
-	{
-		rules: {
-			semi: ['warn', 'always'],
-			'no-console': 'warn',
-			'no-alert': 'warn',
-			'dot-notation': 'error',
-			'prefer-const': 'warn',
-			'no-undef': 'off'
-		}
-	}
+  js.configs.recommended,
+  {
+    rules: {
+      semi: ['warn', 'always'],
+      'no-console': 'warn',
+      'no-alert': 'warn',
+      'dot-notation': 'error',
+      'prefer-const': 'warn',
+      'no-undef': 'off',
+    },
+  },
+  {
+    files: ['**/*.test.js'],
+    extends: ['plugin:jest/recommended'],
+  },
 ];
