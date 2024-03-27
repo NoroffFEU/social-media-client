6f92e5e116a9aae039da25b78dfa4b312e0a0f41
alexander <alexanderhardeland@gmail.com>
2024-03-27 08:26:28 +0100

    Adds a dummy import file to see if babel works

--- src/unit-tests/baseline.test.js
+++ src/unit-tests/baseline.test.js
@@ -1,3 +1,9 @@
-test('It adds 1 and 2 and gets 3 as a result', () => {
-  expect(1 + 2).toEqual(3);
+import { wait } from './es6';
+
+test("It resolves with 'abc' value after 1 second", async () => {
+  const start = Date.now();
+  const data = await wait(1000, 'abc');
+  expect(data).toEqual('abc');
+  const end = Date.now();
+  expect(end - start).toBeGreaterThanOrEqual(1000);
 });
